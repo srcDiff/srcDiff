882a34b276c9045a9e4ecd7c9fd8af176e8c0414
Michael John Decker <mdecker6@kent.edu>
2011-08-28 00:05:37 +0000

    Added call
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@7775 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -271,10 +271,10 @@ int main(int argc, char * argv[]) {
       // add preceeding unchanged
       if(edits->operation == DELETE)
         for(int j = last_diff; j < edits->offset_sequence_one; ++rbuf_old.line_number, ++j)
-          output_xml_line(&rbuf_old, reader_old, writer);
+          compare_same_line(&rbuf_old, reader_old, &rbuf_new, reader_new, writer);
       else
         for(int j = last_diff; j < edits->offset_sequence_one + 1; ++rbuf_old.line_number, ++j)
-          output_xml_line(&rbuf_old, reader_old, writer);
+          compare_same_line(&rbuf_old, reader_old, &rbuf_new, reader_new, writer);
 
       // detect and change
       struct edit * edit_next = edits->next;
