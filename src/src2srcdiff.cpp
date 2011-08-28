50b634c4cceb478bb9638b5762a16565ff61d913
Michael John Decker <mdecker6@kent.edu>
2011-08-28 00:03:12 +0000

    Added dual same read
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@7773 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -516,16 +516,20 @@ void compare_same_line(struct reader_buffer * rbuf_old, xmlTextReaderPtr reader_
         rbuf_old->characters = NULL;
 
         not_done = xmlTextReaderRead(reader_old);
+        xmlTextReaderRead(reader_new);
       }
     }
     else {
 
       update_context(rbuf_old, reader_old);
       update_in_diff(rbuf_old, reader_old, false);
+      update_context(rbuf_new, reader_new);
+      update_in_diff(rbuf_new, reader_new, false);
 
       // output non-text node and get next node
       outputXML(reader_old, writer);
       not_done = xmlTextReaderRead(reader_old);
+      xmlTextReaderRead(reader_new);
     }
 }
 
