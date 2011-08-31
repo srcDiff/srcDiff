9d1b7b08d247191bf31f5428623882de3cb204ac
Michael John Decker <mdecker6@kent.edu>
2011-08-31 19:46:09 +0000

    Added basic output
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@7871 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -810,11 +810,17 @@ void output_double(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf_
     // output diff tag
     xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<diff:old status=\"start\"/>"));
 
+    for(int j = 0; j < edits->length; ++j)
+          outputNode(*(*rbuf_old->buffer)[edits->offset_sequence_one + j], writer);
+
     // output diff tag
     xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<diff:old status=\"end\"/>"));
 
     // output diff tag
     xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<diff:new status=\"start\"/>"));
+
+    for(int j = 0; j < edit_next->length; ++j)
+          outputNode(*(*rbuf_new->buffer)[edit_next->offset_sequence_one + j], writer);
 
     // output diff tag
     xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<diff:new status=\"end\"/>"));
@@ -832,6 +838,9 @@ void output_double(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf_
     // output diff tag
     xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<diff:new status=\"start\"/>"));
 
+    for(int j = 0; j < edits->length; ++j)
+          outputNode(*(*rbuf_old->buffer)[edits->offset_sequence_one + j], writer);
+
     // output diff tag
     xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<diff:new status=\"end\"/>"));
 
@@ -842,6 +851,9 @@ void output_double(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf_
 
     // output diff tag
     xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<diff:old status=\"start\"/>"));
+
+    for(int j = 0; j < edit_next->length; ++j)
+          outputNode(*(*rbuf_new->buffer)[edit_next->offset_sequence_one + j], writer);
 
     // output diff tag
     xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<diff:old status=\"end\"/>"));
