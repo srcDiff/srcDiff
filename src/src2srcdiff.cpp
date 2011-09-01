2d286226d33dfcac4358216d21869f5d07b6d273
Michael John Decker <mdecker6@kent.edu>
2011-09-01 16:40:07 +0000

    Commented about where will detect whitespace
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@7885 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -816,6 +816,8 @@ void output_double(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf_
     if(edits->operation == DELETE && edits->next != NULL && edit_next->operation == INSERT
        && (edits->offset_sequence_one + edits->length - 1) == edits->next->offset_sequence_one) {
 
+      // look for pure whitespace change
+
       // output diff tag
       xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<diff:old status=\"start\"/>"));
 
