7be08cb4fea5e9ae94ae36d1f73a19f0fa8813d4
Michael John Decker <mdecker6@kent.edu>
2011-09-01 20:43:34 +0000

    Computing whitespace length
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@7900 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -755,6 +755,8 @@ void output_double(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf_
          && (xmlReaderTypes)(*rbuf_old->buffer)[edits->offset_sequence_one]->type == XML_READER_TYPE_TEXT
          && edits->length == 1 && edit_next->length == 1) {
 
+        int whitespace_length_one = strspn((*rbuf_old->buffer)[edits->offset_sequence_one]->contents, " \t\r\n");
+
       }
 
       // output diff tag
