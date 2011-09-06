2d4954e5017e9a3a573bbc84aaca90e05021ae01
Michael John Decker <mdecker6@kent.edu>
2011-09-06 16:54:10 +0000

    Added test if whitespace
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@7919 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -810,6 +810,10 @@ void output_double(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf_
         int whitespace_length_old = strspn((const char *)(*rbuf_old->buffer)[edits->offset_sequence_one]->content, " \t\r\n");
         int whitespace_length_new = strspn((const char *)(*rbuf_new->buffer)[edit_next->offset_sequence_two]->content, " \t\r\n");
 
+        if(whitespace_length_old == rbuf_old->buffer->size() && whitespace_length_new == rbuf_new->buffer->size()) {
+
+        }
+
       }
 
       // output diff tag
