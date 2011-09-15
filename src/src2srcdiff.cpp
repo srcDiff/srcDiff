fddaa640840942f2d9e24f62aeef21c9694632dd
Michael John Decker <mdecker6@kent.edu>
2011-09-15 20:37:10 +0000

    Commented out debug
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@7970 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -533,7 +533,7 @@ void collect_difference(struct reader_buffer * rbuf, xmlTextReaderPtr reader, st
             text->name = (const xmlChar *)"text";
 
             const char * content = strndup((const char *)characters_start, rbuf->characters  - characters_start);
-            fprintf(stderr, "HERE: '%s'\n", content);
+            //fprintf(stderr, "HERE: '%s'\n", content);
             text->content = (xmlChar *)content;
             rbuf->buffer->push_back(text);
 
@@ -550,7 +550,7 @@ void collect_difference(struct reader_buffer * rbuf, xmlTextReaderPtr reader, st
           text->name = (const xmlChar *)"text";
 
           const char * content = strndup((const char *)characters_start, rbuf->characters  - characters_start);
-          fprintf(stderr, "HERE: '%s'\n", content);
+          //fprintf(stderr, "HERE: '%s'\n", content);
           text->content = (xmlChar *)content;
           rbuf->buffer->push_back(text);
 
@@ -571,7 +571,7 @@ void collect_difference(struct reader_buffer * rbuf, xmlTextReaderPtr reader, st
           text->name = (const xmlChar *)"text";
 
           const char * content = strndup((const char *)characters_start, (rbuf->characters + 1) - characters_start);
-          fprintf(stderr, "HERE: '%s'\n", content);
+          //fprintf(stderr, "HERE: '%s'\n", content);
           text->content = (xmlChar *)content;
           rbuf->buffer->push_back(text);
 
@@ -599,7 +599,7 @@ void collect_difference(struct reader_buffer * rbuf, xmlTextReaderPtr reader, st
           text->name = (const xmlChar *)"text";
 
           const char * content = strdup((const char *)characters_start);
-          fprintf(stderr, "HERE: '%s'\n", content);
+          //       fprintf(stderr, "HERE: '%s'\n", content);
           text->content = (xmlChar *)content;
           rbuf->buffer->push_back(text);
 
