313f4b991243e4313efe6508dfa53e764842985e
Michael John Decker <mdecker6@kent.edu>
2011-09-15 20:10:12 +0000

    Added debug for text nodes
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@7963 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -533,6 +533,7 @@ void collect_difference(struct reader_buffer * rbuf, xmlTextReaderPtr reader, st
             text->name = (const xmlChar *)"text";
 
             const char * content = strndup((const char *)characters_start, rbuf->characters  - characters_start);
+            fprintf(stderr, "HERE: '%s'", content);
             text->content = (xmlChar *)content;
             rbuf->buffer->push_back(text);
 
@@ -549,6 +550,7 @@ void collect_difference(struct reader_buffer * rbuf, xmlTextReaderPtr reader, st
           text->name = (const xmlChar *)"text";
 
           const char * content = strndup((const char *)characters_start, rbuf->characters  - characters_start);
+          fprintf(stderr, "HERE: '%s'", content);
           text->content = (xmlChar *)content;
           rbuf->buffer->push_back(text);
 
@@ -569,6 +571,7 @@ void collect_difference(struct reader_buffer * rbuf, xmlTextReaderPtr reader, st
           text->name = (const xmlChar *)"text";
 
           const char * content = strndup((const char *)characters_start, (rbuf->characters + 1) - characters_start);
+          fprintf(stderr, "HERE: '%s'", content);
           text->content = (xmlChar *)content;
           rbuf->buffer->push_back(text);
 
