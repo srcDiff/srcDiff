2af459753d36d79db6748e0792b845bc05c2a342
Michael John Decker <mdecker6@kent.edu>
2011-10-03 19:42:34 +0000

    Started to set boolean
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8115 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -607,7 +607,7 @@ void output_single(struct reader_buffer * rbuf, struct edit * edit, xmlTextWrite
   int last_open;
   for(last_open = (rbuf->in_diff->size() - 1); last_open > 0 && (*rbuf->in_diff)[last_open] == edit->operation; --last_open);
 
-  bool issue_diff;
+  bool issue_diff = (last_open + 1) != rbuf->in_diff->size();
 
   // output starting diff tag
   if(edit->operation == DELETE)
