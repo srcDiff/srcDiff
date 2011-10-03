610d5959f8b6d315e6b1eb21eff1d711133a734d
Michael John Decker <mdecker6@kent.edu>
2011-10-03 20:19:35 +0000

    Fixing conditions
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8131 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -612,9 +612,9 @@ void output_single(struct reader_buffer * rbuf, struct edit * edit, xmlTextWrite
   int last_open;
   for(last_open = (rbuf->in_diff->size() - 1); last_open > 0 && (*rbuf->in_diff)[last_open] == edit->operation; --last_open);
 
-  bool issued_diff = (last_open + 1) == rbuf->in_diff->size() || !(*rbuf->issued_diff)[last_open + 1];
+  bool issued_diff = (last_open + 1) != rbuf->in_diff->size() && !(*rbuf->issued_diff)[last_open + 1];
 
-  if((last_open + 1) == rbuf->in_diff->size() && !(*rbuf->issued_diff)[last_open + 1])
+  if((last_open + 1) != rbuf->in_diff->size() && !(*rbuf->issued_diff)[last_open + 1])
     (*rbuf->issued_diff)[last_open + 1] = true;
 
   // output starting diff tag
