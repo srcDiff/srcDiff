9dc7b07a73655e77f7a4b926b5df45950a0c4f28
Michael John Decker <mdecker6@kent.edu>
2011-10-04 02:00:48 +0000

    Fixing condition
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8208 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -695,9 +695,6 @@ void output_single(struct reader_buffer * rbuf, struct edit * edit, xmlTextWrite
 
     if((xmlReaderTypes)bnode->type == XML_READER_TYPE_END_ELEMENT && strcmp((const char *)node->name, (const char *)bnode->name) == 0) {
 
-
-      ++i;
-      
       if(j == last_open)
         break;
       else
