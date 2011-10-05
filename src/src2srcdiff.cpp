c15c5b90ea3e4170b57711844a44cc54cf745292
Michael John Decker <mdecker6@kent.edu>
2011-10-05 22:47:13 +0000

    Reverted
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8364 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -536,7 +536,9 @@ void compare_same_line(struct reader_buffer * rbuf_old, xmlTextReaderPtr reader_
         output_type = rbuf_old->in_diff->back();
         output_end = rbuf_old->issued_diff->size() - 2;
 
-      } else if(rbuf_new->issued_diff->back() && (xmlReaderTypes)getRealCurrentNode(reader_new)->type == XML_READER_TYPE_END_ELEMENT) {
+      }
+
+      if(rbuf_new->issued_diff->back() && (xmlReaderTypes)getRealCurrentNode(reader_new)->type == XML_READER_TYPE_END_ELEMENT) {
 
         mark_open = true;
         output_type = rbuf_new->in_diff->back();
