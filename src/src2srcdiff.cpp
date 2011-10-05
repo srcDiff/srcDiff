afcba0050ef8a7239574387710f8efcf4d1b5700
Michael John Decker <mdecker6@kent.edu>
2011-10-05 16:18:12 +0000

    Fixing compile
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8298 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -513,7 +513,7 @@ void compare_same_line(struct reader_buffer * rbuf_old, xmlTextReaderPtr reader_
       if(rbuf_old->issued_diff->back() && (xmlReaderTypes)getRealCurrentNode(reader_old)->type == XML_READER_TYPE_END_ELEMENT) {
 
         mark_open = true;
-        output_type = rbuf->in_diff->back();
+        output_type = rbuf_old->in_diff->back();
         output_end = rbuf_old->issued_diff->size() - 2;
 
       }
