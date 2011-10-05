13314cad280857984903a8cf47b45a22723cdfad
Michael John Decker <mdecker6@kent.edu>
2011-10-05 16:36:09 +0000

    Fixing still
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8314 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/src/ExtractSource.cpp
+++ test/src/ExtractSource.cpp
@@ -42,7 +42,7 @@ int main(int argc, char * argv[]) {
   xmlSAXHandler sax = factory();
   ctxt->sax = &sax;
 
-  struct source_diff data = { diff ? INSERT : DELETE, false};
+  struct source_diff data = { diff,  new std::vector<int>()};
 
   ctxt->_private = &data;
 
