a87aa54fc64f26b9867e817b5caf91711bdf3074
Michael John Decker <mdecker6@kent.edu>
2011-10-05 14:43:31 +0000

    More correction
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8239 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -329,7 +329,7 @@ int main(int argc, char * argv[]) {
     }
 
     for(unsigned int j = last_diff; j < lines1.size(); ++rbuf_old.line_number, ++rbuf_new.line_number, ++j)
-      compare_same_line(&rbuf_old, reader_old, &rbuf_new, reader_new, writer);
+      compare_same_line(&rbuf_old, reader_old, &rbuf_new, reader_new, writer, last_diff + lines1.size());
 
     compare_same_line(&rbuf_old, reader_old, &rbuf_new, reader_new, writer);
   
