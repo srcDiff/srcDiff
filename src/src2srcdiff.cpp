37ca29b2d37077050c72acd5f5b13f1d280a9562
Michael John Decker <mdecker6@kent.edu>
2011-10-25 17:08:35 +0000

    Removing warnings
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8437 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -943,8 +943,7 @@ void output_single(struct reader_buffer * rbuf, struct edit * edit, xmlTextWrite
 
   // count number same open
   int count = 1;
-  for(unsigned 
-int i = last_open + 1; i < rbuf->context->size(); ++i) {
+  for(unsigned int i = last_open + 1; i < rbuf->context->size(); ++i) {
 
     xmlNodePtr bnode = (*rbuf->context)[i];
 
