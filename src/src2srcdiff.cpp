4ddde0ce019d088e5fe27ef3d08cc51f6f0c3a4a
Michael John Decker <mdecker6@kent.edu>
2011-10-27 23:40:35 +0000

    Work
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8643 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -1146,7 +1146,11 @@ void output_handler(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf
   }
 
   // check if need to void output_buffer
-  if(wait_output != COMMON &&  wait_output == rbuf_old->output_diff->back()->operation){
+  if(wait_output != COMMON && wait_output == rbuf_old->output_diff->back()->operation) {
+
+    wait_output == COMMON;
+    for(int i = 0; i < output_buffer.size(); ++i)
+      output_handler(rbuf_old, rbuf_new, output_buffer[i], rbuf_old->output_diff->back()->operation, writer);
 
   }
 
