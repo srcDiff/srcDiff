4a833b152655825dcf8595f360bdb05d7a2a8708
Michael John Decker <mdecker6@kent.edu>
2011-10-28 03:01:27 +0000

    Fixing a condition
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8679 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -1092,8 +1092,8 @@ void output_handler(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf
     } else */
     
     if((((operation == DELETE ? rbuf_old->output_diff->back()->operation : rbuf_new->output_diff->back()->operation) == COMMON 
-        && rbuf_old->open_diff->back()->operation != COMMON
-        && rbuf_new->open_diff->back()->operation != COMMON)
+         && (rbuf_old->open_diff->back()->operation != COMMON
+             || rbuf_new->open_diff->back()->operation != COMMON))
 
               && (strcmp((const char *)rbuf_old->open_diff->back()->open_elements->back()->name, (const char *)node->name) != 0
                   || strcmp((const char *)rbuf_old->output_diff->back()->open_elements->back()->name, (const char *)node->name) != 0
