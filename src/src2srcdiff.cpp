e640884753b867a05849f26c6f54697858b2941b
Michael John Decker <mdecker6@kent.edu>
2011-10-29 23:44:02 +0000

    Fixing something
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8735 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -1259,7 +1259,8 @@ void output_handler(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf
         || strcmp((const char *)rbuf->open_diff->back()->open_elements->back(), (const char *)node->name) == 0)) {
     */
 
-    if(rbuf->open_diff->back()->operation != COMMON
+    if((rbuf->open_diff->back()->operation != COMMON
+       && strcmp((const char *)rbuf->open_diff->back()->open_elements->back()->name, (const char *)node->name) == 0)
         && (rbuf->open_diff->back()->operation != rbuf->output_diff->back()->operation
             || strcmp((const char *)rbuf->output_diff->back()->open_elements->back()->name, (const char *)node->name) != 0)) {
 
