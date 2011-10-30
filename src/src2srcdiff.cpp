cafb5aece01bfcee49530b07e7c0736622b9ed35
Michael John Decker <mdecker6@kent.edu>
2011-10-30 21:59:18 +0000

    Now trying to add optimization
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8762 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -443,17 +443,9 @@ void compare_same_line(struct reader_buffer * rbuf_old, xmlTextReaderPtr reader_
 
         collect_difference(rbuf_old, reader_old, DELETE, rbuf_old->line_number + 1);
 
-        //output_double(rbuf_old, rbuf_new, writer);
-
-        struct edit edit;
-        edit.operation = DELETE;
-
-        output_single(rbuf_old, rbuf_new, &edit, writer);
-
-        edit.operation = INSERT;
         collect_difference(rbuf_new, reader_new, INSERT, rbuf_new->line_number + 1);
 
-        output_single(rbuf_old, rbuf_new, &edit, writer);
+        output_double(rbuf_old, rbuf_new, writer);
 
         --rbuf_old->line_number;
         --rbuf_new->line_number;
