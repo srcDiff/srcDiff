eec9cf322beca5ca1499420b4ccad1d537e64a96
Michael John Decker <mdecker6@kent.edu>
2011-10-31 22:11:41 +0000

    Fixing condition
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8857 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -1179,13 +1179,13 @@ void output_handler(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf
         && (*rbuf_old->open_diff->back()->open_tags)[unmarked]->marked; --unmarked);
  
       if(strcmp((const char *)(*rbuf_old->open_diff->back()->open_tags)[unmarked]->node->name, (const char *)node->name) == 0)
-        (*rbuf_old->open_diff->back()->open_tags)[unmarked]->marked;
+        (*rbuf_old->open_diff->back()->open_tags)[unmarked]->marked = true;;
 
       for(unmarked = rbuf_new->open_diff->back()->open_tags->size() - 1; unmarked > 0 
             && (*rbuf_new->open_diff->back()->open_tags)[unmarked]->marked; --unmarked);
 
       if(strcmp((const char *)(*rbuf_new->open_diff->back()->open_tags)[unmarked]->node->name, (const char *)node->name) == 0)
-        (*rbuf_new->open_diff->back()->open_tags)[unmarked]->marked;
+        (*rbuf_new->open_diff->back()->open_tags)[unmarked]->marked = true;
 
     /*
     if(rbuf->output_diff->back()->operation != COMMON
