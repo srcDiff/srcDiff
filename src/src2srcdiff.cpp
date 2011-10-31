e51354b542a61fb1c0c7f685abedcc1d41d45149
Michael John Decker <mdecker6@kent.edu>
2011-10-31 20:52:57 +0000

    UNdo
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8843 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -365,7 +365,7 @@ int main(int argc, char * argv[]) {
 
       // detect and change
       struct edit * edit_next = edits->next;
-      if(0 && edits->operation == DELETE && edits->next != NULL && edit_next->operation == INSERT
+      if(edits->operation == DELETE && edits->next != NULL && edit_next->operation == INSERT
          && (edits->offset_sequence_one + edits->length - 1) == edits->next->offset_sequence_one) {
 
         collect_difference(&rbuf_old, reader_old, DELETE, edits->offset_sequence_one + edits->length);
