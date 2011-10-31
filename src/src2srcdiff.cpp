c5608b1aca0e45c5b8ad44ed471c4aafa4343ec4
Michael John Decker <mdecker6@kent.edu>
2011-10-31 19:28:39 +0000

    Going full elements
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8817 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -169,20 +169,16 @@ int main(int argc, char * argv[]) {
   const char * srcdiff_file;
   srcdiff_file = "-";
 
-diff_old_start = { 0 };
-diff_old_start.name = "old";
+  diff_old_start.name = (xmlChar *)"old";
 diff_old_start.type = XML_READER_TYPE_ELEMENT;
 
-diff_old_end = { 0 };
-diff_old_end.name = "old";
+diff_old_end.name = (xmlChar *)"old";
 diff_old_end.type = XML_READER_TYPE_END_ELEMENT;
 
-diff_new_start = { 0 };
-diff_new_start.name = "new";
+diff_new_start.name = (xmlChar *)"new";
 diff_new_start.type = XML_READER_TYPE_ELEMENT;
 
-diff_new_end = { 0 };
-diff_new_end.name = "new";
+diff_new_end.name = (xmlChar *)"new";
 diff_new_end.type = XML_READER_TYPE_END_ELEMENT;
 
   /*
