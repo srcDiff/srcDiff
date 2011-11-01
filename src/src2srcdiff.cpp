6fc4f3a907cdb4426fcd5bcaf7d52f5b14e8f5ba
Michael John Decker <mdecker6@kent.edu>
2011-11-01 16:32:56 +0000

    Adding another function to diff sets of nodes
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8909 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -91,26 +91,23 @@ const void * node_index(int idx, const void *s) {
 }
 
 // diff node comparison function
-int node_compare(const void * e1, const void * e2) {
+int node_set_compare(const void * e1, const void * e2) {
   std::vector<xmlNode *> * node_set1 = (std::vector<xmlNode *> *)e1;
   std::vector<xmlNode *> * node_set2 = (std::vector<xmlNode *> *)e2;
 
+  if(node_set1->size() != node_set2->size())
+    return 1;
+  else
+    for(int i = 0; i < node_set1->size(); ++i)
+      if(node_compare(node_set1.at(i), node_set2.at(i)))
+        return 1;
 
-  if(node1->type == node2->type && strcmp((const char *)node1->name, (const char *)node2->name) == 0) {
-
-    // end if text node contents differ
-    if((xmlReaderTypes)node1->type == XML_READER_TYPE_TEXT)
-      return strcmp((const char *)node1->content, (const char *)node2->content);
-    else
-      return 0;
-  }
-
-  return 1;
+  return 0;
 }
 
 
 // diff node comparison function
-int node_set_compare(const void * e1, const void * e2) {
+int node_compare(const void * e1, const void * e2) {
   xmlNode * node_set1 = (xmlNode *)e1;
   xmlNode * node_set2 = (xmlNode *)e2;
 
