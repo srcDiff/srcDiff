396adc123490731aec88fda30d449559121bae6d
Michael John Decker <mdecker6@kent.edu>
2011-11-01 17:54:37 +0000

    Leftovers from faulty merge
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8922 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -905,7 +905,7 @@ std::vector<std::vector<xmlNodePtr> *> * create_node_set(struct reader_buffer * 
 // output a change
 void output_double(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf_new, xmlTextWriterPtr writer) {
 
-  fprintf(stderr, "HERE_DOUBLE\n");
+  //fprintf(stderr, "HERE_DOUBLE\n");
 
   std::vector<std::vector<xmlNodePtr> *> * node_sets_old = create_node_set(rbuf_old);
   std::vector<std::vector<xmlNodePtr> *> * node_sets_new = create_node_set(rbuf_new);
