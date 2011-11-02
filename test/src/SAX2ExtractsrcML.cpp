823ff3eba79156e824f1f3f49885aa8e59adc75e
Michael John Decker <mdecker6@kent.edu>
2011-11-02 20:45:54 +0000

    Changing to output srcml
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8968 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/src/SAX2ExtractsrcML.cpp
+++ test/src/SAX2ExtractsrcML.cpp
@@ -10,6 +10,11 @@
 #include <vector>
 #include "SAX2ExtractSource.hpp"
 
+void output_start_node(void* ctx, const xmlChar* localname, const xmlChar* prefix, const xmlChar* URI,
+                       int nb_namespaces, const xmlChar** namespaces, int nb_attributes, int nb_defaulted,
+                       const xmlChar** attributes);
+
+
 xmlSAXHandler factory() {
 
   xmlSAXHandler sax = { 0 };
@@ -47,33 +52,15 @@ void startElementNs(void* ctx, const xmlChar* localname, const xmlChar* prefix, 
 
   if(strcmp((const char *)localname, "common") == 0)
     data->in_diff->push_back(COMMON);
+  else
 
   if(strcmp((const char *)localname, "old") == 0)
     data->in_diff->push_back(DELETE);
-
+  else
   if(strcmp((const char *)localname, "new") == 0)
     data->in_diff->push_back(INSERT);
-
-  }
-
-
-  if(strcmp((const char *)localname, "escape") == 0) {
-
-    int index;
-    for(int i = 0, index = 0; i < nb_attributes; ++i, index += 5) {
-
-      if(strcmp((const char *)attributes[index], "char") == 0) {
-
-        int end = attributes[index + 4] - attributes[index + 3];
-        char * value = strndup((const char *)attributes[index + 3], end);
-
-        fprintf(stdout, "%c", (char)strtol(value, NULL, 16));
-
-      }
-
-    }
-
-  }
+  else
+  output_start_node(ctx, localname, prefix, URI, nb_namespaces, namepsaces, nb_attributes, nb_defaulted, attributes);
 
 }
 
@@ -86,7 +73,8 @@ void endElementNs(void *ctx, const xmlChar *localname, const xmlChar *prefix, co
      || strcmp((const char *)localname, "old") == 0
      || strcmp((const char *)localname, "new") == 0)
     data->in_diff->pop_back();
-
+  else
+    output_close_node(ctx, localname, prefix, URI);
 }
 
 void characters(void* ctx, const xmlChar* ch, int len) {
@@ -106,34 +94,35 @@ void comments(void* ctx, const xmlChar* ch) {
   // fprintf(stderr, "%s\n\n", __FUNCTION__);
 }
 
-void output_start_node() {
+void output_start_node(void* ctx, const xmlChar* localname, const xmlChar* prefix, const xmlChar* URI,
+                       int nb_namespaces, const xmlChar** namespaces, int nb_attributes, int nb_defaulted,
+                       const xmlChar** attributes) {
 
   std::string node = "<";
 
-  
-
   if(prefix) {
 
-    fprintf(stderr, "%s:\t%s:%s\t%s\n", __FUNCTION__, prefix, localname, URI);
-  } else {
-
-    fprintf(stderr, "%s:\t%s\t%s\n", __FUNCTION__, localname, URI);
+    node += (const char *)prefix;
+    node += ":";
   }
 
-  if(nb_namespaces) {
-    fprintf(stderr, "\t\tNamespaces:\n");
+  node += (const char *)localname;
+
+  if(strcmp((const char *)localname, "unit") == 0 && nb_namespaces) {
 
     int index;
     for(int i = 0, index = 0; i < nb_namespaces; ++i, index += 2)
-      if(namespaces[i])
-        fprintf(stderr, "\t\t\t%d. %s:%s\n", (i + 1) / 2 + 1, namespaces[index], namespaces[index + 1]);
-      else
-        fprintf(stderr, "\t\t\t%d. %s\n", (i + 1) / 2 + 1, namespaces[index + 1]);
+      if(namespaces[i]) {
+
+        node += (const char *)namespaces[index];
+        node += ":";
+      }
+
+    node += (const char *)namespaces[index + 1];
 
   }
 
   if(nb_attributes) {
-    fprintf(stderr, "\t\tAttributes:\n");
 
     int index;
     for(int i = 0, index = 0; i < nb_attributes; ++i, index += 5) {
@@ -141,18 +130,47 @@ void output_start_node() {
       int end = attributes[index + 4] - attributes[index + 3];
       char * value = strndup((const char *)attributes[index + 3], end);
 
-      if(attributes[index + 1])
-        fprintf(stderr, "\t\t\t%d. %s:%s=%s", i + 1, attributes[index + 1], attributes[index], value);
-      else
-        fprintf(stderr, "\t\t\t%d. %s=%s", i + 1, attributes[index], value);
+      if(attributes[index + 1]) {
 
+        node += (const char *)attributes[index + 1];
+        node += (const char *)":";
+      }
+
+      node += (const char *)attributes[index];
+
+      node += (const char *)"=\"";
+  
+      node += (const char *)value;
+
+      node += (const char *)"\"";
+
+      /*
       if(attributes[index + 2])
         fprintf(stderr, "\t%s\n", attributes[index + 2]);
       else
         fprintf(stderr, "\n");
+      */
 
     }
 
   }
+
+      node += (const char *)">";
+
+}
+
+void output_end_node(void *ctx, const xmlChar *localname, const xmlChar *prefix, const xmlChar *UR) {
+
+  std::string node = "</";
+
+  if(prefix) {
+
+    node += (const char *)prefix;
+    node += ":";
+  }
+
+  node += (const char *)localname;
+
+  node += (const char *)">";
 
 }
