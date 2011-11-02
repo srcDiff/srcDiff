d7020955c7d95cf5aaff6991bafec7264977cc91
Michael John Decker <mdecker6@kent.edu>
2011-11-02 19:56:48 +0000

    Fixing stuff
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8967 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/src/SAX2ExtractsrcML.cpp
+++ test/src/SAX2ExtractsrcML.cpp
@@ -108,8 +108,51 @@ void comments(void* ctx, const xmlChar* ch) {
 
 void output_start_node() {
 
-  if(prefix)
+  std::string node = "<";
+
+  
+
+  if(prefix) {
+
     fprintf(stderr, "%s:\t%s:%s\t%s\n", __FUNCTION__, prefix, localname, URI);
-  else
+  } else {
+
     fprintf(stderr, "%s:\t%s\t%s\n", __FUNCTION__, localname, URI);
+  }
+
+  if(nb_namespaces) {
+    fprintf(stderr, "\t\tNamespaces:\n");
+
+    int index;
+    for(int i = 0, index = 0; i < nb_namespaces; ++i, index += 2)
+      if(namespaces[i])
+        fprintf(stderr, "\t\t\t%d. %s:%s\n", (i + 1) / 2 + 1, namespaces[index], namespaces[index + 1]);
+      else
+        fprintf(stderr, "\t\t\t%d. %s\n", (i + 1) / 2 + 1, namespaces[index + 1]);
+
+  }
+
+  if(nb_attributes) {
+    fprintf(stderr, "\t\tAttributes:\n");
+
+    int index;
+    for(int i = 0, index = 0; i < nb_attributes; ++i, index += 5) {
+
+      int end = attributes[index + 4] - attributes[index + 3];
+      char * value = strndup((const char *)attributes[index + 3], end);
+
+      if(attributes[index + 1])
+        fprintf(stderr, "\t\t\t%d. %s:%s=%s", i + 1, attributes[index + 1], attributes[index], value);
+      else
+        fprintf(stderr, "\t\t\t%d. %s=%s", i + 1, attributes[index], value);
+
+      if(attributes[index + 2])
+        fprintf(stderr, "\t%s\n", attributes[index + 2]);
+      else
+        fprintf(stderr, "\n");
+
+    }
+
+  }
+
 }
