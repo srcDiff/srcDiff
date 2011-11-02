ec1e50b22de2555851133417f161aebcd22be3e6
Michael John Decker <mdecker6@kent.edu>
2011-11-02 19:37:12 +0000

    Copied files to extract text for extract source
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@8964 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ test/src/SAX2ExtractsrcML.hpp
@@ -0,0 +1,38 @@
+/*
+  SAX2ExtractSource.hpp
+
+  Michael J. Decker
+  mjd52@zips.uakron.edu
+*/
+
+#ifndef INCLUDED_SAX2EXTRACTSOURCE_HPP
+#define INCLUDED_SAX2EXTRACTSOURCE_HPP
+
+#include <libxml/parser.h>
+
+enum operation { COMMON, DELETE, INSERT };
+
+struct source_diff {
+
+  operation op;
+  std::vector<int> * in_diff;
+};
+
+xmlSAXHandler factory();
+
+void startDocument(void* ctx);
+
+void endDocument(void* ctx);
+
+
+void startElementNs(void* ctx, const xmlChar* localname, const xmlChar* prefix, const xmlChar* URI,
+                    int nb_namespaces, const xmlChar** namespaces, int nb_attributes, int nb_defaulted,
+                    const xmlChar** attributes);
+
+void endElementNs(void *ctx, const xmlChar *localname, const xmlChar *prefix, const xmlChar *URI);
+
+void characters(void* ctx, const xmlChar* ch, int len);
+
+void comments(void* ctx, const xmlChar* ch);
+
+#endif
