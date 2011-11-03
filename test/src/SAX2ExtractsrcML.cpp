a6dd7ea461d122462a8ee63f0e232003bdde3508
Michael John Decker <mdecker6@kent.edu>
2011-11-03 14:43:28 +0000

    Fixing still
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9011 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- test/src/SAX2ExtractsrcML.cpp
+++ test/src/SAX2ExtractsrcML.cpp
@@ -74,7 +74,7 @@ void startElementNs(void* ctx, const xmlChar* localname, const xmlChar* prefix, 
     isempty = true;
     outputend = true;
     if(strcmp((const char *)localname, "unit") == 0)
-      fprintf(stdout, "<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" xmlns:lit=\"http://www.sdml.info/srcML/literal\" xmlns:op=\"http://www.sdml.info/srcML/operator\" xmlns:type=\"http://www.sdml.info/srcML/modifier\" language=\"C\">");
+      fprintf(stdout, "<unit xmlns=\"http://www.sdml.info/srcML/src\" xmlns:cpp=\"http://www.sdml.info/srcML/cpp\" xmlns:lit=\"http://www.sdml.info/srcML/literal\" xmlns:op=\"http://www.sdml.info/srcML/operator\" xmlns:type=\"http://www.sdml.info/srcML/modifier\" language=\"C\"");
     else
       output_start_node(ctx, localname, prefix, URI, nb_namespaces, namespaces, nb_attributes, nb_defaulted, attributes);
   }
