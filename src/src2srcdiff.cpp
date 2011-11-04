1cff70fd9941fb57b0a686f89be7d2b5467348fe
Michael John Decker <mdecker6@kent.edu>
2011-11-04 20:29:48 +0000

    Now should be outputting correctly
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9063 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/src2srcdiff.cpp
+++ src/src2srcdiff.cpp
@@ -1831,8 +1831,6 @@ void compare_many2many(struct reader_buffer * rbuf_old, std::vector<std::vector<
 
           output_handler(rbuf_old, rbuf_new, diff_common_end, COMMON, writer);
       
-      output_file_level(rbuf_old, node_sets_old->at(edits->offset_sequence_one + matches->old_offset)
-                        , rbuf_new, node_sets_new->at(edit_next->offset_sequence_two + matches->new_offset), writer);
     }
     
     else {
