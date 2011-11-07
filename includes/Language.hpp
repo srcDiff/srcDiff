07c398c0be1c2c412fd321889881ba8820531981
Michael John Decker <mdecker6@kent.edu>
2011-11-07 15:50:53 +0000

    Made include directory and moved includes there
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9185 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ includes/Language.hpp
@@ -0,0 +1,113 @@
+/*
+  Language.hpp
+
+  Copyright (C) 2004-2011  SDML (www.sdml.info)
+
+  This file is part of the srcML translator.
+
+  The srcML translator is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The srcML translator is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the srcML translator; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef LANGUAGE_HPP
+#define LANGUAGE_HPP
+
+#include <fnmatch.h>
+#include <cstdio>
+#include <cstring>
+#include <string>
+
+struct pair {
+  const char* s;
+  int n;
+};
+
+class Language {
+
+ public:
+
+  enum Languages {
+    LANGUAGE_C = 1,
+    LANGUAGE_CXX = 2,
+    LANGUAGE_CXX_0X = 4,
+    LANGUAGE_JAVA = 8,
+    LANGUAGE_ASPECTJ = 16,
+    LANGUAGE_CXX_FAMILY = LANGUAGE_CXX | LANGUAGE_CXX_0X,
+    LANGUAGE_C_FAMILY = LANGUAGE_C | LANGUAGE_CXX_FAMILY,
+    LANGUAGE_JAVA_FAMILY = LANGUAGE_JAVA | LANGUAGE_ASPECTJ,
+    LANGUAGE_OO = LANGUAGE_CXX_FAMILY | LANGUAGE_JAVA_FAMILY,
+    LANGUAGE_NONE = 32,
+    LANGUAGE_ALL = ~0,
+  };
+
+  Language(int lang)
+    : language(lang) {
+
+  }
+
+  static bool filledLang();
+
+  // gets the current language
+  inline bool inLanguage(int l) const {
+    return (l & language) > 0;
+  }
+
+  // gets the current language
+  inline int getLanguage() const {
+    return language;
+  }
+
+  // gets the current language
+  static int getLanguage(const char* const s) {
+
+    for (const pair* pos = lang2int; pos->s != 0; ++pos)
+      if (strcmp(pos->s, s) == 0)
+	return pos->n;
+
+    return 0;
+  }
+
+  // gets the current language
+  const char* getLanguageString() const {
+
+    for (const pair* pos = lang2int; pos->s != 0; ++pos)
+      if (pos->n == language)
+	return pos->s;
+
+    return "";
+  }
+
+  static bool registerUserExt(const char* ext, int language);
+  static bool registerUserExt(const char* ext, const char* language);
+
+  // gets the current language based on the extenstion
+  static int getLanguageFromFilename(const char* const path);
+
+  // register the standard language file extensions
+  static void register_standard_file_extensions();
+
+  ~Language() {}
+
+ private:
+
+  const int language;
+
+  static pair lang2int[];
+
+  static pair ext2int[];
+
+  static pair userext2int[];
+};
+
+#endif
