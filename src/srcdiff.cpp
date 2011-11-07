1ff8f6b5e3e3ca8c4118735b2f84405a97663dfc
Michael John Decker <mdecker6@kent.edu>
2011-11-07 23:27:02 +0000

    Fix infinite loop
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9234 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -920,7 +920,7 @@ std::vector<std::vector<xmlNodePtr> *> * create_comment_paragraph_set(std::vecto
     std::vector <xmlNode *> * node_set = new std::vector <xmlNode *>;
 
     bool first_newline = false;
-    while(i < end) {
+    for(; i < end; ++i) {
 
       node_set->push_back(diff_nodes->at(i));
 
