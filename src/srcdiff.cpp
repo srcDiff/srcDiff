fb55beee71eb952fe1a8d0fb3f6396a3478a333b
Michael John Decker <mdecker6@kent.edu>
2011-11-07 20:32:04 +0000

    Reformatted method
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9222 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -1434,7 +1434,10 @@ bool output_peek(struct reader_buffer * rbuf_old, struct reader_buffer * rbuf_ne
 
 void output_recursive(struct reader_buffer * rbuf_old, std::vector<std::vector<xmlNodePtr> *> * node_sets_old
                       , struct reader_buffer * rbuf_new, std::vector<std::vector<xmlNodePtr> *> * node_sets_new
-                      , xmlTextWriterPtr writer) {
+                      , struct edit * edit_script, xmlTextWriterPtr writer) {
+
+  struct edit * edits = edit_script;
+  struct edit * edit_next = edit_script->next;
 
   if(rbuf_old->open_diff->back()->operation != COMMON)
     output_handler(rbuf_old, rbuf_new, diff_common_start, COMMON, writer);
@@ -1446,27 +1449,27 @@ void output_recursive(struct reader_buffer * rbuf_old, std::vector<std::vector<x
   // collect subset of nodes
   std::vector<std::vector<xmlNodePtr> *> * next_node_set_old
     = create_node_set(node_sets_old->at(edits->offset_sequence_one), 1
-  , node_sets_old->at(edits->offset_sequence_one)->size() - 1);
+                      , node_sets_old->at(edits->offset_sequence_one)->size() - 1);
 
-std::vector<std::vector<xmlNodePtr> *> * next_node_set_new
-= create_node_set(node_sets_new->at(edit_next->offset_sequence_two), 1
-                                                                      , node_sets_new->at(edit_next->offset_sequence_two)->size() - 1);
+  std::vector<std::vector<xmlNodePtr> *> * next_node_set_new
+    = create_node_set(node_sets_new->at(edit_next->offset_sequence_two), 1
+                      , node_sets_new->at(edit_next->offset_sequence_two)->size() - 1);
 
-// compare subset of nodes
-if(strcmp((const char *)node_sets_old->at(edits->offset_sequence_one)->at(0)->name, "comment") == 0)
-  output_comment(rbuf_old, next_node_set_old, rbuf_new, next_node_set_new, writer);
- else
-   output_diffs(rbuf_old, next_node_set_old, rbuf_new, next_node_set_new, writer);
+  // compare subset of nodes
+  if(strcmp((const char *)node_sets_old->at(edits->offset_sequence_one)->at(0)->name, "comment") == 0)
+    output_comment(rbuf_old, next_node_set_old, rbuf_new, next_node_set_new, writer);
+  else
+    output_diffs(rbuf_old, next_node_set_old, rbuf_new, next_node_set_new, writer);
 
-output_handler(rbuf_old, rbuf_new,
-               node_sets_old->at(edits->offset_sequence_one)->
-               at(node_sets_old->at(edits->offset_sequence_one)->size() - 1)
-               , COMMON, writer);
+  output_handler(rbuf_old, rbuf_new,
+                 node_sets_old->at(edits->offset_sequence_one)->
+                 at(node_sets_old->at(edits->offset_sequence_one)->size() - 1)
+                 , COMMON, writer);
 
-if(rbuf_old->open_diff->back()->operation == COMMON && rbuf_old->open_diff->size() > 1)
-  rbuf_old->open_diff->back()->open_tags->front()->marked = true;
+  if(rbuf_old->open_diff->back()->operation == COMMON && rbuf_old->open_diff->size() > 1)
+    rbuf_old->open_diff->back()->open_tags->front()->marked = true;
 
-output_handler(rbuf_old, rbuf_new, diff_common_end, COMMON, writer);
+  output_handler(rbuf_old, rbuf_new, diff_common_end, COMMON, writer);
 
 }
 
