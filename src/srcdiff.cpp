591fa75efbdf1e9236c6c946decccd8ad78e0b46
Michael John Decker <mdecker6@kent.edu>
2011-11-07 17:45:20 +0000

    Split calls to fit on one line
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9213 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -881,10 +881,13 @@ void output_diffs(struct reader_buffer * rbuf_old, std::vector<std::vector<xmlNo
           output_handler(rbuf_old, rbuf_new, node_sets_old->at(edits->offset_sequence_one)->at(0), COMMON, writer);
 
           // collect subset of nodes
-          std::vector<std::vector<xmlNodePtr> *> * next_node_set_old = create_node_set(node_sets_old->at(edits->offset_sequence_one), 1
-                                                                                       , node_sets_old->at(edits->offset_sequence_one)->size() - 1);
-          std::vector<std::vector<xmlNodePtr> *> * next_node_set_new = create_node_set(node_sets_new->at(edit_next->offset_sequence_two), 1
-                                                                                       , node_sets_new->at(edit_next->offset_sequence_two)->size() - 1);
+          std::vector<std::vector<xmlNodePtr> *> * next_node_set_old
+            = create_node_set(node_sets_old->at(edits->offset_sequence_one), 1
+                              , node_sets_old->at(edits->offset_sequence_one)->size() - 1);
+
+          std::vector<std::vector<xmlNodePtr> *> * next_node_set_new
+            = create_node_set(node_sets_new->at(edit_next->offset_sequence_two), 1
+                              , node_sets_new->at(edit_next->offset_sequence_two)->size() - 1);
 
           // compare subset of nodes
           output_diffs(rbuf_old, next_node_set_old, rbuf_new, next_node_set_new, writer);
