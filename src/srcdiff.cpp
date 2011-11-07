31f60429eb730243b07241e8584a133abbeb8622
Michael John Decker <mdecker6@kent.edu>
2011-11-07 23:56:14 +0000

    Paragraph and line working
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9242 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -1018,12 +1018,12 @@ void output_comment_paragraph(struct reader_buffer * rbuf_old, std::vector<std::
 
         // collect subset of nodes
         std::vector<std::vector<xmlNodePtr> *> * next_node_set_old
-          = create_comment_line_set(node_sets_old->at(edits->offset_sequence_one), 1
-                                    , node_sets_old->at(edits->offset_sequence_one)->size() - 1);
+          = create_comment_line_set(node_sets_old->at(edits->offset_sequence_one), 0
+                                    , node_sets_old->at(edits->offset_sequence_one)->size());
         
         std::vector<std::vector<xmlNodePtr> *> * next_node_set_new
-          = create_comment_line_set(node_sets_new->at(edit_next->offset_sequence_two), 1
-                                    , node_sets_new->at(edit_next->offset_sequence_two)->size() - 1);
+          = create_comment_line_set(node_sets_new->at(edit_next->offset_sequence_two), 0
+                                    , node_sets_new->at(edit_next->offset_sequence_two)->size());
         
         output_comment_line(rbuf_old, next_node_set_old, rbuf_new, next_node_set_new, writer);
 
