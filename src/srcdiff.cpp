93403d145a43babbe77f7d6e48e5b9ab8cb7d367
Michael John Decker <mdecker6@kent.edu>
2011-11-13 21:03:40 +0000

    Fixing more todo
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9481 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -555,9 +555,7 @@ void collect_entire_tag(std::vector<xmlNodePtr> * nodes, std::vector<int> * node
 
   ++(*start);
 
-  // TODO:  No stack is needed.  Just a count of open tags.
   int is_open = 1;
-
   for(; is_open; ++(*start)) {
 
     if(is_white_space(nodes->at(*start)))
@@ -777,7 +775,6 @@ std::vector<std::vector<int> *> create_comment_paragraph_set(std::vector<xmlNode
 
     if(is_new_line(nodes->at(i))) {
 
-      // TODO:  Fix bad for loop formatting
       for(; is_new_line(nodes->at(i)); ++i)
         ;
 
