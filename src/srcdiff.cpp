be2b274a9b2e835340d804c3b602faa0d586a782
Michael John Decker <mdecker6@kent.edu>
2011-11-14 16:11:35 +0000

    Commenting
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9547 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -459,6 +459,7 @@ bool is_atomic_srcml(std::vector<xmlNodePtr> * nodes, unsigned start) {
   return false;
 }
 
+// collect an entire tag from open tag to closing tag
 void collect_entire_tag(std::vector<xmlNodePtr> * nodes, std::vector<int> * node_set, int * start) {
 
   //const char * open_node = (const char *)nodes->at(*start)->name;
@@ -470,17 +471,21 @@ void collect_entire_tag(std::vector<xmlNodePtr> * nodes, std::vector<int> * node
 
   ++(*start);
 
+  // track open tags because could have same type nested
   int is_open = 1;
   for(; is_open; ++(*start)) {
 
+    // skip whitespace
     if(!is_white_space(nodes->at(*start))) {
 
       node_set->push_back(*start);
 
+      // opening tags
       if((xmlReaderTypes)nodes->at(*start)->type == XML_READER_TYPE_ELEMENT
          && !(nodes->at(*start)->extra & 0x1))
         ++is_open;
 
+      // closing tags
       else if((xmlReaderTypes)nodes->at(*start)->type == XML_READER_TYPE_END_ELEMENT)
         --is_open;
 
@@ -491,20 +496,25 @@ void collect_entire_tag(std::vector<xmlNodePtr> * nodes, std::vector<int> * node
   --(*start);
 }
 
+// create the node sets for shortest edit script
 std::vector<std::vector<int> *> create_node_set(std::vector<xmlNodePtr> * nodes, int start, int end) {
 
   std::vector<std::vector<int> *> node_sets;
 
+  // runs on a subset of base array
   for(int i = start; i < end; ++i) {
 
+    // skip whitespace
     if(!is_white_space(nodes->at(i))) {
 
       std::vector <int> * node_set = new std::vector <int>;
 
+      // text is separate node if not surrounded by a tag in range
       if((xmlReaderTypes)nodes->at(i)->type == XML_READER_TYPE_TEXT) {
         //fprintf(stderr, "HERE: %s %s %d '%s'\n", __FILE__, __FUNCTION__, __LINE__, (const char *)nodes->at(i)->content);
         node_set->push_back(i);
 
+        // a base case of sorts
       } else if(is_atomic_srcml(nodes, i)) {
 
         node_set->push_back(i);
@@ -513,12 +523,15 @@ std::vector<std::vector<int> *> create_node_set(std::vector<xmlNodePtr> * nodes,
 
         i += 2;
 
+        // open tags handle here
       } else if((xmlReaderTypes)nodes->at(i)->type == XML_READER_TYPE_ELEMENT) {
 
         collect_entire_tag(nodes, node_set, &i);
 
       } else {
 
+        // could be a closing tag, but then something should be wrong.
+        // TODO: remove this and make sure it works
         node_set->push_back(i);
       }
 
