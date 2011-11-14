4db9be17b338b79323cba7a50680c68853e80a26
Michael John Decker <mdecker6@kent.edu>
2011-11-14 20:26:58 +0000

    Fixing logic
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9621 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -2030,10 +2030,10 @@ void output_nested(struct reader_state rbuf_old, std::vector<int> * structure_ol
 
       if(strcmp((const char *)nodes_old.at(structure_old->at(0))->name, "if") == 0) {
 
-        for(start = 0; start < structure_old->size() && strcmp((const char *)nodes_old.at(structure_old->at(start))->name, "then") != 0; ++start)
+        for(start = 0; strcmp((const char *)nodes_old.at(structure_old->at(start))->name, "then") != 0; ++start)
           ;
 
-        for(end = start + 1; end < structure_old->size() && nodes_old.at(structure_old->at(end))->type == XML_READER_TYPE_END_ELEMENT
+        for(end = start + 1; nodes_old.at(structure_old->at(end))->type == XML_READER_TYPE_END_ELEMENT
               && strcmp((const char *)nodes_old.at(structure_old->at(end))->name, "then") != 0; ++end)
           ;
 
