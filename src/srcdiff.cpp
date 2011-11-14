7cdc0a6bee3799d96620495b95d319e24f731f20
Michael John Decker <mdecker6@kent.edu>
2011-11-14 18:21:26 +0000

    Function prototype made
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9580 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -1913,4 +1913,9 @@ bool is_block_type(std::vector<int> * structure, std::vector<xmlNodePtr> & nodes
   if(nodes.at(structure.at(0))->type != XML_READER_TYPE_ELEMENT)
     return false;
 
+  for(int i = 0; block_types[i]; ++i)
+    if(strcmp((const char *)nodes.at(structure.at(0))->name, block_types[i]) == 0)
+      return true;
+
+  return false;
 }
