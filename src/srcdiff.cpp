8ef8823c176cfdc56cd2e4088898490821e75718
Michael John Decker <mdecker6@kent.edu>
2011-11-14 02:35:04 +0000

    Changing to reference types
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9533 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -298,7 +298,7 @@ int main(int argc, char * argv[]) {
     std::vector<std::vector<int> *> node_set_old = create_node_set(&nodes_old, 0, nodes_old.size());
     std::vector<std::vector<int> *> node_set_new = create_node_set(&nodes_new, 0, nodes_new.size());
 
-    output_diffs(rbuf_old, node_set_old, rbuf_new, &node_set_new, &wstate);
+    output_diffs(rbuf_old, &node_set_old, rbuf_new, &node_set_new, &wstate);
 
     // output srcdiff unit
     outputNode(*getRealCurrentNode(reader_old), wstate.writer);
