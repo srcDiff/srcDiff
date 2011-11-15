a4570070c1f45103186d0a3b302638a54c44c3c1
Michael John Decker <mdecker6@kent.edu>
2011-11-15 18:00:04 +0000

    Fixed something
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9673 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -599,10 +599,10 @@ void output_common(struct reader_state & rbuf_old, unsigned int end_old
   // output common nodes
   markup_whitespace(rbuf_old, oend, rbuf_new, nend, wstate);
 
+  output_white_space(rbuf_old, rbuf_new, wstate);
+
   // output common tag if needed
   output_node(rbuf_old, rbuf_new, &diff_common_end, COMMON, wstate);
-
-  output_white_space(rbuf_old, rbuf_new, wstate);
 
 }
 
