6d26ac1de7d5196b2073d8334e7e538ff7945d06
Michael John Decker <mdecker6@kent.edu>
2011-11-15 17:51:08 +0000

    Fixing whitespace
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9671 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -2074,7 +2074,7 @@ void output_nested(struct reader_state & rbuf_old, std::vector<int> * structure_
       
       output_diffs(rbuf_old, &next_node_set_old, rbuf_new, &next_node_set_new, wstate);
 
-    output_white_space(rbuf_old, rbuf_new, wstate);
+      output_white_space(rbuf_old, rbuf_new, wstate);
 
       // could output change here instead
       for(unsigned int i = end_pos; i < (structure_old->back() + 1); ++i)
