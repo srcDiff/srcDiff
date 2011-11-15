4329e350dbb8e9f698244b123cffd5afeb62f319
Michael John Decker <mdecker6@kent.edu>
2011-11-15 21:27:55 +0000

    Added another whitespace output
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9723 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -1583,20 +1583,10 @@ void output_recursive(struct reader_state & rbuf_old, std::vector<std::vector<in
   }
 
   output_common(rbuf_old, node_sets_old->at(start_old)->back() + 1, rbuf_new, node_sets_new->at(start_new)->back() + 1, wstate);
-  //markup_whitespace(rbuf_old, node_sets_old->at(start_old)->back() + 1, rbuf_new, node_sets_new->at(start_new)->back() + 1, wstate);
-
-  /*
-    output_node(rbuf_old, rbuf_new,
-    nodes_old.at(node_sets_old->at(start_old)->
-    at(node_sets_old->at(start_old)->size() - 1))
-    , COMMON, wstate);
-
-    ++rbuf_old.last_output;
-    ++rbuf_new.last_output;
-  */
-
 
   output_node(rbuf_old, rbuf_new, &diff_common_end, COMMON, wstate);
+
+  output_white_space(rbuf_old, rbuf_new, wstate);
 
 }
 
