0ce7f26c93392a13710384dc153b9f874e373da9
Michael John Decker <mdecker6@kent.edu>
2011-11-15 18:07:37 +0000

    Forked to have whitespace all and whitespace statment (needs rename
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9676 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -570,10 +570,10 @@ void output_white_space(struct reader_state & rbuf_old
   for(; nend < nodes_new.size() && is_white_space(nodes_new.at(nend)) && !is_new_line(nodes_new.at(nend)); ++nend)
     ;
 
-  if(oend < nodes_old.size() && is_new_line(nodes_old.at(oend))
+  if(oend < nodes_old.size() && is_new_line(nodes_old.at(oend)))
      ++oend;
 
-  if(nend < nodes_new.size() && is_new_line(nodes_new.at(nend))
+  if(nend < nodes_new.size() && is_new_line(nodes_new.at(nend)))
      ++nend;
 
   markup_whitespace(rbuf_old, oend, rbuf_new, nend, wstate);
