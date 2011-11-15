bdaea2489ab6cd34a4d6ec7f1b0983334c5c91e3
Michael John Decker <mdecker6@kent.edu>
2011-11-15 21:44:33 +0000

    More use of whitespace
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9734 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -2133,7 +2133,8 @@ void output_nested(struct reader_state & rbuf_old, std::vector<int> * structure_
     rbuf_old.last_output = structure_old->back() + 1;
     //rbuf_new.last_output = structure_new->back() + 1;
 
-    markup_whitespace(rbuf_old, rbuf_old.last_output, rbuf_new, rbuf_new.last_output, wstate);
+    output_white_space_all(rbuf_old, rbuf_new, wstate);
+    //markup_whitespace(rbuf_old, rbuf_old.last_output, rbuf_new, rbuf_new.last_output, wstate);
 
   } else {
 
