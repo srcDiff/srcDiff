7d3b5f48a0521d145065d0beda4ac25ac192a2ca
Michael John Decker <mdecker6@kent.edu>
2011-11-15 21:54:22 +0000

    Outputting differences before closing delete
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9735 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -2127,13 +2127,15 @@ void output_nested(struct reader_state & rbuf_old, std::vector<int> * structure_
     for(unsigned int i = end_pos; i < (structure_old->back() + 1); ++i)
       output_node(rbuf_old, rbuf_new, nodes_old.at(i), DELETE, wstate);
 
+    rbuf_old.last_output = structure_old->back() + 1;
+
+    output_white_space_all(rbuf_old, rbuf_new, wstate);
+
     // output diff tag begin
     output_node(rbuf_old, rbuf_new, &diff_old_end, DELETE, wstate);
 
-    rbuf_old.last_output = structure_old->back() + 1;
     //rbuf_new.last_output = structure_new->back() + 1;
 
-    output_white_space_all(rbuf_old, rbuf_new, wstate);
     //markup_whitespace(rbuf_old, rbuf_old.last_output, rbuf_new, rbuf_new.last_output, wstate);
 
   } else {
