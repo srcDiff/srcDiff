4dd7c76616a7204d3ea10863824a9c44c8028654
Michael John Decker <mdecker6@kent.edu>
2011-11-16 20:28:23 +0000

    More stuff
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9782 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -1953,8 +1953,14 @@ void output_change(struct reader_state & rbuf_old, unsigned int end_old
 
       for(; offset_old < size_old && offset_new < size_new && content_old[offset_old] == content_new[offset_new]; ++offset_old, ++offset_new)
         ;
-
-      xmlTextWriterWriteRawLen(wstate.writer, content_old, offset_old);
+      
+      xmlNode * text = new xmlNode;
+      text->type = (xmlElementType)XML_READER_TYPE_TEXT;
+      text->name = (const xmlChar *)"text";
+      text->content = (const xmlChar *)strndup((const char *)content_old, offset_old);
+      
+      output_node(rbuf_old, rbuf_new, text, wstate);
+      //xmlTextWriterWriteRawLen(wstate.writer, content_old, offset_old);
 
       if(offset_old < size_old) {
 
