88a943a72236fc53178244171c6ba0d15bd13203
Michael John Decker <mdecker6@kent.edu>
2011-11-16 16:47:41 +0000

    More grouping
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9754 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -1689,7 +1689,10 @@ void markup_whitespace(struct reader_state & rbuf_old, unsigned int end_old, str
           output_node(rbuf_old, rbuf_new, &diff_old_start, DELETE, wstate);
 
 	if(ostart < size_old)
-	  xmlTextWriterWriteRawLen(wstate.writer, content_old + ostart, size_old - ostart);
+	  xmlTextWriterWriteRawLen(wstate.writer, content_old + ostart, size_old - ostart);\
+
+	while(i = i + 1; i < olength; ++i)
+	  output_node(rbuf_old, rbuf_new, nodes_old.at(i), DELETE, wstate);
 
         // output diff tag
         output_node(rbuf_old, rbuf_new, &diff_old_end, DELETE, wstate);
