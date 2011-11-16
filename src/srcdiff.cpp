dc099f67cd527b5e905f7f6287527fc8a172f2ad
Michael John Decker <mdecker6@kent.edu>
2011-11-16 21:50:12 +0000

    Put static back in the function
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9799 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -1338,9 +1338,6 @@ void update_diff_stack(std::vector<struct open_diff *> & open_diffs, xmlNodePtr 
 
 }
 
-static bool delay = false;
-static int delay_operation = COMMON;
-
 void output_node(struct reader_state & rbuf_old, struct reader_state & rbuf_new, xmlNodePtr node, int operation, struct writer_state & wstate) {
 
   /*
@@ -1352,6 +1349,9 @@ void output_node(struct reader_state & rbuf_old, struct reader_state & rbuf_new,
     else
     fprintf(stderr, "HERE: %s %s %d %s\n", __FILE__, __FUNCTION__, __LINE__, (const char *)node->name);
   */
+
+  static bool delay = false;
+  static int delay_operation = COMMON;
 
   // check if delaying DELETE/INSERT/COMMON tag. should only stop if operation is different or not whitespace
   if(delay && delay_operation != operation) {
