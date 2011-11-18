b5a78e55b20bc6d903584e708e8a509ad197ba75
Michael John Decker <mdecker6@kent.edu>
2011-11-18 02:22:17 +0000

    Wrapping prefix in a common
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9901 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -2028,8 +2028,12 @@ void output_white_space_prefix(struct reader_state & rbuf_old
   if(rbuf_new.last_output < nend && (is_white_space(nodes_new.at(nend - 1)) && !is_new_line(nodes_new.at(nend - 1))))
     --nend;
 
+  output_node(rbuf_old, rbuf_new, &diff_common_start, COMMON, wstate);  
+
   for(int i = ostart; ostart < oend; ++i)
     output_node(rbuf_old, rbuf_new, nodes_old.at(i), COMMON, wstate);
+
+  output_node(rbuf_old, rbuf_new, &diff_common_end, COMMON, wstate);  
 
   rbuf_old.last_output = oend;
   rbuf_new.last_output = nend;
