1124efd943993b85af5fe56cfab8f1ca7fb22653
Michael John Decker <mdecker6@kent.edu>
2011-11-18 17:53:17 +0000

    Turned off some processing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@9936 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -1837,11 +1837,11 @@ void markup_whitespace(struct reader_state & rbuf_old, unsigned int end_old, str
       unsigned int olength = i + 1;
       unsigned int nlength = j + 1;
 
-      for(; olength < oend && is_white_space(nodes_old.at(olength)); ++olength)
-        ;
+      //for(; olength < oend && is_white_space(nodes_old.at(olength)); ++olength)
+      //  ;
 
-      for(; nlength < nend && is_white_space(nodes_new.at(nlength)); ++nlength)
-	;
+        //for(; nlength < nend && is_white_space(nodes_new.at(nlength)); ++nlength)
+	//;
 
       if(ostart < size_old || (i + 1) < olength) {
 
