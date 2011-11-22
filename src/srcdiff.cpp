62999603c189ebad33b099dfab08e3883c7032f1
Michael John Decker <mdecker6@kent.edu>
2011-11-22 16:22:00 +0000

    Fixed Makefile
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10271 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -11,6 +11,9 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <Options.hpp>
+#include <srcMLTranslator.hpp>
+#include <Language.hpp>
 
 #include "shortest_edit_script.h"
 #include "srcDiffUtility.hpp"
