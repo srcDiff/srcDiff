c5aa5bb99346da70fb97368d79b1e86a14bab7e5
Michael John Decker <mdecker6@kent.edu>
2011-11-22 04:30:48 +0000

    Added includes
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10235 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffDiff.hpp
+++ src/srcDiffDiff.hpp
@@ -1,6 +1,8 @@
 #ifndef INCLUDED_SRCDIFFDIFF_HPP
 #define INCLUDED_SRCDIFFDIFF_HPP
 
+#include <vector>
+
 // create sets of nodes
 std::vector<std::vector<int> *> create_node_set(std::vector<xmlNodePtr> & nodes, int start, int end);
 
