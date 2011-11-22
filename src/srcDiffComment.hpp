ff84e3b762d729931589cf4ac2510bc8e7463ce3
Michael John Decker <mdecker6@kent.edu>
2011-11-22 02:41:10 +0000

    Made header file
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10195 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/srcDiffComment.hpp
@@ -0,0 +1,6 @@
+std::vector<std::vector<int> *> create_comment_paragraph_set(std::vector<xmlNodePtr> & nodes, int start, int end);
+  std::vector<std::vector<int> *> create_comment_line_set(std::vector<xmlNodePtr> & nodes, int start, int end);
+  void output_comment_paragraph(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
+  void output_comment_line(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
+  void output_comment_word(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
+
