1582049941722d46fc15fbdd604a4a4fb00c0822
Michael John Decker <mdecker6@kent.edu>
2011-11-22 04:01:27 +0000

    Added to header
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10215 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/srcDiffChange.hpp
@@ -0,0 +1,11 @@
+void output_change_white_space(reader_state & rbuf_old, unsigned int end_old
+                               , reader_state & rbuf_new, unsigned int end_new
+                               , writer_state & wstate);
+
+void output_change(reader_state & rbuf_old, unsigned int end_old
+                   , reader_state & rbuf_new, unsigned int end_new
+                   , writer_state & wstate);
+
+void output_pure_operation_white_space(reader_state & rbuf_old, unsigned int end_old
+                                       , reader_state & rbuf_new, unsigned int end_new
+                                       , int operation, writer_state & wstate);
