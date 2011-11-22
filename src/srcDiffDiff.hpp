aa4a8456724d4f39c40b9668f44a1ec999c42ab2
Michael John Decker <mdecker6@kent.edu>
2011-11-22 04:43:12 +0000

    Adding headers
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10247 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffDiff.hpp
+++ src/srcDiffDiff.hpp
@@ -10,4 +10,22 @@ std::vector<std::vector<int> *> create_node_set(std::vector<xmlNodePtr> & nodes,
 
 void collect_entire_tag(std::vector<xmlNodePtr> & nodes, std::vector<int> & node_set, int & start);
 
+void output_diffs(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate) {
+
+  int compute_similarity(std::vector<int> * node_set_old, std::vector<int> * node_set_new);
+
+void match_differences(std::vector<std::vector<int> *> * node_sets_old
+                       , std::vector<std::vector<int> *> * node_sets_new
+                       , edit * edit_script, offset_pair ** matches);
+
+void output_unmatched(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old
+                      , int start_old, int end_old
+                      , reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new
+                      , int start_new, int end_new
+                      , writer_state & wstate);
+
+void compare_many2many(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old
+                       , reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new
+                       , edit * edit_script, writer_state & wstate);
+
 #endif
