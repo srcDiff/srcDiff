c4135cedb17223abf448aee765b2da09248a16a5
Michael John Decker <mdecker6@kent.edu>
2011-11-22 16:40:17 +0000

    Fixed a declaration in header
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10291 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffNested.hpp
+++ src/srcDiffNested.hpp
@@ -12,17 +12,6 @@ void output_nested(reader_state & rbuf_old, std::vector<int> * structure_old
 
 bool is_block_type(std::vector<int> * structure, std::vector<xmlNodePtr> & nodes);
 
-bool is_nest_type(std::vector<int> * structure, std::vector<xmlNodePtr> & nodes) {
-
-  if((xmlReaderTypes)nodes.at(structure->at(0))->type != XML_READER_TYPE_ELEMENT)
-    return false;
-
-  for(int i = 0; nest_types[i]; ++i)
-    if(strcmp((const char *)nodes.at(structure->at(0))->name, nest_types[i]) == 0)
-      return true;
-
-  return false;
-}
-
+bool is_nest_type(std::vector<int> * structure, std::vector<xmlNodePtr> & nodes);
 
 #endif
