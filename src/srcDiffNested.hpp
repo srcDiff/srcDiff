2fa34e15000867239297817d2dacb8ea04ad2fd6
Michael John Decker <mdecker6@kent.edu>
2011-11-22 16:38:01 +0000

    Added include guards
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10288 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffNested.hpp
+++ src/srcDiffNested.hpp
@@ -1,3 +1,6 @@
+#ifndef INCLUDED_SRCDIFFNESTED_HPP
+#define INCLUDED_SRCDIFFNESTED_HPP
+
 bool is_nestable(std::vector<int> * structure_one, std::vector<xmlNodePtr> & nodes_one
                  , std::vector<int> * structure_two, std::vector<xmlNodePtr> & nodes_two);
 
@@ -7,3 +10,4 @@ void output_nested(reader_state & rbuf_old, std::vector<int> * structure_old
                    , reader_state & rbuf_new ,std::vector<int> * structure_new
                    , int operation, writer_state & wstate);
 
+#endif
