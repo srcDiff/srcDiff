ec42767aa7703dd310c45b37ec173348801b0e35
Michael John Decker <mdecker6@kent.edu>
2011-11-22 02:42:00 +0000

    Added include guard
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10196 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffComment.hpp
+++ src/srcDiffComment.hpp
@@ -1,6 +1,16 @@
+#ifndef INCLUDED_SRCDIFFCOMMENT_HPP
+#define INCLUDED_SRCDIFFCOMMENT_HPP
+
 std::vector<std::vector<int> *> create_comment_paragraph_set(std::vector<xmlNodePtr> & nodes, int start, int end);
-  std::vector<std::vector<int> *> create_comment_line_set(std::vector<xmlNodePtr> & nodes, int start, int end);
-  void output_comment_paragraph(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
-  void output_comment_line(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
+
+std::vector<std::vector<int> *> create_comment_line_set(std::vector<xmlNodePtr> & nodes, int start, int end);
+
+void output_comment_paragraph(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::ve
+ctor<int> *> * node_sets_new, writer_state & wstate);
+
+void output_comment_line(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
+
   void output_comment_word(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
+
+#endif
 
