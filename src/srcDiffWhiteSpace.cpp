205d8d929f07490f0d7b76ba8660abe7f8edb35b
Michael John Decker <mdecker6@kent.edu>
2011-11-22 01:52:15 +0000

    Fixing includes
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10156 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffWhiteSpace.cpp
+++ src/srcDiffWhiteSpace.cpp
@@ -1,4 +1,5 @@
 #include "srcDiffWhiteSpace.hpp"
+#include "shorest_edit_script.h"
 
 void output_white_space_nested(reader_state & rbuf_old
                                , reader_state & rbuf_new
