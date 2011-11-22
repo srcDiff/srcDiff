613a5e2fb06f088fb65affb78b07e8793c5266bc
Michael John Decker <mdecker6@kent.edu>
2011-11-22 04:39:14 +0000

    Moving over diff function
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10245 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffDiff.cpp
+++ src/srcDiffDiff.cpp
@@ -83,4 +83,164 @@ std::vector<std::vector<int> *> create_node_set(std::vector<xmlNodePtr> & nodes,
 
 }
 
+/*
+
+  Outputs diff on each level.  First, Common areas as well as inserts and deletes
+  are output directly.  One-to-one matches result in recursion on syntax match and
+  direct output otherwise.  Many-to-many decides to treat parts or all of the
+  change as multiple one-one or deletions followed by insert.
+
+  Whitespace is dis-regarded during the entire process, and is incorporated on
+  output before and after changes/common sections.
+
+*/
+void output_diffs(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate) {
+
+  //fprintf(stderr, "HERE_DOUBLE\n");
+
+  edit * edit_script;
+  int distance = shortest_edit_script(node_sets_old->size(), (void *)node_sets_old, node_sets_new->size(),
+                                      (void *)node_sets_new, node_set_syntax_compare, node_set_index, &edit_script);
+
+  if(distance < 0) {
+
+    fprintf(stderr, "Error with shortest edit script");
+    exit(distance);
+  }
+
+  int last_diff_old = 0;
+  int last_diff_new = 0;
+  int diff_end_old = rbuf_old.last_output;
+  int diff_end_new = rbuf_new.last_output;
+
+  edit * edits = edit_script;
+  for (; edits; edits = edits->next) {
+
+    // determine ending position to output
+    diff_end_old = rbuf_old.last_output;
+    diff_end_new = rbuf_new.last_output;
+    if(edits->operation == DELETE && last_diff_old < edits->offset_sequence_one) {
+
+      diff_end_old = node_sets_old->at(edits->offset_sequence_one - 1)->back() + 1;
+      diff_end_new = node_sets_new->at(last_diff_new + (edits->offset_sequence_one - last_diff_old) - 1)->back() + 1;
+
+    } else if(edits->operation == INSERT && last_diff_old <= edits->offset_sequence_one) {
+
+      diff_end_old = node_sets_old->at(edits->offset_sequence_one)->back() + 1;
+      diff_end_new = node_sets_new->at(last_diff_new + (edits->offset_sequence_one - last_diff_old))->back() + 1;
+    }
+
+    // output area in common
+    output_common(rbuf_old, diff_end_old, rbuf_new, diff_end_new, wstate);
+
+    // detect and change
+    edit * edit_next = edits->next;
+    if(is_change(edits)) {
+
+      //      fprintf(stderr, "HERE\n");
+      
+      // 1-1
+      if(edits->length == edit_next->length && edits->length == 1
+         && (node_sets_old->at(edits->offset_sequence_one)->size() > 1
+             || node_sets_old->at(edits->offset_sequence_one)->size() > 1)) {
+
+        // syntax match
+        if(node_compare(nodes_old.at(node_sets_old->at(edits->offset_sequence_one)->at(0))
+                        , nodes_new.at(node_sets_new->at(edit_next->offset_sequence_two)->at(0))) == 0
+           && (xmlReaderTypes)nodes_old.at(node_sets_old->at(edits->offset_sequence_one)->at(0))->type != XML_READER_TYPE_TEXT) {
+
+          output_recursive(rbuf_old, node_sets_old, edits->offset_sequence_one
+                           , rbuf_new, node_sets_new, edit_next->offset_sequence_two, wstate);
+
+        } else {
+
+          /*
+            if(is_nestable(node_sets_old->at(edits->offset_sequence_one)
+            , nodes_old, node_sets_new->at(edit_next->offset_sequence_two), nodes_new)) {
+
+            //output_nested(rbuf_old, node_sets_old->at(edits->offset_sequence_one), rbuf_new, node_sets_new->at(edit_next->offset_sequence_two)
+            //              , INSERT, wstate);
+            // syntax mismatch
+            output_change_white_space(rbuf_old, node_sets_old->at(edits->offset_sequence_one)->back() + 1
+            , rbuf_new, node_sets_new->at(edit_next->offset_sequence_two)->back() + 1, wstate);
+
+            } else if(is_nestable(node_sets_new->at(edit_next->offset_sequence_two)
+            , nodes_new, node_sets_old->at(edits->offset_sequence_one), nodes_old)) {
+
+            output_nested(rbuf_old, node_sets_old->at(edits->offset_sequence_one), rbuf_new, node_sets_new->at(edit_next->offset_sequence_two)
+            , DELETE, wstate);
+
+            } else {
+          */
+          // syntax mismatch
+          output_change_white_space(rbuf_old, node_sets_old->at(edits->offset_sequence_one)->back() + 1
+                                    , rbuf_new, node_sets_new->at(edit_next->offset_sequence_two)->back() + 1, wstate);
+          //          }
+
+        }
+
+      } else {
+
+        // many to many handling
+        compare_many2many(rbuf_old, node_sets_old, rbuf_new, node_sets_new, edits, wstate);
+
+      }
+
+      // update for common
+      last_diff_old = edits->offset_sequence_one + edits->length;
+      last_diff_new = edit_next->offset_sequence_two + edit_next->length;
+      edits = edits->next;
+
+    } else {
+
+      // handle pure delete or insert
+      switch (edits->operation) {
+
+      case INSERT:
+
+        //fprintf(stderr, "HERE\n");
+        output_pure_operation_white_space(rbuf_old, 0
+                                          , rbuf_new, node_sets_new->at(edits->offset_sequence_two + edits->length - 1)->back() + 1,
+                                          INSERT, wstate);
+
+
+        // update for common
+        last_diff_old = edits->offset_sequence_one + 1;
+        last_diff_new = edits->offset_sequence_two + edits->length;
+
+        break;
+
+      case DELETE:
+
+        //fprintf(stderr, "HERE\n");
+        output_pure_operation_white_space(rbuf_old, node_sets_old->at(edits->offset_sequence_one + edits->length - 1)->back() + 1
+                                          , rbuf_new, 0, DELETE, wstate);
+
+        // update for common
+        last_diff_old = edits->offset_sequence_one + edits->length;
+        last_diff_new = edits->offset_sequence_two + 1;
+
+        break;
+      }
+    }
+
+  }
+
+  // determine ending position to output
+  diff_end_old = rbuf_old.last_output;
+  diff_end_new = rbuf_new.last_output;
+  if(last_diff_old < (signed)node_sets_old->size()) {
+
+    diff_end_old = node_sets_old->back()->back() + 1;
+    diff_end_new = node_sets_new->back()->back() + 1;
+
+  }
+
+  // output area in common
+  output_common(rbuf_old, diff_end_old, rbuf_new, diff_end_new, wstate);
+
+  free_shortest_edit_script(edit_script);
+
+}
+
 
