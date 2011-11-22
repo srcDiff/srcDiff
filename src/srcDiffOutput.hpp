82b912b0d6da36778ee0e0cbe76db88ad7686824
Michael John Decker <mdecker6@kent.edu>
2011-11-22 02:12:35 +0000

    Adding include
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10176 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffOutput.hpp
+++ src/srcDiffOutput.hpp
@@ -1,3 +1,8 @@
+#ifndef INCLUDED_SRCDIFFOUTPUT_HPP
+#define INCLUDED_SRCDIFFOUTPUT_HPP
+
+#include "srcDiffTypes.hpp"
+#include <vector>
 
 void output_node(reader_state & rbuf_old, reader_state & rbuf_new, xmlNodePtr node, int operation, writer_state & wstate);
 
@@ -6,3 +11,4 @@ void update_diff_stack(std::vector<diff_set *> & open_diffs, xmlNodePtr node, in
 
 void output_text_as_node(reader_state & rbuf_old, reader_state & rbuf_new, xmlChar * text, int operation, writer_state & wstate);
 
+#endif
