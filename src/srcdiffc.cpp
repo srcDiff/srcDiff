6ab9e7fc60eef2307711224e438d4e25cb002c20
Michael John Decker <mdecker6@kent.edu>
2011-11-22 20:06:35 +0000

    Moved closing writer
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10386 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiffc.cpp
+++ src/srcdiffc.cpp
@@ -623,7 +623,7 @@ int main(int argc, char* argv[]) {
       // from the full path
       for (int i = input_arg_start; (i  + 1) <= input_arg_end; i += 2) {
 
-
+        fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
         srcdiff_translate(argv[i], argv[i + 1], 0, writer);
 
         /*
@@ -638,9 +638,6 @@ int main(int argc, char* argv[]) {
         */
       }
 
-      // cleanup writer
-      xmlTextWriterEndDocument(writer);
-      xmlFreeTextWriter(writer);
     }
 
     if (count == 0)
@@ -661,6 +658,10 @@ int main(int argc, char* argv[]) {
 #else
 } catch (...) {}
 #endif
+
+      // cleanup writer
+      xmlTextWriterEndDocument(writer);
+      xmlFreeTextWriter(writer);
 
   return exit_status;
 }
