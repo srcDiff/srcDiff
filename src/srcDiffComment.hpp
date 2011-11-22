d5043c6f04691be9e24264e42683916f4e07887a
Michael John Decker <mdecker6@kent.edu>
2011-11-22 02:42:33 +0000

    added includes
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10197 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffComment.hpp
+++ src/srcDiffComment.hpp
@@ -1,6 +1,9 @@
 #ifndef INCLUDED_SRCDIFFCOMMENT_HPP
 #define INCLUDED_SRCDIFFCOMMENT_HPP
 
+#include "srcDiffTypes.hpp"
+#include <vector>
+
 std::vector<std::vector<int> *> create_comment_paragraph_set(std::vector<xmlNodePtr> & nodes, int start, int end);
 
 std::vector<std::vector<int> *> create_comment_line_set(std::vector<xmlNodePtr> & nodes, int start, int end);
@@ -10,7 +13,7 @@ ctor<int> *> * node_sets_new, writer_state & wstate);
 
 void output_comment_line(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
 
-  void output_comment_word(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
+void output_comment_word(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old, reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new, writer_state & wstate);
 
 #endif
 
