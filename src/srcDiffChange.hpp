83bf60d676fbcfdd1d87d9fccd429868f584f826
Michael John Decker <mdecker6@kent.edu>
2011-11-22 04:02:26 +0000

    Now including appropriate file in hpp
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10216 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffChange.hpp
+++ src/srcDiffChange.hpp
@@ -1,3 +1,8 @@
+#ifndef INCLUDED_SRCDIFFCHANGE_HPP
+#define  INCLUDED_SRCDIFFCHANGE_HPP
+
+#include "srcDiffTypes.hpp"
+
 void output_change_white_space(reader_state & rbuf_old, unsigned int end_old
                                , reader_state & rbuf_new, unsigned int end_new
                                , writer_state & wstate);
@@ -9,3 +14,5 @@ void output_change(reader_state & rbuf_old, unsigned int end_old
 void output_pure_operation_white_space(reader_state & rbuf_old, unsigned int end_old
                                        , reader_state & rbuf_new, unsigned int end_new
                                        , int operation, writer_state & wstate);
+
+#endif
