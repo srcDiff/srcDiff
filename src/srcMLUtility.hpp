3f3666d30e268e30b9d8470a2d934d7c9665d0bb
Michael John Decker <mdecker6@kent.edu>
2011-11-22 16:14:50 +0000

    Changed include
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10268 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLUtility.hpp
+++ src/srcMLUtility.hpp
@@ -1,7 +1,7 @@
 #ifndef INCLUDED_SRCMLUTILITY_HPP
 #define INCLUDED_SRCMLUTILITY_HPP
 
-#include "srcMLUtility.hpp"
+#include "srcDiffUtility.hpp"
 
 // converts source code to srcML
 xmlBuffer * translate_to_srcML(const char * source_file, const char * srcml_file, const char * dir);
