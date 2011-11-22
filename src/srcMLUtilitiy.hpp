089218f2b4c057173feed3657d30cf114c86f2ae
Michael John Decker <mdecker6@kent.edu>
2011-11-22 16:07:58 +0000

    Moved translator over to new file
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10263 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLUtilitiy.hpp
+++ src/srcMLUtilitiy.hpp
@@ -1,6 +1,8 @@
 #ifndef INCLUDED_SRCMLUTILITY_HPP
 #define INCLUDED_SRCMLUTILITY_HPP
 
+#include "srcMLUtility.hpp"
+
 // converts source code to srcML
 xmlBuffer * translate_to_srcML(const char * source_file, const char * srcml_file, const char * dir);
 
--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -311,35 +311,6 @@ int main(int argc, char * argv[]) {
   return 0;
 }
 
-// converts source code to srcML
-xmlBuffer* translate_to_srcML(const char * source_file, const char * srcml_file, const char * dir) {
-
-  // register default language extensions
-  Language::register_standard_file_extensions();
-
-  // get language from file extension
-  int language = Language::getLanguageFromFilename(source_file);
-
-  // select basic options
-  OPTION_TYPE options = OPTION_CPP_MARKUP_ELSE | OPTION_CPP | OPTION_XMLDECL | OPTION_XML  | OPTION_LITERAL | OPTION_OPERATOR | OPTION_MODIFIER;
-
-  xmlBuffer* output_buffer = xmlBufferCreate();
-
-  // create translator object
-  srcMLTranslator translator(language, output_buffer, options);
-
-  // set input file (must be done)
-  translator.setInput(source_file);
-
-  // translate file
-  translator.translate(NULL, dir, source_file, NULL, language);
-
-  // close the input file
-  translator.close();
-
-  return output_buffer;
-}
-
 // collect the differnces
 void collect_nodes(std::vector<xmlNode *> * nodes, xmlTextReaderPtr reader) {
 
