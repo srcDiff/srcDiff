19fb0505aae1cd5cef80059fcbd973f9391885bc
Michael John Decker <mdecker6@kent.edu>
2011-11-23 02:24:59 +0000

    More spacing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10408 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiffc.cpp
+++ src/srcdiffc.cpp
@@ -590,11 +590,14 @@ int main(int argc, char* argv[]) {
       xmlTextWriterStartDocument(writer, XML_VERSION, poptions.xml_encoding, XML_DECLARATION_STANDALONE);
 
       if(input_arg_count / 2 > 1)
-        xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<unit>\n\n"));
+        xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("<unit>"));
 
       // translate in batch the input files on the command line extracting the directory and filename attributes
       // from the full path
       for (int i = input_arg_start; (i  + 1) <= input_arg_end; i += 2) {
+
+      if(input_arg_count / 2 > 1)
+        xmlTextWriterWriteRawLen(writer, LITERALPLUSSIZE("\n\n"));
 
 
         srcdiff_translate(argv[i], argv[i + 1], 0, writer);
