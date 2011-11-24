332b53ead34d11298f406230de8d17ef163520c6
Michael John Decker <mdecker6@kent.edu>
2011-11-24 00:17:49 +0000

    Longer translator all added
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10470 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcMLUtility.cpp
+++ src/srcMLUtility.cpp
@@ -30,26 +30,21 @@ void translate_to_srcML(const char * source_file, const char * srcml_file, const
   // close the input file
   translator.close();
 }
+
 void translate_to_srcML(int language, const char* src_encoding, const char* xml_encoding, xmlBuffer* output_buffer, OPTION_TYPE& options,
                 const char* directory, const char* filename, const char* version, const char* uri[], int tabsize) {
-  /*
-  // get language from file extension
-  int language = Language::getLanguageFromFilename(source_file);
-
-  // select basic options
-  OPTION_TYPE options = OPTION_CPP_MARKUP_ELSE | OPTION_CPP | OPTION_XMLDECL | OPTION_XML  | OPTION_LITERAL | OPTION_OPERATOR | OPTION_MODIFIER;
 
   // create translator object
-  srcMLTranslator translator(language, output_buffer, options);
+  srcMLTranslator translator(language, src_encoding, xml_encoding, output_buffer, options, directory, filename, version, uri, tabsize);
 
   // set input file (must be done)
-  translator.setInput(source_file);
+  translator.setInput(filename);
 
   // translate file
-  translator.translate(NULL, dir, source_file, NULL, language);
+  translator.translate(NULL, NULL, filename, NULL, language);
 
   // close the input file
-  translator.close();*/
+  translator.close();
 }
 
 // create srcdiff unit
--- src/srcMLUtility.hpp
+++ src/srcMLUtility.hpp
@@ -7,6 +7,9 @@
 // converts source code to srcML
 void translate_to_srcML(const char * source_file, const char * srcml_file, const char * dir, xmlBuffer* output_buffer);
 
+void translate_to_srcML(int language, const char* src_encoding, const char* xml_encoding, xmlBuffer* output_buffer, OPTION_TYPE& options,
+                        const char* directory, const char* filename, const char* version, const char* uri[], int tabsize);
+
 // create srcdiff unit
 xmlNodePtr create_srcdiff_unit(xmlNodePtr unit_old, xmlNodePtr unit_new);
 
