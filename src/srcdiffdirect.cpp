681be1c0dc104144d764c9b9c824ea1a78c8836f
Michael John Decker <mdecker6@kent.edu>
2011-11-28 19:42:55 +0000

    Now builds
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10619 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiffdirect.cpp
+++ src/srcdiffdirect.cpp
@@ -10,19 +10,13 @@
 
 
 #include <string.h>
-#include "srcDiffTranslator.hpp"
+#include "srcDiffTool.hpp"
 #include <Options.hpp>
 #include <srcMLTranslator.hpp>
 #include <Language.hpp>
 
 #include <libxml/xmlreader.h>
 #include <libxml/xmlwriter.h>
-
-// TODO:  Get the output encoding from the input
-const char* XML_DECLARATION_STANDALONE = "yes";
-const char* XML_VERSION = "1.0";
-
-const char* output_encoding = "UTF-8";
 
 int main(int argc, char * argv[]) {
 
@@ -33,47 +27,13 @@ int main(int argc, char * argv[]) {
     return 1;
   }
 
-  int is_srcML = strcmp(argv[1], "--srcml") == 0;
-
-  char * filename_one;
-  char * filename_two;
-  if(!is_srcML) {
-    filename_one = argv[1];
-    filename_two = argv[2];
-  } else {
-    filename_one = argv[2];
-    filename_two = argv[3];
-
-  }
-    
-
   const char * srcdiff_file = "-";
 
   Language::register_standard_file_extensions();
 
-  /*
+  OPTION_TYPE options = OPTION_CPP_MARKUP_ELSE | OPTION_CPP | OPTION_XMLDECL | OPTION_XML  | OPTION_LITERAL | OPTION_OPERATOR | OPTION_MODIFIER;
 
-    Setup output file
+  //srcDiffTool translator = srcDiffTool(1, srcdiff_file, options);
 
-  */
-
-  // create the writer
-  xmlTextWriterPtr writer = NULL;
-  writer = xmlNewTextWriterFilename(srcdiff_file, 0);
-  if (writer == NULL) {
-    fprintf(stderr, "Unable to open file '%s' as XML", srcdiff_file);
-
-    exit(1);
-  }
-
-  // issue the xml declaration
-  xmlTextWriterStartDocument(writer, XML_VERSION, output_encoding, XML_DECLARATION_STANDALONE);
-
-  int status = srcdiff_translate(filename_one, filename_two, is_srcML, writer);
-
-  // cleanup writer
-  xmlTextWriterEndDocument(writer);
-  xmlFreeTextWriter(writer);
-
-  return status;
+  return 0;
 }
