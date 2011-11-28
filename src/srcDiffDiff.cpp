5e7beb3bac14847342daaec1ca3e8492786bc7f6
Michael John Decker <mdecker6@kent.edu>
2011-11-28 16:21:43 +0000

    Fixing compile
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10582 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffDiff.cpp
+++ src/srcDiffDiff.cpp
@@ -177,7 +177,7 @@ void output_diffs(reader_state & rbuf_old, std::vector<std::vector<int> *> * nod
             if(is_nestable(node_sets_old->at(edits->offset_sequence_one)
             , nodes_old, node_sets_new->at(edit_next->offset_sequence_two), nodes_new)) {
 
-	      output_nested(rbuf_old, node_sets_old->at(edits->offset_sequence_one), rbuf_new, node_sets_new->at(edit_next->offset_sequence_two);
+	      output_nested(rbuf_old, node_sets_old->at(edits->offset_sequence_one), rbuf_new, node_sets_new->at(edit_next->offset_sequence_two));
             //              , INSERT, wstate);
             // syntax mismatch
             //output_change_white_space(rbuf_old, node_sets_old->at(edits->offset_sequence_one)->back() + 1
