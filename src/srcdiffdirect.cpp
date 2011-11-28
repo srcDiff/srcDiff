6c5b8328eb382d98926396ddf743108afa89fd2a
Michael John Decker <mdecker6@kent.edu>
2011-11-28 20:21:52 +0000

    Passing uri
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10626 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiffdirect.cpp
+++ src/srcdiffdirect.cpp
@@ -31,13 +31,26 @@ int main(int argc, char * argv[]) {
 
   Language::register_standard_file_extensions();
 
-  OPTION_TYPE options = OPTION_CPP_MARKUP_ELSE | OPTION_CPP | OPTION_XMLDECL | OPTION_XML;// | OPTION_LITERAL | OPTION_OPERATOR | OPTION_MODIFIER;
+  OPTION_TYPE options = OPTION_CPP_MARKUP_ELSE | OPTION_CPP | OPTION_XMLDECL | OPTION_XML | OPTION_LITERAL | OPTION_OPERATOR | OPTION_MODIFIER;
 
   std::string filename = argv[1];
   filename += "|";
   filename += argv[2];
 
-  srcDiffTool translator = srcDiffTool(1, "ISO-8859-1", "UTF-8", srcdiff_file, options, 0, 0, 0, 0, 8);
+  const char * uri[] = {
+
+    0,
+    0,
+    0, 
+    0,
+    0,
+    0, 
+    0, 
+    0, 
+
+  };
+
+  srcDiffTool translator = srcDiffTool(1, "ISO-8859-1", "UTF-8", srcdiff_file, options, 0, 0, 0, uri, 8);
   translator.translate(argv[1], argv[2], options, 0, filename.c_str(), 0, 1);
 
   return 0;
