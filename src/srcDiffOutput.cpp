2001960908ba147e2b4b8088f7eb7e0ce9e0b49b
Michael John Decker <mdecker6@kent.edu>
2011-11-28 22:08:41 +0000

    Turned off debug
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10642 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffOutput.cpp
+++ src/srcDiffOutput.cpp
@@ -17,12 +17,12 @@ void output_node(reader_state & rbuf_old, reader_state & rbuf_new, xmlNodePtr no
   /*
     fprintf(stderr, "HERE: %s %s %d %d\n", __FILE__, __FUNCTION__, __LINE__, operation);
     fprintf(stderr, "HERE: %s %s %d %d\n", __FILE__, __FUNCTION__, __LINE__, rbuf->output_diff.back()->operation);
-  */
 
     if(node->type == XML_READER_TYPE_TEXT)
     fprintf(stderr, "HERE: %s %s %d '%s'\n", __FILE__, __FUNCTION__, __LINE__, (const char *)node->content);
     else
     fprintf(stderr, "HERE: %s %s %d %s\n", __FILE__, __FUNCTION__, __LINE__, (const char *)node->name);
+  */
 
   static bool delay = false;
   static int delay_operation = -2;
