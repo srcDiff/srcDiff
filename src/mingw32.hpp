3f0a73931e94bcbdd8a212dd50703f03aac838a0
Michael John Decker <mdecker6@kent.edu>
2011-11-28 16:02:34 +0000

    Changed to just have header in hpp
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10571 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/mingw32.hpp
+++ src/mingw32.hpp
@@ -4,15 +4,7 @@
 #include <string.h>
 #include <io.h>
 
-char * strndup(const char * s1, size_t n) {
+char * strndup(const char * s1, size_t n);
 
-  char * dest = (char *)malloc(n * sizeof(char) + 1);
-
-  memcpy(dest, s1, n);
-
-  dest[n] = 0;
-
-  return dest;
-}
 #endif
 
