fec4a212cd9fb4fbcae46e831f56a486eeb5e4cb
Michael John Decker <mdecker6@kent.edu>
2011-11-28 22:07:54 +0000

    Correcting condition
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10641 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffOutput.cpp
+++ src/srcDiffOutput.cpp
@@ -38,9 +38,12 @@ void output_node(reader_state & rbuf_old, reader_state & rbuf_new, xmlNodePtr no
 
   // check if delaying DELETE/INSERT/COMMON tag. should only stop if operation is different or not whitespace
   if(delay && (delay_operation != operation)
-     && ((delay_operation == DELETE &&strcmp((const char *)wstate.output_diff.back()->open_tags.back()->name, (const char *)diff_old_end.name) == 0)
-         (delay_operation == INSERT && strcmp((const char *)wstate.output_diff.back()->open_tags.back()->name, (const char *)diff_new_end.name) == 0)
-         (delay_operation == COMMON && strcmp((const char *)wstate.output_diff.back()->open_tags.back()->name, (const char *)diff_common_end.name) == 0)) {
+     && ((delay_operation == DELETE 
+          && strcmp((const char *)wstate.output_diff.back()->open_tags.back()->name, (const char *)diff_old_end.name) == 0)
+         || (delay_operation == INSERT 
+             && strcmp((const char *)wstate.output_diff.back()->open_tags.back()->name, (const char *)diff_new_end.name) == 0)
+         || (delay_operation == COMMON 
+             && strcmp((const char *)wstate.output_diff.back()->open_tags.back()->name, (const char *)diff_common_end.name) == 0))) {
 
     if(delay_operation == DELETE) {
 
