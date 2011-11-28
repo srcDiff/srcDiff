090224162ac59f268d41ef17d535da9e3bd3cfdc
Michael John Decker <mdecker6@kent.edu>
2011-11-28 18:45:49 +0000

    Fixed something accidently commented out
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10605 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffChange.cpp
+++ src/srcDiffChange.cpp
@@ -69,7 +69,7 @@ void output_change(reader_state & rbuf_old, unsigned int end_old
   if(end_old > begin_old && end_new > begin_new) {
 
     // set attribute to change
-    //diff_type.children = &change;
+    diff_type.children = &change;
     diff_old_start.properties = &diff_type;
     diff_new_start.properties = &diff_type;
 
