c14fa63841737d3e8e3b2357863f1aba28ef4ffd
Michael John Decker <mdecker6@kent.edu>
2011-11-29 16:30:38 +0000

    Freeing in diff tool
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10650 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffTool.cpp
+++ src/srcDiffTool.cpp
@@ -308,6 +308,9 @@ void srcDiffTool::translate(const char* path_one, const char* path_two, OPTION_T
 
   }
 
+  free_node_sets(node_set_old);
+  free_node_sets(node_set_new);
+
   // Because of grouping need to output a common to end grouping
 
   nodes_old.clear();
