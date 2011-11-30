7121278b0a926b4f5e67b11c7ff88b83d435cd31
Michael John Decker <mdecker6@kent.edu>
2011-11-30 03:32:58 +0000

    Switched to malloc and free
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10703 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffTool.cpp
+++ src/srcDiffTool.cpp
@@ -326,7 +326,7 @@ void srcDiffTool::translate(const char* path_one, const char* path_two, OPTION_T
 
     if((xmlReaderTypes)nodes_old.at(i)->type == XML_READER_TYPE_TEXT) {
 
-      delete nodes_old.at(i);
+      free(nodes_old.at(i));
 
     }
   }
@@ -335,7 +335,7 @@ void srcDiffTool::translate(const char* path_one, const char* path_two, OPTION_T
 
     if((xmlReaderTypes)nodes_new.at(i)->type == XML_READER_TYPE_TEXT) {
 
-      delete nodes_new.at(i);
+      free(nodes_new.at(i));
 
     }
   }
