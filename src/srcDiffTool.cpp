7d963ec4ad8229a072644536924b862d0a9e80e6
Michael John Decker <mdecker6@kent.edu>
2011-12-01 00:24:57 +0000

    Readded code back for posterity
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10765 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffTool.cpp
+++ src/srcDiffTool.cpp
@@ -278,6 +278,33 @@ void srcDiffTool::translate(const char* path_one, const char* path_two, OPTION_T
   Language l(language);
   startUnit(l.getLanguageString(), local_options, unit_directory, unit_filename, unit_version, uri, writer);
 
+  /*
+  unsigned int i;
+  for(i = 0; i < nodes_old.size() && i < nodes_new.size(); ++i) {
+
+   if(nodes_old.at(i)->type != nodes_new.at(i)->type)
+      break;
+
+    if((xmlReaderTypes)nodes_old.at(i)->type != XML_READER_TYPE_TEXT
+    || (xmlReaderTypes)nodes_old.at(i)->type != XML_READER_TYPE_SIGNIFICANT_WHITESPACE)
+      continue;
+
+    if(strcmp(nodes_old.at(i)->name, nodes_new.at(i)->nmae) != 0)
+      break;
+
+    if(strcmp(nodes_old.at(i)->content, nodes_new.at(i)->content) != 0)
+      break;
+
+  }
+
+  if(i == nodes_old.size() && i == nodes_new.size()) {
+
+    for(i = 0; i < nodes_old.size(); ++i)
+      outputNode(*nodes_old.at(i), wstate.writer);
+
+
+  } else {
+  */
   // create srcdiff unit
   //xNodePtr unit = create_srcdiff_unit(unit_old, unit_new);
 
@@ -301,6 +328,8 @@ void srcDiffTool::translate(const char* path_one, const char* path_two, OPTION_T
 
   free_node_sets(node_set_old);
   free_node_sets(node_set_new);
+
+  // } 
 
   if(isoption(global_options, OPTION_NESTED)) {
 
