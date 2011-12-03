65ecce854172e57afa43de738e11ef33dc8c3f4e
Michael John Decker <mdecker6@kent.edu>
2011-12-03 04:06:41 +0000

    Corrected usage
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@10823 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcdiff.cpp
+++ src/srcdiff.cpp
@@ -157,7 +157,7 @@ using namespace LanguageName;
 
 // output help
 void output_help(const char* name) {
-  printf( "Usage: %s [options] <src_infile>... [-o <srcDiff_outfile>]\n\n"
+  printf( "Usage: %s [options] <old_src_infile new_src_infile>... [-o <srcDiff_outfile>]\n\n"
 
           "Translates C, C++, and Java source code into the XML source-code representation srcDiff.\n"
           "Input can be from standard input, a file, a directory, or an archive file, i.e., tar, cpio, and zip.\n"
