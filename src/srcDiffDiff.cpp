e220171b6f44f90b5b068e307cdf0c6568863657
Michael John Decker <mdecker6@kent.edu>
2011-12-11 03:38:07 +0000

    Refined truth value
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11082 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffDiff.cpp
+++ src/srcDiffDiff.cpp
@@ -137,7 +137,7 @@ bool go_down_a_level(reader_state & rbuf_old, std::vector<std::vector<int> *> * 
   if(size_new > max_length)
     max_length = size_new;
 
-  return 3 * similarity < max_length;
+  return 3 * similarity <= max_length;
 
 }
 
