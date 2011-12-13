b3c05660f07d90caff2e406f5f1e57e61b2c1c01
Michael John Decker <mdecker6@kent.edu>
2011-12-13 19:35:50 +0000

    Accidental overwrite
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11211 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2ColorDiff.cpp
+++ src/SAX2ColorDiff.cpp
@@ -57,11 +57,11 @@ void startDocument(void* ctx) {
   std::string span_class = "class=\"";
 
   if(data->in_diff->back() == COMMON)
-    span_class = common_color;
+    span_class += common_color;
   else if(data->in_diff->back() == DELETE)
-    span_class = delete_color;
+    span_class += delete_color;
   else
-    span_class = insert_color;
+    span_class += insert_color;
 
   span_class += "\" class=\"";
 
@@ -70,19 +70,19 @@ void startDocument(void* ctx) {
   if(data->line_old < data->lines_old.size() && data->lines_old.at(data->line_old)
      && data->line_new < data->lines_new.size() && data->lines_new.at(data->line_new)){
 
-      span_out = span_class + diff_color_change;
+    span_out = span_class + diff_color_change;
 
   } else if(data->line_old < data->lines_old.size() && data->lines_old.at(data->line_old)) {
 
-      span_out = span_class + diff_color_change;
+    span_out = span_class + diff_color_change;
 
   } else if(data->line_new < data->lines_new.size() && data->lines_new.at(data->line_new)) {
 
-      span_out = span_class + diff_color_change;
+    span_out = span_class + diff_color_change;
 
   } else {
 
-      span_out = span_class + diff_color_common;
+    span_out = span_class + diff_color_common;
 
   }
 
@@ -152,11 +152,11 @@ void characters(void* ctx, const xmlChar* ch, int len) {
   std::string span_class = "class=\"";
 
   if(data->in_diff->back() == COMMON)
-    span_class = common_color;
+    span_class += common_color;
   else if(data->in_diff->back() == DELETE)
-    span_class = delete_color;
+    span_class += delete_color;
   else
-    span_class = insert_color;
+    span_class += insert_color;
 
   span_class += "\" class=\"";
 
@@ -165,19 +165,19 @@ void characters(void* ctx, const xmlChar* ch, int len) {
   if(data->line_old < data->lines_old.size() && data->lines_old.at(data->line_old)
      && data->line_new < data->lines_new.size() && data->lines_new.at(data->line_new)){
 
-      span_out = span_class + diff_color_change;
+    span_out = span_class + diff_color_change;
 
   } else if(data->line_old < data->lines_old.size() && data->lines_old.at(data->line_old)) {
 
-      span_out = span_class + diff_color_change;
+    span_out = span_class + diff_color_change;
 
   } else if(data->line_new < data->lines_new.size() && data->lines_new.at(data->line_new)) {
 
-      span_out = span_class + diff_color_change;
+    span_out = span_class + diff_color_change;
 
   } else {
 
-      span_out = span_class + diff_color_common;
+    span_out = span_class + diff_color_common;
 
   }
 
@@ -213,44 +213,46 @@ void characters(void* ctx, const xmlChar* ch, int len) {
       }
 
 
-  std::string span_out = span_class;
+      std::string span_out = span_class;
 
-  if(data->line_old < data->lines_old.size() && data->lines_old.at(data->line_old)
-     && data->line_new < data->lines_new.size() && data->lines_new.at(data->line_new)){
+      if(data->line_old < data->lines_old.size() && data->lines_old.at(data->line_old)
+         && data->line_new < data->lines_new.size() && data->lines_new.at(data->line_new)){
 
-      span_out = span_class + diff_color_change;
+        span_out = span_class + diff_color_change;
 
-  } else if(data->line_old < data->lines_old.size() && data->lines_old.at(data->line_old)) {
+      } else if(data->line_old < data->lines_old.size() && data->lines_old.at(data->line_old)) {
 
-      span_out = span_class + diff_color_change;
+        span_out = span_class + diff_color_change;
 
-  } else if(data->line_new < data->lines_new.size() && data->lines_new.at(data->line_new)) {
+      } else if(data->line_new < data->lines_new.size() && data->lines_new.at(data->line_new)) {
 
-      span_out = span_class + diff_color_change;
+        span_out = span_class + diff_color_change;
 
-  } else {
+      } else {
 
-      span_out = span_class + diff_color_common;
+        span_out = span_class + diff_color_common;
 
-  }
+      }
 
-  span_out += "\"";
+      span_out += "\"";
 
-  // clear color before output line
-  fprintf(stdout, "</span><span class=\"%s\">", normal_color);
-  fprintf(stdout, "%c", (char)'\n');
-  fprintf(stdout, "</span><span %s>%d-%d\t", span_out.c_str(), data->line_old, data->line_new);
-    
+      // clear color before output line
+      fprintf(stdout, "</span><span class=\"%s\">", normal_color);
+      fprintf(stdout, "%c", (char)'\n');
+      fprintf(stdout, "</span><span %s>%d-%d\t", span_out.c_str(), data->line_old, data->line_new);
+
     }
 
-  if(data->in_diff->back() == COMMON)
-    span_class = common_color;
-  else if(data->in_diff->back() == DELETE)
-    span_class = delete_color;
-  else
-    span_class = insert_color;
+    span_class = "class=\"";
 
-  span_class += "\" class=\"";
+    if(data->in_diff->back() == COMMON)
+      span_class += common_color;
+    else if(data->in_diff->back() == DELETE)
+      span_class += delete_color;
+    else
+      span_class += insert_color;
+
+    span_class += "\" class=\"";
 
   }
 
