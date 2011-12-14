497d9b89b3897a5491effbe1ee441bc9f73c32e2
Michael John Decker <mdecker6@kent.edu>
2011-12-14 03:30:37 +0000

    Adding namespace to find
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11245 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/SAX2DiffTrace.cpp
+++ diff_trace/SAX2DiffTrace.cpp
@@ -134,15 +134,24 @@ void SAX2DiffTrace::startElementNs(void* ctx, const xmlChar* localname, const xm
 
     if(tracer.elements.size() > 0 && tracer.diff_stack.back().level == 0) {
 
-      std::map<std::string, int>::iterator pos = tracer.diff_stack.back().children.find(std::string((const char *)localname));
+      std::string tag;
+      if(!prefix || strcmp((const char *)prefix, "") == 0) {
+
+        tag += (const char *)prefix;
+        tag += ":";
+
+      }
+      tag += (const char *)localname;
+
+      std::map<std::string, int>::iterator pos = tracer.diff_stack.back().children.find(tag);
 
       if(pos != tracer.diff_stack.back().children.end()) {
 
-        ++tracer.diff_stack.back().children[std::string((const char *)localname)];
+        ++tracer.diff_stack.back().children[tag];
 
       } else {
 
-        tracer.diff_stack.back().children[std::string((const char *)localname)] = 1;
+        tracer.diff_stack.back().children[tag] = 1;
 
       }
 
