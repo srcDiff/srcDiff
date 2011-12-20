a3a564dd4e3bd13eaa43878dab213b192720b932
Michael John Decker <mdecker6@kent.edu>
2011-12-20 18:36:19 +0000

    NOw have an option to specify to thread
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11460 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffTool.cpp
+++ src/srcDiffTool.cpp
@@ -173,6 +173,12 @@ void srcDiffTool::translate(const char* path_one, const char* path_two, OPTION_T
     
   }
 
+  if(!isoption(global_options, OPTION_THREAD) && is_old != -2 && pthread_join(thread_old, NULL)) {
+
+    is_old = -2;
+
+  }
+
   /*
 
     Input for file two
@@ -194,7 +200,7 @@ void srcDiffTool::translate(const char* path_one, const char* path_two, OPTION_T
 
   }
 
-  if(is_old != -2 && pthread_join(thread_old, NULL)) {
+  if(isoption(global_options, OPTION_THREAD) && is_old != -2 && pthread_join(thread_old, NULL)) {
 
     is_old = -2;
 
