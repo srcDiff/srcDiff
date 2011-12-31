0843bf45662ec189ede184b659a9f23aec4ac8d7
Michael John Decker <mdecker6@kent.edu>
2011-12-31 00:40:52 +0000

    Removed externs
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11606 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffWhiteSpace.cpp
+++ src/srcDiffWhiteSpace.cpp
@@ -4,11 +4,6 @@
 #include "srcDiffCommon.hpp"
 #include "srcDiffOutput.hpp"
 
-
-// global structures defined in main
-extern std::vector<xNode *> nodes_old;
-extern std::vector<xNode *> nodes_new;
-
 // more external variables
 extern xNode diff_common_start;
 extern xNode diff_common_end;
