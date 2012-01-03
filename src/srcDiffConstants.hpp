020eb2a3a680a915fa5f6b46be6661e48c6ebddd
Michael John Decker <mdecker6@kent.edu>
2012-01-03 02:31:07 +0000

    Put in a include guard
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11634 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffConstants.hpp
+++ src/srcDiffConstants.hpp
@@ -1,6 +1,10 @@
+#ifndef INCLUDED_SRCDIFFCONSTANTS_HPP
+#define INCLUDED_SRCDIFFCONSTANTS_HPP
+
 const char * const DIFF_PREFIX = "diff:";
 const char * const DIFF_OLD = "delete";
 const char * const DIFF_NEW = "insert";
 const char * const DIFF_COMMON = "common";
 const char * const DIFF_TYPE = "type";
 
+#endif
