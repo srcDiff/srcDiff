4203f99c4c6aa10f9cf4f8b6a4485effd24caa95
Michael John Decker <mdecker6@kent.edu>
2012-01-05 22:50:45 +0000

    Made generic run command
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11721 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/multi_xpath_extract.py
+++ diff_trace/multi_xpath_extract.py
@@ -5,10 +5,10 @@ import subprocess
 
 def run(command, inputs) :
 
-    process = subprocess.Popen(command, stdin=Popen.stdin, stdout=Popen.stdout, stderr=Popen.stderr)
+    process = subprocess.Popen(command, stdin=subprocess.Popen.stdin, stdout=subprocess.Popen.stdout, stderr=subprocess.Popen.stderr)
 
     output = process.communicate(inputs)[0]
 
     return output
 
-print run(["ls"])
+print run(["ls"], "")
