39de4a1fc6aa38b6ffb10004293ba375a68decba
Michael John Decker <mdecker6@kent.edu>
2012-01-05 23:28:41 +0000

    Forgot to split
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11734 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/test/test_suite.py
+++ diff_trace/test/test_suite.py
@@ -16,7 +16,6 @@ def run(command, inputs) :
 
     return output
 
-
 def create_xpath_results(srcDiff, xpath_list) :
 
     xpath_results = ""
@@ -38,6 +37,6 @@ srcDiff = srcDiff_file.read()
 srcDiff_file.close()
 
 command = [difftrace, sys.argv[1]]
-xpath_list = run(command, "")
+xpath_list = split(run(command, ""), "\n")
 
 print create_xpath_results(srcDiff, xpath_list)
