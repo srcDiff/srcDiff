68b14d17b9ea9ca25d4f634d925c1cd2eb99a6ae
Michael John Decker <mdecker6@kent.edu>
2012-01-05 23:16:25 +0000

    Changed to function
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11728 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/multi_xpath_extract.py
+++ diff_trace/multi_xpath_extract.py
@@ -16,23 +16,28 @@ def run(command, inputs) :
     return output
 
 
-srcDiff_file = open(sys.argv[1], "r")
+def create_xpath_results(srcDiff, xpath_list) :
+
+    xpath_results = ""
+
+    for xpath in xpath_list :
+
+        command = [srcml2src_utility, "--xpath", xpath]
+        
+        results = run(command, srcDiff)
+
+        command = [srcml2src_utility, "--xslt", strip_units]
+
+        xpath_results = xpath_results + run(command, results)
+    
+    return xpath_results
+
+srcDiff_file = open(srcdiff_filename, "r")
 srcDiff = srcDiff_file.read()
 srcDiff_file.close()
 
-xpath_file = open(sys.argv[2], "r")
+xpath_file = open(xpath_filename, "r")
 xpath_list = string.split(xpath_file.read(), "\n")
+xpath_file.close()
 
-xpath_results = ""
-
-for xpath in xpath_list :
-
-    command = [srcml2src_utility, "--xpath", xpath]
-
-    results = run(command, srcDiff)
-
-    command = [srcml2src_utility, "--xslt", strip_units]
-
-    xpath_results = xpath_results + run(command, results)
-    
-print xpath_results
+print create_xpath_results(srcDiff, xpath_list)
