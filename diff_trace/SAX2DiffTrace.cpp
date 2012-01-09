6c090729e506a3b72766c5afab60ce4786f067b9
Michael John Decker <mdecker6@kent.edu>
2012-01-09 18:03:35 +0000

    Correcting append calls
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11896 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/SAX2DiffTrace.cpp
+++ diff_trace/SAX2DiffTrace.cpp
@@ -615,15 +615,15 @@ void SAX2DiffTrace::characters(void* ctx, const xmlChar* ch, int len) {
     if(tracer.diff_stack.back().operation == COMMON) {
 
       tracer.elements.at(tracer.collect_node_pos).signature_name_old.back().append((const char *)ch, (const char *)ch + len);
-      tracer.elements.at(tracer.collect_node_pos).signature_name_new.back()append((const char *)ch, (const char *)ch + len);
+      tracer.elements.at(tracer.collect_node_pos).signature_name_new.back().append((const char *)ch, (const char *)ch + len);
 
     } else if(tracer.diff_stack.back().operation == DELETE) {
 
-      tracer.elements.at(tracer.collect_node_pos).signature_name_old.back()append((const char *)ch, (const char *)ch + len);
+      tracer.elements.at(tracer.collect_node_pos).signature_name_old.back().append((const char *)ch, (const char *)ch + len);
 
     } else  if(tracer.diff_stack.back().operation == INSERT) {
 
-      tracer.elements.at(tracer.collect_node_pos).signature_name_new.back()append((const char *)ch, (const char *)ch + len);
+      tracer.elements.at(tracer.collect_node_pos).signature_name_new.back().append((const char *)ch, (const char *)ch + len);
 
     }
 
