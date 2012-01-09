7b56928f849209ae8f98258b6b78184619c7070d
Michael John Decker <mdecker6@kent.edu>
2012-01-09 18:19:25 +0000

    Now ending collection
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@11904 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/SAX2DiffTrace.cpp
+++ diff_trace/SAX2DiffTrace.cpp
@@ -564,6 +564,9 @@ void SAX2DiffTrace::endElementNs(void *ctx, const xmlChar *localname, const xmlC
   if(strcmp((const char *)URI, "http://www.sdml.info/srcDiff") != 0)
     --tracer.diff_stack.back().level;
 
+  if(tracer.collect && is_end_collect((const char *)localname, (const char *)prefix, tracer.elements.at(tracer.collect_node_pos).name.c_str()))
+    tracer.collect = false;
+
   if(tracer.wait && is_end_collect_and_wait(tracer, (const char *)localname, (const char *)prefix, tracer.elements.at(tracer.collect_node_pos).name.c_str())) {
 
     std::string pre = "";
