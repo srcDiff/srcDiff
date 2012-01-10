f0031a4aa3b44fbcf6c4ce0ee6bb38e7d0a1ab0a
Michael John Decker <mdecker6@kent.edu>
2012-01-10 18:21:48 +0000

    Fixing condition
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12012 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/SAX2DiffTrace.cpp
+++ diff_trace/SAX2DiffTrace.cpp
@@ -585,7 +585,7 @@ void SAX2DiffTrace::endElementNs(void *ctx, const xmlChar *localname, const xmlC
 
   }
 
-  if(tracer.wait && (tracer.collect_node_pos == tracer.elements.size() - 1)) {
+  if(tracer.wait && is_wait((const char *)localname, (const char *)prefix) {
 
     output_missed(tracer);
 
