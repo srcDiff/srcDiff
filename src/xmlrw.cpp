1ad60e0e3a512f4ee1482b6ece8475c1900fe820
Michael John Decker <mdecker6@kent.edu>
2012-01-11 23:37:17 +0000

    Return to normal
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12061 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/xmlrw.cpp
+++ src/xmlrw.cpp
@@ -189,7 +189,7 @@ xNode* getCurrentNode(xmlTextReaderPtr reader, int context) {
   std::map<std::string, xNode*> * starttags = &starttags_old;
   std::map<std::string, xNode*> * endtags = &endtags_old;
   std::vector<std::string> * namelist = &namelist_old;
-  /*
+
   if(context == SESINSERT) {
 
     starttags = &starttags_new;
@@ -197,7 +197,7 @@ xNode* getCurrentNode(xmlTextReaderPtr reader, int context) {
     namelist = &namelist_new;
 
   }
-  */
+
   xmlNode* curnode = xmlTextReaderCurrentNode(reader);
 
   xNode * node = 0;
