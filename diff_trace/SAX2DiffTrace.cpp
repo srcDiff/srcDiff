c7579b18e717cbed735ae00c71a383907aad2dea
Michael John Decker <mdecker6@kent.edu>
2012-01-11 04:06:47 +0000

    Enhancing output
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12030 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/SAX2DiffTrace.cpp
+++ diff_trace/SAX2DiffTrace.cpp
@@ -745,7 +745,7 @@ void SAX2DiffTrace::characters(void* ctx, const xmlChar* ch, int len) {
         if(pos != tracer.collect_node_pos + 1)
           path += "/";
 
-        path += create_string_from_element(tracer.elements.at(pos), next_element, count, tracer.diff_stack.back().operation, tracer.options);
+        //path += create_string_from_element(tracer.elements.at(pos), next_element, count, tracer.diff_stack.back().operation, tracer.options);
 
       }
 
