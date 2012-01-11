b9730c218538e9cd2f8a492b953c5b9a06c5a515
Michael John Decker <mdecker6@kent.edu>
2012-01-11 23:58:09 +0000

    Using only one list
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12064 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/xmlrw.cpp
+++ src/xmlrw.cpp
@@ -121,7 +121,7 @@ xNode * createInternalNode(xmlNode & node) {
 
   xnode->free = false;
 
-  return xnode;
+v  return xnode;
 }
 
 void freeXNode(xNode * node) {
@@ -190,6 +190,7 @@ xNode* getCurrentNode(xmlTextReaderPtr reader, int context) {
   std::map<std::string, xNode*> * endtags = &endtags_old;
   std::vector<std::string> * namelist = &namelist_old;
 
+  /*
   if(context == SESINSERT) {
 
     starttags = &starttags_new;
@@ -197,6 +198,7 @@ xNode* getCurrentNode(xmlTextReaderPtr reader, int context) {
     namelist = &namelist_new;
 
   }
+  */
 
   xmlNode* curnode = xmlTextReaderCurrentNode(reader);
 
