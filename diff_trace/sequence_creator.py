cfbfa7a3246aab3ac51f69fd12145b22fd345cf7
Michael John Decker <mdecker6@kent.edu>
2012-01-13 21:14:23 +0000

    Fixing to return what I want
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12101 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/sequence_creator.py
+++ diff_trace/sequence_creator.py
@@ -4,6 +4,6 @@ import itertools
 
 def power_set(alist):
 
-    return chain.from_iterable(itertools.combinations(alist, i) for i in range(len(alist) +1))
+    return list(itertools.chain.from_iterable(itertools.combinations(alist, i) for i in range(1, len(alist) +1)))
 
 print power_set([1,2,3])
