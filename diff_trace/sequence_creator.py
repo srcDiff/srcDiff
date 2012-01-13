f84ce41bee9d7df422bda8788f62ebec006d1275
Michael John Decker <mdecker6@kent.edu>
2012-01-13 21:13:04 +0000

    Fixing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12099 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/sequence_creator.py
+++ diff_trace/sequence_creator.py
@@ -2,9 +2,8 @@
 
 import itertools
 
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
+def powerset(alist):
+
+    return chain.from_iterable(itertools.combinations(alist, i) for i in range(len(alist) +1))
 
 print power_set([1,2,3])
