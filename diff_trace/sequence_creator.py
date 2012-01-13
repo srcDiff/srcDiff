1fcf897681cdc42da554106a1c8f9a363384a479
Michael John Decker <mdecker6@kent.edu>
2012-01-13 21:11:31 +0000

    Added api powerset
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12097 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/sequence_creator.py
+++ diff_trace/sequence_creator.py
@@ -11,4 +11,9 @@ def power_set(alist) :
 
     return sequence
 
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
 print power_set([1,2,3])
