5db87bf382dd7872d3b0951a46a97ea4ce259529
Michael John Decker <mdecker6@kent.edu>
2012-01-13 21:01:15 +0000

    Writing own powerset
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12090 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/sequence_creator.py
+++ diff_trace/sequence_creator.py
@@ -1,5 +1,16 @@
-import itertools
 
-def power_set(iterable):
+def power_set(alist) :
 
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    return power_set_helper(alist, 0, len(alist))
+
+def power_set_helper(alist, start, num) :
+
+    if start == len(alist) || num == 0
+    return alist
+
+    newlist = alist
+
+    newlist.append(alist, start + 1, num)
+    newlist.append(newlist, start +1, num - 1)
+
+    return newlist
