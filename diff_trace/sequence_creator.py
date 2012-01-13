d6b8a6d3cb41ed1fab519837dbd55ea9a254285d
Michael John Decker <mdecker6@kent.edu>
2012-01-13 21:24:37 +0000

    Creating sets
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12102 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/sequence_creator.py
+++ diff_trace/sequence_creator.py
@@ -2,8 +2,39 @@
 
 import itertools
 
+def split_xpath(xpath) :
+
+    return xpath.split("/")
+
+def sequence_create(sequence, tag) :
+
+    name = get_name(tag)
+    predicates = get_predicates(tag)
+
+    return
+
+def get_predicates(tag) :
+
+    predicates = []
+
+    for tag.find("[") != -1 :
+        
+        predicate = [ tag.find("[") : tag.find("]") + 1 ]
+
+        tag = [ : tag.find("]") + 1 ]
+
+    return predicates
+
+def get_name(tag) :
+
+    offset = tag.find("[") != -1
+    
+    if offset == -1 :
+        return tag
+    else :
+        return tag[: offset]
+
 def power_set(alist):
 
     return list(itertools.chain.from_iterable(itertools.combinations(alist, i) for i in range(1, len(alist) +1)))
 
-print power_set([1,2,3])
