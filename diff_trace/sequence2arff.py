3e19c10cd3d3fa36ed7b444368d8979708cb848e
Michael John Decker <mdecker6@kent.edu>
2012-01-14 21:42:15 +0000

    Writing to create arff format
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12145 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/sequence2arff.py
+++ diff_trace/sequence2arff.py
@@ -22,3 +22,19 @@ def create_sequence_list(num_sequence) :
 
     return range(1, num_sequence + 1)
 
+def write_sequence(arff_file, sequence_list) :
+
+    arff_file.write("@attribute sequence { ")
+
+    sequence_length = len(sequence_list)
+
+    if sequence_length > 0 :
+        arff_file.write(str(sequence_list[0]))
+    
+        for sequence in sequence_list[ 1 : ] :
+
+            arff_file.write(", " + str(sequence))
+
+    arff_file.write(" }\n")
+
+    return
