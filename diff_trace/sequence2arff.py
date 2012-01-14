b9b297de49af8960d5d5dfec0d40e8490afc7192
Michael John Decker <mdecker6@kent.edu>
2012-01-14 21:32:49 +0000

    Writing to create arff format
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12142 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/sequence2arff.py
+++ diff_trace/sequence2arff.py
@@ -2,4 +2,19 @@
 
 import sys
 
+def get_num_sequence(sequence_data) :
+
+    num_sequence = sequence_data[len(sequence_data) - 1].split[0]
+
+    return num_sequence
+
+def collect_structures(sequence_data) :
+
+    structures = []
+
+    for line in sequence_data :
+        structures.append(line.split("\t")[1])
+
+
+    return structures
 
