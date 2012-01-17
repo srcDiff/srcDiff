ee5dc8e2e159be6d3169ebfd32d5a315bbb9ca98
Michael John Decker <mdecker6@kent.edu>
2012-01-17 15:52:07 +0000

    Fixing to use what I found
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12188 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/run_CSPADE.r
+++ diff_trace/run_CSPADE.r
@@ -2,7 +2,7 @@ library(Matrix)
 library(arules)
 library(arulesSequences)
 
-sequence_data <- read_baskets(file = system.file("misc", "test.txt", package = "arulesSequences"), info = c("sequenceID","eventID","SIZE"))
+sequence_data <- read_baskets(/Users/Michael/R/x86_64-apple-darwin11.2.0-library/2.14/arulesSequences/misc/zaki.txt, info = c("sequenceID","eventID","SIZE"))
 as(sequence_data, "data.frame")
 
 results <- cspade(sequence_data, parameter = list(support = 0.4), control = list(verbose = TRUE))
