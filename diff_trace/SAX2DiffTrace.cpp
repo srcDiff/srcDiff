380506da736b556b6e8a8b00c184e3778f5496dc
Michael John Decker <mdecker6@kent.edu>
2012-01-17 22:29:44 +0000

    Marking call and not expr_stmt
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12206 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/SAX2DiffTrace.cpp
+++ diff_trace/SAX2DiffTrace.cpp
@@ -129,8 +129,8 @@ bool SAX2DiffTrace::is_wait(const char * name, const char * prefix) {
   if(strcmp(name, "decl_stmt") == 0)
     return true;
 
-  if(strcmp(name, "expr_stmt") == 0)
-    return true;
+  //if(strcmp(name, "expr_stmt") == 0)
+  //return true;
 
   if(strcmp(name, "call") == 0)
     return true;
@@ -183,11 +183,11 @@ bool SAX2DiffTrace::is_end_wait(const char * name, const char * prefix, const ch
   if(strcmp(name, "decl_stmt") == 0 && strcmp(name, "init") == 0)
     return true;
 
-  if(strcmp(name, "expr_stmt") == 0 && strcmp(name, "expr") == 0)
-    return true;
+  //if(strcmp(name, "expr_stmt") == 0 && strcmp(name, "expr") == 0)
+  //return true;
 
   if(strcmp(name, "call") == 0 && strcmp(name, "argument_list") == 0)
-    return true;
+  return true;
 
 
   //if((strcmp(context, "class_decl") == 0 || strcmp(context, "struct_decl") == 0 || strcmp(context, "union_decl") == 0) && strcmp(name, "block") == 0)
@@ -216,11 +216,11 @@ bool SAX2DiffTrace::is_end_collect(const char * name, const char * prefix, const
   if(strcmp(name, "decl_stmt") == 0 && strcmp(name, "name") == 0)
     return true;
 
-  if(strcmp(name, "expr_stmt") == 0 && strcmp(name, "name") == 0)
-    return true;
+  //if(strcmp(name, "expr_stmt") == 0 && strcmp(name, "name") == 0)
+  //return true;
 
   if(strcmp(name, "call") == 0 && strcmp(name, "name") == 0)
-    return true;
+  return true;
 
   return false;
 }
