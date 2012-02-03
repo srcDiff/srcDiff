b9e42812cf3cb88e068cb9326435038b7a668c74
Michael John Decker <mdecker6@kent.edu>
2012-02-03 00:44:33 +0000

    Commented out uneeded info
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12439 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- diff_trace/run_CSPADE.r
+++ diff_trace/run_CSPADE.r
@@ -3,10 +3,10 @@ library(arules)
 library(arulesSequences)
 
 sequence_data <- read_baskets("out.txt", sep = "[\t]+", info = c("sequenceID", "eventID", "SIZE"))
-as(sequence_data, "data.frame")
+#as(sequence_data, "data.frame")
 
-results <- cspade(sequence_data, parameter = list(support = 0.02, maxsize = 10, maxlen = 10), control = list(verbose = TRUE))
+results <- cspade(sequence_data, parameter = list(support = 0.5, maxsize = 10, maxlen = 10), control = list(verbose = TRUE))
 
-summary(results)
+#summary(results)
 
 as(results, "data.frame")
