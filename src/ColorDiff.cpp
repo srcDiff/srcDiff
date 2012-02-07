bf2ac2e339ce798eac61f50b97353e74eb658475
Michael John Decker <mdecker6@kent.edu>
2012-02-07 22:19:00 +0000

    Fix typing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12525 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/ColorDiff.cpp
+++ src/ColorDiff.cpp
@@ -22,7 +22,7 @@ static xmlParserCtxtPtr createURLParserCtxt(xmlBuffer * srcdiff);
 static void parseDocument(xmlParserCtxtPtr ctxt);
 
 ColorDiff::ColorDiff(xmlBuffer * srcdiff, const char * outfile) 
-  : srcdiff(srcdif), outfile(outfile) {}
+  : srcdiff(srcdiff), outfile(outfile) {}
 
 int ColorDiff::colorize(const char * file_one, const char * file_two) {
 
