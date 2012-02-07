05237f7fd38f92479674d332e1850009d8d45474
Michael John Decker <mdecker6@kent.edu>
2012-02-07 23:05:09 +0000

    Fixing small errors
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12546 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2ColorDiff.cpp
+++ src/SAX2ColorDiff.cpp
@@ -255,7 +255,7 @@ void characters(void* ctx, const xmlChar* ch, int len) {
 
   span_out += "\"";
 
-  data->colordiff_file << "</span><span " << span_out.c_str()) << ">";
+  data->colordiff_file << "</span><span " << span_out.c_str() << ">";
 
   for (int i = 0; i < len; ++i) {
 
@@ -311,7 +311,7 @@ void characters(void* ctx, const xmlChar* ch, int len) {
       // clear color before output line
       data->colordiff_file << "</span><span class=\"" normal_color << "\">";
       data->colordiff_file << << (char)'\n' << data->line_old << "-" <<  data->line_new;
-      data->colordiff_file << "</span><span " <<span_out.c_str()) << ">\t";
+      data->colordiff_file << "</span><span " <<span_out.c_str() << ">\t";
 
     }
 
