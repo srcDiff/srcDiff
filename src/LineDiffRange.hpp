fae4a5fd0bdb86e6c4ab75e37ef69c7ebc0dc84a
Michael John Decker <mdecker6@kent.edu>
2012-02-07 18:21:52 +0000

    Added hpp file
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12452 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/LineDiffRange.hpp
@@ -0,0 +1,30 @@
+/*
+  LineDiffRange.hpp
+
+  Compute range of line diffs
+
+  Michael J. Decker
+  mjd52@zips.uakron.edu
+*/
+
+#include "shortest_edit_script.hpp"
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+#ifndef INCLUDED_LINEDIFFRANGE_HPP
+#define INCLUDED_LINEDIFFRANGE_HPP
+
+int line_compare(const void * line_one, const void * line_two, const void * context);
+
+
+const void * line_accessor(int position, const void * lines, const void * context);
+
+  
+
+std::vector<std::string> read_file(const char file);
+
+void get_line_diff_range(const char * file_one, const char * file_two);
+
+#endif
