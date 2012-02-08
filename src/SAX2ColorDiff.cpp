f3e054b643c20860526b4b96c1b2cc7d28f7b23c
Michael John Decker <mdecker6@kent.edu>
2012-02-08 18:59:56 +0000

    Setting version to filename
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12620 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2ColorDiff.cpp
+++ src/SAX2ColorDiff.cpp
@@ -141,7 +141,7 @@ void startDocument(void* ctx) {
 
   }
 
-  data->colordiff_file << "<div class=\"srcdiff\" version1=\"" << data->file_one << "\" version2=\"" << data->file_two << "\">";
+  data->colordiff_file << "<div class=\"srcdiff\" filename1=\"" << data->file_one << "\" filename2=\"" << data->file_two << "\">";
   data->colordiff_file << "<h1>" << file_name << "</h1>\n";
 
   std::string span_class = "class=\"";
