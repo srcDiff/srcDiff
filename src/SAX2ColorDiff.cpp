0e0c58f10ee260f1f1ad679dd56596ee2d106ef5
Michael John Decker <mdecker6@kent.edu>
2012-02-08 20:19:11 +0000

    forgot to fix
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12639 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2ColorDiff.cpp
+++ src/SAX2ColorDiff.cpp
@@ -107,6 +107,7 @@ void output_start_document(std::ostream & colordiff_file, std::string & css_url)
   colordiff_file << "}\n";
   colordiff_file << "div.key\n";
   colordiff_file << "{\n";
+  colordiff_file << "position:fixed;\n";
   colordiff_file << "right:10px;\n";
   colordiff_file << "left:auto;\n";
   colordiff_file << "}\n";
