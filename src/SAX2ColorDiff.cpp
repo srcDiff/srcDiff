b9fda3fd070671cc610fc4f1eb0938305bc8a360
Michael John Decker <mdecker6@kent.edu>
2012-02-08 18:49:29 +0000

    Fixing spacing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12616 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2ColorDiff.cpp
+++ src/SAX2ColorDiff.cpp
@@ -125,7 +125,7 @@ void output_start_document(std::ostream & colordiff_file, std::string & css_url)
   colordiff_file << ".line\n";
   colordiff_file << "{\n";
   colordiff_file << "\n";
-  colordiff_file << "display:inine-block;\n";
+  colordiff_file << "display:inline-block;\n";
   colordiff_file << "width:4.5em;\n";
   colordiff_file << "text-align:right;\n";
   colordiff_file << "margin-right:1em;\n";
