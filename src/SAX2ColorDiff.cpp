4116b5bee76d93dcd503589d8fb7e36120330a9d
Michael John Decker <mdecker6@kent.edu>
2012-02-08 21:50:44 +0000

    Fixing something
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12661 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2ColorDiff.cpp
+++ src/SAX2ColorDiff.cpp
@@ -169,7 +169,7 @@ void startDocument(void* ctx) {
 
     std::vector<std::string> path_one;
     int start = 0, end = 0;
-    for(; (end = data->file_one.find("/", start)) != std::string::npos; start = end)
+    for(; (end = data->file_one.find("/", start)) != std::string::npos; start = end + 1)
       path_one.push_back(data->file_one.substr(start, end));
 
     path_one.push_back(data->file_one.substr(start));
