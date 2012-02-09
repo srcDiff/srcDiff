ec38d0075133ee16b588f7bdf88105e333e16bc9
Michael L. Collard, Ph.D <collard@uakron.edu>
2012-02-09 13:10:13 +0000

    Add tooltips for part of key
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12698 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2ColorDiff.cpp
+++ src/SAX2ColorDiff.cpp
@@ -136,9 +136,9 @@ void output_start_document(std::ostream & colordiff_file, std::string & css_url)
   colordiff_file << "<body>\n";
 
   colordiff_file << "<div class=\"key\">\n";
-  colordiff_file << "<span class=\"" << diff_color_delete << "\"><cite>diff</cite> Delete</span>";
-  colordiff_file << "<span class=\"" << delete_color << "\"><cite>srcDiff</cite> Delete</span>";
-  colordiff_file << "<span class=\"" << delete_color << " " << diff_color_delete << "\"><cite>diff</cite> &amp; <cite>srcDiff</cite> Delete</span>";
+  colordiff_file << "<span title=\"Line delete background color\" class=\"" << diff_color_delete << "\"><cite>diff</cite> Delete</span>";
+  colordiff_file << "<span title=\"srcDiff delete font color\" class=\"" << delete_color << "\"><cite>srcDiff</cite> Delete</span>";
+  colordiff_file << "<span title=\"Line delete background color with srcDiff delete font color\" class=\"" << delete_color << " " << diff_color_delete << "\"><cite>diff</cite> &amp; <cite>srcDiff</cite> Delete</span>";
   colordiff_file << "<span class=\"" << diff_color_insert << "\"><cite>diff</cite> Insert</span>";
   colordiff_file << "<span class=\"" << insert_color << "\"><cite>srcDiff</cite> Insert</span>";
   colordiff_file << "<span class=\"" << insert_color << " " << diff_color_insert << "\"><cite>diff</cite> &amp; <cite>srcDiff</cite> Insert</span>";
