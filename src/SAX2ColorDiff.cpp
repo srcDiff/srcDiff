b57f3558800ea048c58cbe00ffff7a53412b675a
Michael John Decker <mdecker6@kent.edu>
2012-02-09 18:35:45 +0000

    Added another attribute
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12725 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/SAX2ColorDiff.cpp
+++ src/SAX2ColorDiff.cpp
@@ -56,6 +56,7 @@ void output_start_document(std::ostream & colordiff_file, std::string & css_url)
   colordiff_file << "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"\n";
   colordiff_file << "[\n";
   colordiff_file << "<!ATTLIST div filename1 CDATA #IMPLIED>\n";
+  colordiff_file << "<!ATTLIST div filename2 CDATA #IMPLIED>\n";
   colordiff_file << "]>\n";
   colordiff_file << "<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\"\n>";
 
