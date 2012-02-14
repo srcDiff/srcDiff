16845bfab90bf3b3f2c98badf2a99a47ee5eb6e5
Michael John Decker <mdecker6@kent.edu>
2012-02-14 21:30:55 +0000

    Fixing
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12833 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffNested.cpp
+++ src/srcDiffNested.cpp
@@ -224,7 +224,7 @@ bool is_same_nestable(std::vector<int> *  structure_one, std::vector<xNodePtr> &
   std::vector<std::vector<int> *> node_set = create_node_set(nodes_one, structure_two->at(1), structure_two->back()
                                                              , nodes_two.at(structure_one->at(0)));
 
-  unsigned int match = best_match(nodes_one, node_set, nodes_two, structure_one.at(0), SESDELETE);
+  unsigned int match = best_match(nodes_one, node_set, nodes_two, structure_one->at(0), SESDELETE);
 
   unsigned int match_similarity = compute_similarity(nodes_one, nodes_one, *node_set, node_set.at(match));
 
