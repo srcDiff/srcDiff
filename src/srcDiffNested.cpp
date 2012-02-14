87706c071f0613f83a52c270db8adb4926e78075
Michael John Decker <mdecker6@kent.edu>
2012-02-14 20:42:27 +0000

    Making function
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12820 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffNested.cpp
+++ src/srcDiffNested.cpp
@@ -151,6 +151,13 @@ bool is_same_nestable(std::vector<std::vector<int> * > & structure_one, std::vec
   if(!is_nestable(structure_one, nodes_one, structure_two, nodes_two))
     return false;
 
+    std::vector<std::vector<int> *> node_set = create_node_set(nodes_one, structure_old->at(1), structure_old->back()
+                                                               , nodes_new.at(structure_new->at(0)));
+
+    std::vector<std::vector<int> *> nest_set = create_node_set(nodes_two, structure_new->at(0), structure_new->back() + 1);
+
+    unsigned int match = best_match(nodes_old, node_set, nodes_two, nest_set.at(0), SESDELETE);
+
 }
 
 bool is_nestable(std::vector<int> * structure_one, std::vector<xNodePtr> & nodes_one
