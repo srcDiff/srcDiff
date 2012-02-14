054722963ed6e981e99b380cbbae3fa503c11a1e
Michael John Decker <mdecker6@kent.edu>
2012-02-14 20:52:37 +0000

    Fixing warning
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12827 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffNested.cpp
+++ src/srcDiffNested.cpp
@@ -219,7 +219,7 @@ bool is_same_nestable(std::vector<int> *  structure_one, std::vector<xNodePtr> &
   if(!is_nestable(structure_one, nodes_one, structure_two, nodes_two))
     return false;
 
-  int similarity = compute_similarity(nodes_one, structure_one, nodes_two, structure_two);
+  unsigned int similarity = compute_similarity(nodes_one, structure_one, nodes_two, structure_two);
 
     std::vector<std::vector<int> *> node_set = create_node_set(nodes_one, structure_one->at(1), structure_one->back()
                                                                , nodes_two.at(structure_two->at(0)));
