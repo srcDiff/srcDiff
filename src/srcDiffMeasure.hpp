2f59d582735799f44f2ccceaba9a74064cc74f6e
Michael John Decker <mdecker6@kent.edu>
2012-02-16 21:41:20 +0000

    Copied over hpp
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12931 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/srcDiffMeasure.hpp
@@ -0,0 +1,10 @@
+
+
+int compute_similarity(std::vector<xNodePtr> & nodes_old, std::vector<int> * node_set_old, std::vector<xNodePtr> & nodes_new,
+                       std::vector<int> * node_set_new);
+
+int compute_difference(std::vector<xNodePtr> & nodes_old, std::vector<int> * node_set_old, std::vector<xNodePtr> & nodes_new,
+                       std::vector<int> * node_set_new);
+
+int compute_similarity_old(std::vector<xNodePtr> & nodes_old, std::vector<int> * node_set_old, std::vector<xNodePtr> & nodes_new,
+                       std::vector<int> * node_set_new);
