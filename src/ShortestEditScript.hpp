a245859d33b38dc6baf410d5ddbf6cea4fbc10bb
Michael John Decker <mdecker6@kent.edu>
2012-02-17 20:24:38 +0000

    Adding a few fields
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12961 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/ShortestEditScript.hpp
+++ src/ShortestEditScript.hpp
@@ -1,7 +1,17 @@
 #ifndef INCLUDED_SHORTESTEDITSCRIPT_HPP
 #define INCLUDED_SHORTESTEDITSCRIPT_HPP
 
+#include "shortest_edit_script.hpp"
+
 class ShortestEditScript {
+private:
+
+  edit * edit_script;
+  void * context;
+
+public:
+
+  ~ShortestEditScript();
 
 
 
