9d574a311ddf9aa93fbd8b65e3565553ee4e40bb
Michael John Decker <mdecker6@kent.edu>
2012-02-17 20:02:19 +0000

    Added include guard
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12948 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffMatch.hpp
+++ src/srcDiffMatch.hpp
@@ -1,3 +1,6 @@
+#ifndef INCLUDED_SRCDIFFMATCH_HPP
+#define INCLUDED_SRCDIFFMATCH_HPP
+
 void match_differences_dynamic(std::vector<xNodePtr> & nodes_old, std::vector<std::vector<int> *> * node_sets_old
                                , std::vector<xNodePtr> & nodes_new, std::vector<std::vector<int> *> * node_sets_new
                                , edit * edit_script, offset_pair ** matches);
@@ -9,3 +12,5 @@ void match_differences_dynamic_unordered(std::vector<xNodePtr> & nodes_old, std:
 void match_differences(std::vector<xNodePtr> & nodes_old, std::vector<std::vector<int> *> * node_sets_old
                                , std::vector<xNodePtr> & nodes_new, std::vector<std::vector<int> *> * node_sets_new
                                , edit * edit_script, offset_pair ** matches);
+
+#endif
