d262657f6d39a96923951ede6332916e3b924ff0
Michael John Decker <mdecker6@kent.edu>
2012-02-17 20:39:32 +0000

    Defining constructor
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12966 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/ShortestEditScript.cpp
+++ src/ShortestEditScript.cpp
@@ -1,1 +1,8 @@
 #include "ShortestEditScript.hpp"
+
+
+ShortestEditScript::ShortestEditScript(int (*compare)(void * structure_one, void * structure_two, void * context)
+                                       , void * (*accessor)(int index, void * structure, void * context)
+                                       , void * context) {
+
+}
