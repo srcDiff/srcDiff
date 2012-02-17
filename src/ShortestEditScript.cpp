01bc2b8938659020281276445c5beaf85bcacd79
Michael John Decker <mdecker6@kent.edu>
2012-02-17 20:40:03 +0000

    Changing name
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12967 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/ShortestEditScript.cpp
+++ src/ShortestEditScript.cpp
@@ -1,7 +1,7 @@
 #include "ShortestEditScript.hpp"
 
 
-ShortestEditScript::ShortestEditScript(int (*compare)(void * structure_one, void * structure_two, void * context)
+ShortestEditScript::ShortestEditScript(int (*compare)(void * item_one, void * item_two, void * context)
                                        , void * (*accessor)(int index, void * structure, void * context)
                                        , void * context) {
 
