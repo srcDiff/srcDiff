4876307dca6c595f9a861f1542568ef9eff4f234
Michael John Decker <mdecker6@kent.edu>
2012-02-17 01:50:44 +0000

    Added includes
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12934 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffMeasure.hpp
+++ src/srcDiffMeasure.hpp
@@ -1,6 +1,8 @@
 #ifndef INCLUDED_SRCDIFFMEASURE_HPP
 #define INCLUDED_SRCDIFFMEASURE_HPP
 
+#include <vector>
+
 int compute_similarity(std::vector<xNodePtr> & nodes_old, std::vector<int> * node_set_old, std::vector<xNodePtr> & nodes_new,
                        std::vector<int> * node_set_new);
 
