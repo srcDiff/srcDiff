fad00ee0d26bd573c84173f8dfa7cacdab89fe4f
Michael John Decker <mdecker6@kent.edu>
2012-02-17 20:13:18 +0000

    Fixed include
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@12955 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffMatch.hpp
+++ src/srcDiffMatch.hpp
@@ -3,7 +3,8 @@
 
 #include <vector>
 #include "xmlrw.hpp"
-
+#include "srcDiffDiff.hpp"
+#include "shortest_edit_script.h"
 
 void match_differences_dynamic(std::vector<xNodePtr> & nodes_old, std::vector<std::vector<int> *> * node_sets_old
                                , std::vector<xNodePtr> & nodes_new, std::vector<std::vector<int> *> * node_sets_new
