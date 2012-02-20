a05557ce19ba3591f1415f32c8986979a0f7d33c
Michael John Decker <mdecker6@kent.edu>
2012-02-20 16:37:56 +0000

    Adding object
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@13020 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffCommentDiff.cpp
+++ src/srcDiffCommentDiff.cpp
@@ -351,6 +351,7 @@ void output_comment_word(reader_state & rbuf_old, std::vector<std::vector<int> *
   edit * edit_script;
 
   diff_nodes dnodes = { rbuf_old.nodes, rbuf_new.nodes };
+  ShortestEditScript ses(node_set_syntax_compare, node_set_index, &dnodes);
 
   int distance = shortest_edit_script(node_sets_old->size(), (void *)node_sets_old, node_sets_new->size(),
                                       (void *)node_sets_new, node_set_syntax_compare, node_set_index, &edit_script, &dnodes);
