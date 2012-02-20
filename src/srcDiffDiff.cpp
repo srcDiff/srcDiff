5fa7d2454305e68fec43e163001ea4ec8bc2a898
Michael John Decker <mdecker6@kent.edu>
2012-02-20 16:10:34 +0000

    Switched first to object
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@13006 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffDiff.cpp
+++ src/srcDiffDiff.cpp
@@ -236,7 +236,7 @@ void output_diffs(reader_state & rbuf_old, std::vector<std::vector<int> *> * nod
 
   ShortestEditScript ses(node_set_syntax_compare, node_set_index, &dnodes);
 
-  int distance = ses.compute(node_set_old->size(), (const void *)node_set_old, node_sets_new->size(), (const void *)node_set_new);
+  int distance = ses.compute(node_set_old->size(), (const void *)node_sets_old, node_sets_new->size(), (const void *)node_sets_new);
 
   //int distance = shortest_edit_script(node_sets_old->size(), (void *)node_sets_old, node_sets_new->size(),
   //(void *)node_sets_new, node_set_syntax_compare, node_set_index, &edit_script, &dnodes);
