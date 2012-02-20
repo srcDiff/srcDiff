8102ff6eef874f5af1c0d09de746390e88e7d9e4
Michael John Decker <mdecker6@kent.edu>
2012-02-20 18:30:55 +0000

    Moved check move code
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@13039 f70b1346-9fe7-0310-9bf3-95a0de396e6b

---
+++ src/srcDiffMove.cpp
@@ -0,0 +1,80 @@
+void check_move(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_sets_old
+                       , reader_state & rbuf_new, std::vector<std::vector<int> *> * node_sets_new
+                       , edit * edit_script, writer_state & wstate) {
+      return;
+
+  std::vector<std::vector<int> *> function_old;
+
+  for(unsigned int i = 0; i < node_sets_old->size(); ++i) {
+
+    if(strcmp(rbuf_old.nodes.at(node_sets_old->at(i)->at(0))->name, "function") != 0)
+       continue;
+
+    function_old.push_back(node_sets_old->at(i));
+
+  }
+
+  std::vector<std::vector<int> *> function_new;
+
+  for(unsigned int i = 0; i < node_sets_new->size(); ++i) {
+
+    if(strcmp(rbuf_new.nodes.at(node_sets_new->at(i)->at(0))->name, "function") != 0)
+      continue;
+
+    function_new.push_back(node_sets_new->at(i));
+
+  }
+
+  offset_pair * matches = NULL;
+
+  match_differences_dynamic_unordered(rbuf_old.nodes, &function_old, rbuf_new.nodes, &function_new, edit_script, &matches);
+
+  offset_pair * matches_save = matches;
+
+  static int count = 0;
+
+  int start_count = count;
+
+  for(; matches; matches = matches->next) {
+
+    unsigned int min_size = function_old.at(matches->old_offset)->size();
+    if(function_new.at(matches->new_offset)->size() < min_size)
+      min_size = function_new.at(matches->new_offset)->size();
+
+    //if(compute_similarity(rbuf_old.nodes, function_old.at(matches->old_offset)
+    //                    , rbuf_new.nodes, function_new.at(matches->new_offset)) * 10 > min_size * 5) {
+
+      output_change_white_space(rbuf_old, function_old.at(matches->old_offset)->back() + 1, rbuf_new, rbuf_new.last_output, wstate);
+      ++count;
+
+      //    }
+
+  }
+
+  matches = matches_save;
+
+  for(; matches; matches = matches->next) {
+
+    unsigned int min_size = function_old.at(matches->old_offset)->size();
+    if(function_new.at(matches->new_offset)->size() < min_size)
+      min_size = function_new.at(matches->new_offset)->size();
+
+    //if(compute_similarity(rbuf_old.nodes, function_old.at(matches->old_offset)
+    //                    , rbuf_new.nodes, function_new.at(matches->new_offset)) * 10 > min_size * 9) {
+
+      output_change_white_space(rbuf_old, rbuf_old.last_output, rbuf_new, function_new.at(matches->new_offset)->back() + 1, wstate);
+      ++start_count;
+      //}
+
+  }
+
+  for(; matches_save;) {
+
+    offset_pair * old_match = matches_save;
+    matches_save = matches_save->next;
+    delete old_match;
+
+  }
+
+}
+
