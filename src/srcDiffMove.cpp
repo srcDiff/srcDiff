b7055b399597e6843155579a63afafe45d4ab868
Michael John Decker <mdecker6@kent.edu>
2012-02-20 19:38:09 +0000

    Removing old code
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@13072 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffMove.cpp
+++ src/srcDiffMove.cpp
@@ -34,13 +34,6 @@ void check_move(reader_state & rbuf_old, std::vector<std::vector<int> *> * node_
 
   for(edit * edits = edit_script; edits; edits = edits->next) {
 
-    /*
-      if(is_change(edits)) {
-
-
-      }
-    */
-
     switch(edits->operation) {
 
     case SESINSERT :
