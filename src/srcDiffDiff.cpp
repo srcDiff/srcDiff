e75f26d3d2b120a4321e0ebf890cbb495bbe2a49
Michael John Decker <mdecker6@kent.edu>
2012-02-24 20:56:54 +0000

    Old now recording moves
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@13183 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffDiff.cpp
+++ src/srcDiffDiff.cpp
@@ -473,14 +473,18 @@ void compare_many2many(reader_state & rbuf_old, std::vector<std::vector<int> *> 
 
   std::vector<bool> old_moved;
 
-  for(int i = 0; i < node_sets_old.size(); ++i) {
+  for(int i = 0; i < node_sets_old->size(); ++i) {
 
+    if(node_sets_old->at(i)->at(0)->move)
+      old_moved.push_back(true);
+    else
+      old_moved.push_back(false);
 
   }
 
   std::vector<bool> new_moved;
 
-  for(int i = 0; i < node_sets_new.size(); ++i) {
+  for(int i = 0; i < node_sets_new->size(); ++i) {
 
   } 
 
