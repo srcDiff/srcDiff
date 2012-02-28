179f6765cd8439c1a647b690319cfbe556d0d9e4
Michael John Decker <mdecker6@kent.edu>
2012-02-28 23:39:37 +0000

    Having only check on level 1
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@13249 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffDiff.cpp
+++ src/srcDiffDiff.cpp
@@ -247,7 +247,12 @@ void output_diffs(reader_state & rbuf_old, NodeSets * node_sets_old, reader_stat
     exit(distance);
   }
 
-  mark_moves(rbuf_old, node_sets_old, rbuf_new, node_sets_new, edit_script, wstate);
+  static int depth = 0;
+
+  if(!depth)
+    mark_moves(rbuf_old, node_sets_old, rbuf_new, node_sets_new, edit_script, wstate);
+
+  ++depth;
 
   int last_diff_old = 0;
   int last_diff_new = 0;
