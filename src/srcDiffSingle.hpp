945601d8ff4421ae77c0ec70981adc2db42d873a
Michael John Decker <mdecker6@kent.edu>
2012-02-29 15:57:59 +0000

    Adding include guard
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@13272 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffSingle.hpp
+++ src/srcDiffSingle.hpp
@@ -1,6 +1,10 @@
+#ifndef INCLUDED_SRCDIFFSINGLE_HPP
+#define INCLUDED_SRCDIFFSINGLE_HPP
 
 void output_recursive(reader_state & rbuf_old, NodeSets * node_sets_old
                       , unsigned int start_old
                       , reader_state & rbuf_new, NodeSets * node_sets_new
                       , unsigned int start_new
                       , writer_state & wstate);
+
+#endif
