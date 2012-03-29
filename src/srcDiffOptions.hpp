332a943aa40c655c618d8b88df9e6bf2e74badfa
Michael John Decker <mdecker6@kent.edu>
2012-03-29 19:13:14 +0000

    Fixing options
    git-svn-id: http://calder.sdml.cs.kent.edu/svn/srcDiffPurged/trunk/srcdiff/trunk@13470 f70b1346-9fe7-0310-9bf3-95a0de396e6b

--- src/srcDiffOptions.hpp
+++ src/srcDiffOptions.hpp
@@ -4,25 +4,25 @@
 #include <Options.hpp>
 
 // @srcdiff
-const OPTION_TYPE OPTION_THREAD  = ull(1) << 40;
+const OPTION_TYPE OPTION_THREAD  = ull(1) << 41;
 
 // @srcdiff
-const OPTION_TYPE OPTION_VISUALIZE  = ull(1) << 41;
+const OPTION_TYPE OPTION_VISUALIZE  = ull(1) << 42;
 
 // @srcdiff
-const OPTION_TYPE OPTION_OUTPUTSAME  = ull(1) << 42;
+const OPTION_TYPE OPTION_OUTPUTSAME  = ull(1) << 43;
 
 // @srcdiff
-const OPTION_TYPE OPTION_OUTPUTPURE  = ull(1) << 43;
+const OPTION_TYPE OPTION_OUTPUTPURE  = ull(1) << 44;
 
 // @srcdiff
-const OPTION_TYPE OPTION_CHANGE  = ull(1) << 44;
+const OPTION_TYPE OPTION_CHANGE  = ull(1) << 45;
 
 // @srcdiff
-const OPTION_TYPE OPTION_SRCDIFFONLY  = ull(1) << 45;
+const OPTION_TYPE OPTION_SRCDIFFONLY  = ull(1) << 46;
 
 // @srcdiff
-const OPTION_TYPE OPTION_DIFFONLY  = ull(1) << 46;
+const OPTION_TYPE OPTION_DIFFONLY  = ull(1) << 47;
 
 #endif
 
