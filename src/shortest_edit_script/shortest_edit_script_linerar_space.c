a65e6336af58b7be6244dad4b8d107e2919a1806
Michael John Decker <mdecker6@kent.edu>
2014-05-28 09:38:30 -0400

    Fix compile.

--- src/shortest_edit_script/shortest_edit_script_linerar_space.c
+++ src/shortest_edit_script/shortest_edit_script_linerar_space.c
@@ -21,7 +21,7 @@ struct point {
   int x;
   int y;
 
-}
+};
 
 /*
   Finds the shortest edit script between two sequences.
@@ -37,7 +37,7 @@ struct point {
 int shortest_edit_script_linear_space(int sequence_one_size, const void * sequence_one, int sequence_two_size, const void * sequence_two,
   int compare(const void *, const void *, const void *), const void * accessor(int index, const void *, const void *), struct edit ** edit_script, const void * context) {  return -2; }
 
-int compute_middle_snake(int sequence_one_size, const void * sequence_one, int sequence_two_size, const void * sequence_two, struct point * points,
+int compute_middle_snake(int sequence_one_size, const void * sequence_one, int sequence_two_size, const void * sequence_two, struct point points[2],
   int compare(const void *, const void *, const void *), const void * accessor(int index, const void *, const void *), const void * context) {
 
   // compute delta
@@ -49,13 +49,14 @@ int compute_middle_snake(int sequence_one_size, const void * sequence_one, int s
 
   unsigned int paths_length = center * 2 + 1;
   // allocate arrays to record furthest reaching paths
-  struct point * forward_paths = (struct point *)malloc(sizeof(struct point) * paths_length)
+  struct point * forward_paths = (struct point *)malloc(sizeof(struct point) * paths_length);
   memset(forward_paths, 0, paths_length);
 
   struct point * reverse_paths = (struct point *)malloc(sizeof(struct point) * paths_length);
   size_t path_pos;
+  struct point end_point = { sequence_one_size - 1, sequence_two_size - 1 };
   for(path_pos = 0; path_pos < paths_length; ++path_pos)
-    reverse_paths[path_pos] = { sequence_one_size - 1, sequence_two_size - 1 };
+    reverse_paths[path_pos] = end_point;
 
   int distance;
   for(distance = 0; distance <= center; ++distance ) {
@@ -65,19 +66,23 @@ int compute_middle_snake(int sequence_one_size, const void * sequence_one, int s
 
       int diagonal_pos = diagonal + center;
       int column = forward_paths[diagonal_pos - 1].x;
-      if(diagonal == -distance || (diagonal == D && forward_paths[diagonal_pos - 1].x < forward_paths[diagonal_pos + 1].x))
+      if(diagonal == -distance || (diagonal == distance && forward_paths[diagonal_pos - 1].x < forward_paths[diagonal_pos + 1].x))
         column = forward_paths[diagonal_pos + 1].x;
-      row = column - diagonal;
+      int row = column - diagonal;
 
-      while(column < sequence_one_size && row < sequence_two_size && compare(accessor(column, sequence_one, context), accessor(row, sequence_two, context)) == 0) {
+      while(column < sequence_one_size && row < sequence_two_size && compare(accessor(column, sequence_one, context), accessor(row, sequence_two, context), context) == 0) {
 
         ++column;
         ++row;
 
       }
 
-      if(!is_even && forward_paths[diagonal_pos].x >= reverse_paths[diagonal_pos].y && forward_paths[diagonal_pos].y >= reverse_paths[diagonal_pos].y)
-        return { reverse_paths[diagonal_pos], forward_paths[diagonal_pos] };
+      if(!is_even && forward_paths[diagonal_pos].x >= reverse_paths[diagonal_pos].y && forward_paths[diagonal_pos].y >= reverse_paths[diagonal_pos].y) {
+        points[0] = reverse_paths[diagonal_pos];
+        points[1] = forward_paths[diagonal_pos];
+
+        return 2 * distance - 1;
+      }
 
     }
 
