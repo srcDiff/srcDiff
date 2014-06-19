b0b2b96cbf9046a6bfb29f9ec9b8bd95e492ea03
Michael John Decker <mdecker6@kent.edu>
2014-06-19 11:05:50 -0400

    Small style change.

--- src/translator/srcDiffMatch.cpp
+++ src/translator/srcDiffMatch.cpp
@@ -81,7 +81,8 @@ bool reject_match(int similarity, int difference, int text_old_length, int text_
   if(old_tag != new_tag) return true;
 
   if(old_tag == "name" || old_tag == "expr" || old_tag == "type"
-    || old_tag == "lit:literal" || old_tag == "op:operator" || old_tag == "type:modifier") return false;
+    || old_tag == "lit:literal" || old_tag == "op:operator" || old_tag == "type:modifier")
+    return false;
 
   int min_size = text_old_length < text_new_length ? text_old_length : text_new_length;
   int max_size = text_old_length < text_new_length ? text_new_length : text_old_length;
