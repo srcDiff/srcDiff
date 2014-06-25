e195075db7bf6893c523f4b464e02564dd28356d
Michael John Decker <mdecker6@kent.edu>
2014-06-25 10:13:10 -0400

    Always go into a class/struct/union and access region

--- src/translator/srcDiffUtility.cpp
+++ src/translator/srcDiffUtility.cpp
@@ -336,6 +336,8 @@ bool reject_match(int similarity, int difference, int text_old_length, int text_
   if(old_tag != new_tag) return true;
 
   if(old_tag == "name" || old_tag == "type" || old_tag == "then" || old_tag == "block" || old_tag == "condition" || old_tag == "expr"
+    || old_tag == "class" || old_tag == "struct" || old_tag == "union"
+    || old_tag == "private" || old_tag == "protected" || old_tag == "public"
     || old_tag == "parameter_list" || old_tag == "krparameter_list" || old_tag == "argument_list" || old_tag == "member_list"
     || old_tag == "attribute_list" || old_tag == "association_list" || old_tag == "protocol_list"
     || old_tag == "lit:literal" || old_tag == "op:operator" || old_tag == "type:modifier")
