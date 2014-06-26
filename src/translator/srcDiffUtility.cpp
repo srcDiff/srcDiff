e7230c65ea369d0aff2cbf993db387fb5b2d2539
Michael John Decker <mdecker6@kent.edu>
2014-06-26 11:53:50 -0400

    Default and comment should also be matched.

--- src/translator/srcDiffUtility.cpp
+++ src/translator/srcDiffUtility.cpp
@@ -486,6 +486,7 @@ bool reject_match(int similarity, int difference, int text_old_length, int text_
   if(old_tag != new_tag) return true;
 
   if(old_tag == "name" || old_tag == "type" || old_tag == "then" || old_tag == "block" || old_tag == "condition" || old_tag == "expr"
+    || old_tag == "default" || old_tag == "comment"
     // change to class name
     || old_tag == "class" || old_tag == "struct" || old_tag == "union"
     || old_tag == "private" || old_tag == "protected" || old_tag == "public"
