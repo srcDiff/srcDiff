925ac5ce4d49ffebaa6d7d4341ea8f78a06234bd
Michael John Decker <mdecker6@kent.edu>
2014-07-01 09:02:18 -0400

    Should be locating libraries now.

--- CMake/FindLibSVN.cmake
+++ CMake/FindLibSVN.cmake
@@ -3,8 +3,22 @@
 # LIBSVN_INCLUDE_DIR
 # LIBSVN_LIBRARIES
 
-find_path(LIBSN_INCLUDE_DIR NAMES subvesion-1/svn_types.h PATH_SUFFIXES/subversio-1)
+find_path(LIBSVN_INCLUDE_DIR NAMES subvesion-1/svn_types.h PATH_SUFFIXES/subversio-1)
 
-set(LIBSVN_INCLUDE /usr/local/include/subversion-1;/usr/include/apr-1)
-set(LIBSVN_LIBRARIES /usr/local/lib/libsvn_client-1.dylib;/usr/local/lib/libsvn_fs_util-1.dylib;/usr/local/lib/libsvn_repos-1.dylib;/usr/local/lib/libsvn_delta-1.dylib;/usr/local/lib/libsvn_ra-1.dylib;/usr/local/lib/libsvn_subr-1.dylib;/usr/local/lib/libsvn_diff-1.dylib;/usr/local/lib/libsvn_ra_local-1.dylib;/usr/local/lib/libsvn_wc-1.dylib;/usr/local/lib/libsvn_fs-1.dylib;/usr/local/lib/libsvn_ra_serf-1.dylib;/usr/local/lib/libsvn_fs_fs-1.dylib;/usr/local/lib/libsvn_ra_svn-1.dylib;libapr-1.dylib;libaprutil-1.dylib)
-add_definitions(-DSVN)
+find_library(LIBSVN_CLIENT   NAMES libsvn_client-1)
+find_library(LIBSVN_FS_UTIL  NAMES libsvn_fs_util-1)
+find_library(LIBSVN_REPOS    NAMES libsvn_repos-1)
+find_library(LIBSVN_DELTA    NAMES libsvn_delta-1)
+find_library(LIBSVN_RA       NAMES libsvn_ra-1)
+find_library(LIBSVN_SUBR     NAMES libsvn_subr-1)
+find_library(LIBSVN_DIFF     NAMES libsvn_diff-1)
+find_library(LIBSVN_RA_LOCAL NAMES libsvn_ra_local-1)
+find_library(LIBSVN_WC       NAMES libsvn_wc-1)
+find_library(LIBSVN_FS       NAMES libsvn_fs-1)
+find_library(LIBSVN_RA_SERF  NAMES libsvn_ra_serf-1)
+find_library(LIBSVN_FS_FS    NAMES libsvn_fs_fs-1)
+find_library(LIBSVN_RA_SVN   NAMES libsvn_ra_svn-1)
+
+set(LIBSVN_LIBRARIES ${LIBSVN_CLIENT} ${LIBSVN_FS_UTIL} ${LIBSVN_REPOS} ${LIBSVN_DELTA} ${LIBSVN_RA} ${LIBSVN_SUBR} ${LIBSVN_DIFF}
+ ${LIBSVN_RA_LOCAL} ${LIBSVN_WC} ${LIBSVN_FS} ${LIBSVN_RA_SERF} ${LIBSVN_FS_FS} ${LIBSVN_RA_SVN})
+
