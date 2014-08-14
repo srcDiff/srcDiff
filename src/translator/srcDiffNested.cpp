495ee4dad087bb3ebf52b8e9e4bd8af6192fa54b
Michael John Decker <mdecker6@kent.edu>
2014-08-14 13:35:55 -0400

    Nest into condition.

--- src/translator/srcDiffNested.cpp
+++ src/translator/srcDiffNested.cpp
@@ -50,6 +50,7 @@ const char * const struct_nest_types[]               = { "decl_stmt", "function_
 const char * const union_nest_types[]                = { "decl_stmt", "function_decl",                                                         0 };
 const char * const call_nest_types[]                 = {                                                                                       0 };
 const char * const ternary_nest_types[]              = { "expr",                                                                               0 };
+const char * const condition_nest_types[]            = {                                                                                       0 };
 
 const char * const basic_possible_nest_types[]       = {                                                                                       0 };
 const char * const block_possible_nest_types[]       = { "block", "if", "while", "for", "foreach",                                             0 };
@@ -65,6 +66,7 @@ const char * const struct_possible_nest_types[]      = {                        
 const char * const union_possible_nest_types[]       = {                                                                                       0 };
 const char * const call_possible_nest_types[]        = { "expr", "call", "operator", "literal",                                                0 };
 const char * const ternary_possible_nest_types[]     = { "ternary", "call", "operator", "literal",                                             0 };
+const char * const condition_possible_nest_types[]   = { "expr", "call", "operator", "literal",                                                0 };
   
 // tags that can have something nested in them (incomplete)
 const nest_info nesting[] = {   
@@ -87,6 +89,7 @@ const nest_info nesting[] = {
   { "argument",      call_nest_types,        call_possible_nest_types        },
   { "expr",          call_nest_types,        call_possible_nest_types        },
   { "ternary",       ternary_nest_types,     ternary_possible_nest_types     },
+  { "condition",     condition_nest_types,   condition_possible_nest_types   },
 
   { 0, 0, 0 }
 
