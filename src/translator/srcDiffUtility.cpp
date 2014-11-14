a65c99c43d2eb0b23bc54351e19a0790fd10c9bd
Michael John Decker <mdecker6@kent.edu>
2014-11-14 14:26:04 -0500

    Refine a bit more.  Allowing a bit more difference.

--- src/translator/srcDiffUtility.cpp
+++ src/translator/srcDiffUtility.cpp
@@ -818,13 +818,13 @@ bool reject_similarity(int similarity, int difference, int text_old_length, int 
   int max_size = text_old_length < text_new_length ? text_new_length : text_old_length;
 
   if(min_size <= 2)
-    return 2 * similarity < min_size || (difference > 1.1 * min_size) || difference > max_size;
+    return 2 * similarity < min_size || (difference > 1.25 * min_size) || difference > max_size;
   else if(min_size <= 3)
-    return 3 * similarity < 2 * min_size || (difference > 1.1 * min_size) || difference > max_size;
+    return 3 * similarity < 2 * min_size || (difference > 1.25 * min_size) || difference > max_size;
   else if(min_size <= 30)
-    return 10 * similarity < 7 * min_size || (difference > 1.1 * min_size) || difference > max_size;
+    return 10 * similarity < 7 * min_size || (difference > 1.25 * min_size) || difference > max_size;
   else
-    return 2 * similarity < min_size || (difference > 1.1 * min_size) || difference > max_size;
+    return 2 * similarity < min_size || (difference > 1.25 * min_size) || difference > max_size;
 
 }
 
@@ -986,21 +986,21 @@ bool reject_match_same(int similarity, int difference, int text_old_length, int 
 
   }
 
-  int syntax_similarity, syntax_difference, children_length_old, children_length_new;
-  compute_syntax_measures(nodes_old, node_set_old, nodes_new, node_set_new, syntax_similarity, syntax_difference, children_length_old, children_length_new);
+  // int syntax_similarity, syntax_difference, children_length_old, children_length_new;
+  // compute_syntax_measures(nodes_old, node_set_old, nodes_new, node_set_new, syntax_similarity, syntax_difference, children_length_old, children_length_new);
 
-  int min_child_length = children_length_old < children_length_new ? children_length_old : children_length_new;
-  int max_child_length = children_length_old < children_length_new ? children_length_new : children_length_old;
+  // int min_child_length = children_length_old < children_length_new ? children_length_old : children_length_new;
+  // int max_child_length = children_length_old < children_length_new ? children_length_new : children_length_old;
 
-  if(min_child_length > 1) { 
+  // if(min_child_length > 1) { 
 
-    if(min_child_length < 3 && 2 * syntax_similarity >= min_child_length && syntax_difference <= min_child_length)
-      return false;
+  //   if(min_child_length < 3 && 2 * syntax_similarity >= min_child_length && syntax_difference <= min_child_length)
+  //     return false;
 
-    if(min_child_length > 2 && 3 * syntax_similarity >= 2 * min_child_length && syntax_difference <= min_child_length) 
-      return false;
+  //   if(min_child_length > 2 && 3 * syntax_similarity >= 2 * min_child_length && syntax_difference <= min_child_length) 
+  //     return false;
 
-  }
+  // }
 
   bool is_reject = reject_similarity(similarity, difference, text_old_length, text_new_length, nodes_old, node_set_old, nodes_new, node_set_new);
   return is_reject;
