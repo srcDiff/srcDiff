cb54a893b0e0e214520fed3de669596499ffa50c
Michael John Decker <mdecker6@kent.edu>
2015-01-05 10:37:08 -0500

    Do not have options: modifier, operator, literal.

--- test/test_suite.py
+++ test/test_suite.py
@@ -113,7 +113,7 @@ def linediff(xml_filename1, xml_filename2) :
 # find differences of two files
 def srcdiff(source_file_version_one, source_file_version_two, encoding, language, directory, filename, prefixlist) :
 
-	command = [globals()["srcdiff_utility"], "-d", directory, "-f", filename, "--operator", "--literal", "--modifier"]
+	command = [globals()["srcdiff_utility"], "-d", directory, "-f", filename]
 
 	temp_file = open("temp_file_one.cpp", "w")
 	temp_file.write(source_file_version_one)
