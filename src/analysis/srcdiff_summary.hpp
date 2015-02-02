22162dcf08f433719dcc5c76a16c8fbbf38fc924
Michael John Decker <mdecker6@kent.edu>
2015-02-02 13:09:13 -0500

    Compile simple pretty much empty handler for summary.

--- src/analysis/srcdiff_summary.hpp
+++ src/analysis/srcdiff_summary.hpp
@@ -1,18 +1,22 @@
 #ifndef INCLUDED_SRCDIFF_SUMMARY_HPP
 #define INCLUDED_SRCDIFF_SUMMARY_HPP
 
+#include <string>
+
 class srcdiff_summary {
 
 protected:
 
 private:
 
+	const char * xml_encoding;
+
 public:
 
-	srcdiff_summary();
+	srcdiff_summary(const char * xml_encoding);
 	~srcdiff_summary();
 
-	void append();
+	void append(const std::string & srcdiff);
 
 };
 
---
+++ src/analysis/srcdiff_summary_handler.hpp
@@ -0,0 +1,141 @@
+#ifndef INCLUDED_SRCDIFF_SUMMARY_HANDLER_HPP
+#define INCLUDED_SRCDIFF_SUMMARY_HANDLER_HPP
+
+#include <srcSAXHandler.hpp>
+
+/**
+ * srcSAXHandler
+ *
+ * Base class that provides hooks for SAX processing.
+ */
+class srcdiff_summary_handler : public srcSAXHandler {
+
+private:
+
+protected:
+
+public:
+
+    /**
+     * startDocument
+     *
+     * SAX handler function for start of document.
+     * Overide for desired behaviour.
+     */
+    virtual void startDocument() {}
+
+    /**
+     * endDocument
+     *
+     * SAX handler function for end of document.
+     * Overide for desired behaviour.
+     */
+    virtual void endDocument() {}
+
+    /**
+     * startRoot
+     * @param localname the name of the element tag
+     * @param prefix the tag prefix
+     * @param URI the namespace of tag
+     * @param num_namespaces number of namespaces definitions
+     * @param namespaces the defined namespaces
+     * @param num_attributes the number of attributes on the tag
+     * @param attributes list of attributes
+     *
+     * SAX handler function for start of the root element.
+     * Overide for desired behaviour.
+     */
+    virtual void startRoot(const char * localname, const char * prefix, const char * URI,
+                           int num_namespaces, const struct srcsax_namespace * namespaces, int num_attributes,
+                           const struct srcsax_attribute * attributes) {}
+
+    /**
+     * startUnit
+     * @param localname the name of the element tag
+     * @param prefix the tag prefix
+     * @param URI the namespace of tag
+     * @param num_namespaces number of namespaces definitions
+     * @param namespaces the defined namespaces
+     * @param num_attributes the number of attributes on the tag
+     * @param attributes list of attributes
+     *
+     * SAX handler function for start of an unit.
+     * Overide for desired behaviour.
+     */
+    virtual void startUnit(const char * localname, const char * prefix, const char * URI,
+                           int num_namespaces, const struct srcsax_namespace * namespaces, int num_attributes,
+                           const struct srcsax_attribute * attributes) {}
+
+    /**
+     * startElement
+     * @param localname the name of the element tag
+     * @param prefix the tag prefix
+     * @param URI the namespace of tag
+     * @param num_namespaces number of namespaces definitions
+     * @param namespaces the defined namespaces
+     * @param num_attributes the number of attributes on the tag
+     * @param attributes list of attributes
+     *
+     * SAX handler function for start of an element.
+     * Overide for desired behaviour.
+     */
+    virtual void startElement(const char * localname, const char * prefix, const char * URI,
+                                int num_namespaces, const struct srcsax_namespace * namespaces, int num_attributes,
+                                const struct srcsax_attribute * attributes) {}
+
+    /**
+     * endRoot
+     * @param localname the name of the element tag
+     * @param prefix the tag prefix
+     * @param URI the namespace of tag
+     *
+     * SAX handler function for end of the root element.
+     * Overide for desired behaviour.
+     */
+    virtual void endRoot(const char * localname, const char * prefix, const char * URI) {}
+
+    /**
+     * endUnit
+     * @param localname the name of the element tag
+     * @param prefix the tag prefix
+     * @param URI the namespace of tag
+     *
+     * SAX handler function for end of an unit.
+     * Overide for desired behaviour.
+     */
+    virtual void endUnit(const char * localname, const char * prefix, const char * URI) {}
+
+    /**
+     * endElement
+     * @param localname the name of the element tag
+     * @param prefix the tag prefix
+     * @param URI the namespace of tag
+     *
+     * SAX handler function for end of an element.
+     * Overide for desired behaviour.
+     */
+    virtual void endElement(const char * localname, const char * prefix, const char * URI) {}
+
+    /**
+     * charactersRoot
+     * @param ch the characers
+     * @param len number of characters
+     *
+     * SAX handler function for character handling at the root level.
+     * Overide for desired behaviour.
+     */
+    virtual void charactersRoot(const char * ch, int len) {}
+
+    /**
+     * charactersUnit
+     * @param ch the characers
+     * @param len number of characters
+     *
+     * SAX handler function for character handling within a unit.
+     * Overide for desired behaviour.
+     */
+    virtual void charactersUnit(const char * ch, int len) {}
+
+};
+
+#endif
