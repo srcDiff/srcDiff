94d703b65f60eabf2185aa415bf37536ee0b1ed0
Michael John Decker <mdecker6@kent.edu>
2015-03-07 12:15:31 -0500

    Disable warnings for now.  Will take more thought to make sure unsigned/signed is correct.

--- CMake/config.cmake
+++ CMake/config.cmake
@@ -97,7 +97,7 @@ if(${CMAKE_COMPILER_IS_GNUCXX})
 elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 
     # Configuring the Clang compiler
-    set(CLANG_WARNINGS "-Wno-long-long -Wall -Wextra -Wshorten-64-to-32 -Wno-unknown-pragmas -Wno-int-to-void-pointer-cast")
+    #set(CLANG_WARNINGS "-Wno-long-long -Wall -Wextra -Wshorten-64-to-32 -Wno-unknown-pragmas -Wno-int-to-void-pointer-cast")
     set(CMAKE_CXX_FLAGS "-fPIC -std=c++1y ${CLANG_WARNINGS}")
     set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -DSTATIC_GLOBALS")
     set(CMAKE_CXX_FLAGS_DEBUG   "-O0 -g -DDEBUG")
