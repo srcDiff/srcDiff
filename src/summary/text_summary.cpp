e2adf5009e5aa22a0952cb7e521ccc13755b6610
Michael John Decker <mdecker6@kent.edu>
2015-04-13 10:16:13 -0400

    Fix reporting of number of changes when 0 or 1.

--- src/summary/text_summary.cpp
+++ src/summary/text_summary.cpp
@@ -80,9 +80,9 @@ std::string text_summary::get_type_string_with_count(const std::shared_ptr<profi
 
     }
 
-    if((profile->statement_count - 1) == 0)
+    if(profile->statement_count == 0)
         return "empty " + get_type_string(profile);
-    else if((profile->statement_count - 1) == 1)
+    else if(profile->statement_count == 1)
         return get_type_string(profile) + " with a single statement";
     else
         return get_type_string(profile) + " with " + std::to_string(profile->statement_count) + " statements";   
