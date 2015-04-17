b68eced2bb072767148b8cb25563927ea9d2391f
Michael John Decker <mdecker6@kent.edu>
2015-04-17 10:35:19 -0400

    Detect decl_stmt moves.  Extract functions common to expr_stmt and decl_stmt.

--- src/summary/move_handler.hpp
+++ src/summary/move_handler.hpp
@@ -14,6 +14,23 @@ private:
 
 	profile_t::profile_list_t move_candidates;
 
+	void set_strings(srcdiff_type first_operation, const versioned_string & first, const versioned_string & second, versioned_string & original, versioned_string & modified) const {
+
+        original = first_operation == SRCDIFF_DELETE ? first  : second;
+        modified = first_operation == SRCDIFF_DELETE ? second : first;		
+
+	}
+
+	void set_move(std::shared_ptr<profile_t> & profile, const std::pair<versioned_string, versioned_string> & first_pair, const std::pair<versioned_string, versioned_string> & second_pair) {
+
+		if(first_pair.first.has_original() == first_pair.second.has_modified()
+                && first_pair.first.original() == first_pair.second.modified()
+                && second_pair.first.has_original() == second_pair.second.has_modified()
+                && second_pair.first.original() == second_pair.second.modified())
+			profile->move_id = (size_t)-1;
+
+	}
+
 public:
 
 	move_handler() {}
@@ -46,18 +63,24 @@ public:
                     std::shared_ptr<expr_stmt_profile_t> & first_expr_stmt_profile  = reinterpret_cast<std::shared_ptr<expr_stmt_profile_t> &>(first_profile);
                     std::shared_ptr<expr_stmt_profile_t> & second_expr_stmt_profile = reinterpret_cast<std::shared_ptr<expr_stmt_profile_t> &>(second_profile);
 
-                    versioned_string original_lhs = first_expr_stmt_profile->operation == SRCDIFF_DELETE ? first_expr_stmt_profile->lhs()  : second_expr_stmt_profile->lhs();
-                    versioned_string modified_lhs = first_expr_stmt_profile->operation == SRCDIFF_DELETE ? second_expr_stmt_profile->lhs() : first_expr_stmt_profile->lhs();
+                    versioned_string original_lhs, modified_lhs;
+                    set_strings(first_profile->operation, first_expr_stmt_profile->lhs(), second_expr_stmt_profile->lhs(), original_lhs, modified_lhs);
+                    versioned_string original_rhs, modified_rhs;
+                    set_strings(first_profile->operation, first_expr_stmt_profile->rhs(), second_expr_stmt_profile->rhs(), original_rhs, modified_rhs);
 
-                    versioned_string original_rhs = first_expr_stmt_profile->operation == SRCDIFF_DELETE ? first_expr_stmt_profile->rhs()  : second_expr_stmt_profile->rhs();
-                    versioned_string modified_rhs = first_expr_stmt_profile->operation == SRCDIFF_DELETE ? second_expr_stmt_profile->rhs() : first_expr_stmt_profile->rhs();
+                    set_move(first_profile, std::make_pair(original_lhs, modified_lhs), std::make_pair(original_rhs, modified_rhs));
 
+                } else if(first_profile->type_name == "decl_stmt") {
 
-                    if(original_lhs.has_original() == modified_lhs.has_modified()
-                        && original_lhs.original() == modified_lhs.modified()
-                        && original_rhs.has_original() == modified_rhs.has_modified()
-                        && original_rhs.original() == modified_rhs.modified())
-                        first_profile->move_id = (size_t)-1;
+                    std::shared_ptr<decl_stmt_profile_t> & first_decl_stmt_profile  = reinterpret_cast<std::shared_ptr<decl_stmt_profile_t> &>(first_profile);
+                    std::shared_ptr<decl_stmt_profile_t> & second_decl_stmt_profile = reinterpret_cast<std::shared_ptr<decl_stmt_profile_t> &>(second_profile);
+
+                    versioned_string original_type, modified_type;
+                    set_strings(first_profile->operation, first_decl_stmt_profile->type, second_decl_stmt_profile->type, original_type, modified_type);
+                    versioned_string original_name, modified_name;
+                    set_strings(first_profile->operation, first_decl_stmt_profile->name, second_decl_stmt_profile->name, original_name, modified_name);
+
+                    set_move(first_profile, std::make_pair(original_type, modified_type), std::make_pair(original_name, modified_name));
 
                 }
 
@@ -81,7 +104,6 @@ public:
         }
 
     }
-
 
 };
 
