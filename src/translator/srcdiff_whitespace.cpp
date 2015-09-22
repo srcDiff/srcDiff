2adc2ad127944d0af5ba8ee03c184e61b9d0ba5e
Michael John Decker <mdecker6@kent.edu>
2015-09-22 13:11:57 -0400

    Remove possible warning.

--- src/translator/srcdiff_whitespace.cpp
+++ src/translator/srcdiff_whitespace.cpp
@@ -149,8 +149,8 @@ void srcdiff_whitespace::output_statement() {
   unsigned int oend = rbuf_original->last_output;
   unsigned int nend = rbuf_modified->last_output;
 
-  if((oend - 1) >= 0 && !is_statement(rbuf_original->nodes.at(oend - 1)->name)
-    && (nend - 1) >= 0 && !is_statement(rbuf_modified->nodes.at(nend - 1)->name)) return;
+  if(oend >= 1 && !is_statement(rbuf_original->nodes.at(oend - 1)->name)
+    && nend >= 1 && !is_statement(rbuf_modified->nodes.at(nend - 1)->name)) return;
 
   // advance whitespace after targeted end
   for(; oend < rbuf_original->nodes.size() && rbuf_original->nodes.at(oend)->is_white_space() && !rbuf_original->nodes.at(oend)->is_new_line(); ++oend)
