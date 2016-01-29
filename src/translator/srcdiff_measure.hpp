5b9aad1ce1723c240f9d4a1e7488de9118bb6243
Michael John Decker <mdecker6@kent.edu>
2016-01-29 09:33:25 -0500

    Push down compute text methods.

--- src/translator/srcdiff_measure.hpp
+++ src/translator/srcdiff_measure.hpp
@@ -17,9 +17,6 @@ protected:
 	int original_len;
 	int modified_len;
 
-	void compute_ses(class shortest_edit_script & ses);
-	void compute_ses_important_text(class shortest_edit_script & ses);
-
 public:
 
 	srcdiff_measure(const node_set & set_original, const node_set & set_modified);
--- src/translator/srcdiff_text_measure.hpp
+++ src/translator/srcdiff_text_measure.hpp
@@ -13,6 +13,9 @@ public:
 
 	srcdiff_text_measure(const node_set & set_original, const node_set & set_modified);
 
+    void compute_ses(class shortest_edit_script & ses);
+    void compute_ses_important_text(class shortest_edit_script & ses);
+
 	virtual void compute();
 
 };
