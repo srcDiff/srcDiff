665ca138041ea4adbe6ac5ae6c15dc9e8cf82883
Michael John Decker <mdecker6@kent.edu>
2016-02-03 13:18:59 -0500

    Added start of function to diff in blocks.

--- src/shortest_edit_script/shortest_edit_script.hpp
+++ src/shortest_edit_script/shortest_edit_script.hpp
@@ -3,6 +3,9 @@
 
 #include <shortest_edit_script.h>
 
+#include <vector>
+#include <cmath>
+
 class shortest_edit_script {
   
 private:
@@ -15,6 +18,8 @@ private:
 
 public:
 
+  static const int SIZE_THRESHOLD = 500;
+
   shortest_edit_script(int (*compare)(const void * item_one, const void * item_two, const void * context),
                      const void * (*accessor)(int index, const void * structure, const void * context),
                                         const void * context, int threshold = 1000);
@@ -23,8 +28,58 @@ public:
 
   edit * get_script();
 
+  template<typename T>
+  int compute(std::vector<T> structure_one, std::vector<T> structure_two);
   int compute(const void * structure_one, int size_one, const void * structure_two, int size_two);
 
 };
+
+template<typename T>
+int shortest_edit_script::compute(std::vector<T> structure_one, std::vector<T> structure_two) {
+
+    const int size_one = structure_one.size();
+    const int size_two = structure_two.size();
+
+    if(size_one > SIZE_THRESHOLD || size_two > SIZE_THRESHOLD) {
+
+        edit * edits = nullptr;
+        int distance = 0;
+
+        int blocks_one = ceil(size_one / SIZE_THRESHOLD);
+        int blocks_two = ceil(size_two / SIZE_THRESHOLD);
+        int num_blocks = std::max(blocks_one, blocks_two);
+
+        int i = 0;
+        for(; i < num_blocks; ++i) {
+
+          int current_size_one = 0, offset_one = 0;
+          if(i < blocks_one) {
+
+            current_size_one = std::min(SIZE_THRESHOLD, size_one - SIZE_THRESHOLD * i);
+            offset_one = SIZE_THRESHOLD * i;
+
+          }
+
+          int current_size_two = 0, offset_two = 0;
+          if(i < blocks_two) {
+
+            current_size_two = std::min(SIZE_THRESHOLD, size_two - SIZE_THRESHOLD * i);
+            offset_two = SIZE_THRESHOLD * i;
+
+          }
+
+          distance += shortest_edit_script_hybrid((const void *)(structure_one.data() + offset_one), size_one, (const void *)(structure_two.data() + offset_two), size_two, &edits, compare, accessor, context, threshold);        
+
+        }
+
+        return distance;
+
+    } else {
+
+      return shortest_edit_script_hybrid((const void *)structure_one.data(), size_one, (const void *)structure_two.data(), size_two, &edit_script, compare, accessor, context, threshold);
+
+    }
+
+}
 
 #endif
--- src/shortest_edit_script/shortest_edit_script_class.cpp
+++ src/shortest_edit_script/shortest_edit_script_class.cpp
@@ -4,7 +4,7 @@
 shortest_edit_script::shortest_edit_script(int (*compare)(const void * item_one, const void * item_two, const void * context)
                                        , const void * (*accessor)(int index, const void * structure, const void * context)
                                        , const void * context
-                                       , int threshold) : edit_script(0), context(context), compare(compare), accessor(accessor), threshold(threshold) { }
+                                       , int threshold) : edit_script(nullptr), context(context), compare(compare), accessor(accessor), threshold(threshold) { }
 
 shortest_edit_script::~shortest_edit_script() {
 
@@ -15,7 +15,7 @@ shortest_edit_script::~shortest_edit_script() {
 
 int shortest_edit_script::compute(const void * structure_one, int size_one, const void * structure_two, int size_two) {
 
-  return shortest_edit_script_hybrid(structure_one, size_one, structure_two, size_two, &edit_script, compare, accessor, context, threshold);
+    return shortest_edit_script_hybrid(structure_one, size_one, structure_two, size_two, &edit_script, compare, accessor, context, threshold);
 
 }
 
