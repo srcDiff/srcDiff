129f594a36fb7da185b6f4d13635e7b1c4dda360
Michael John Decker <mdecker6@kent.edu>
2016-04-12 18:03:43 -0400

    Adding a class for monokai color scheme.

---
+++ src/visualization/monokai.hpp
@@ -0,0 +1,13 @@
+class monokai {
+
+    static const COMMENT_COLOR_HTML       = "<span style=\"color: rgb(117, 113, 94);\">";
+    static const STRING_COLOR_HTML        = "<span style=\"color: rgb(230, 219, 116);\">";
+    static const NUMBER_COLOR_HTML        = "<span style=\"color: rgb(174, 129, 255);\">";
+    static const CONSTANT_COLOR_HTML      = "<span style=\"color: rgb(174, 129, 255);\">";
+    static const KEYWORD_COLOR_HTML       = "<span style=\"color: rgb(249, 38, 114);\">";
+    static const STORAGE_COLOR_HTML       = "<span style=\"color: rgb(249, 38, 114);\">";
+    static const STORAGE_TYPE_COLOR_HTML  = "<span style=\"color: rgb(102, 217, 239);\">";
+    static const CLASS_NAME_COLOR_HTML    = "<span style=\"color: rgb(249, 38, 114);\">";
+    static const FUNCTION_NAME_COLOR_HTML = "<span style=\"color: rgb(249, 38, 114);\">";
+
+};
