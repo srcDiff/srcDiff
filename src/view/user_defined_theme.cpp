04900027bbb6bea82c9b17fedcb9e3033a460239
Michael John Decker <mdecker6@kent.edu>
2016-05-16 14:25:28 -0400

    More work on user defined stuff.

--- src/view/user_defined_theme.cpp
+++ src/view/user_defined_theme.cpp
@@ -48,6 +48,20 @@ public:
 
     }
 
+    std::string to_html() const {
+
+        return std::string("rgb(") + std::to_string(red) + ", "
+            + std::to_string(green) + ", "
+            + std::to_string(blue) + ')';
+
+    }
+
+    std::string to_ansi() const {
+
+        return "";
+
+    }
+
     friend std::ostream & operator<<(std::ostream & out, const color_t & color) {
 
         out << '#' << std::hex << color.red << color.green << color.    blue;
@@ -68,7 +82,7 @@ public:
 
 };
 
-void parse_user_definition_file(const std::string & theme_filename) {
+boost::program_options::variables_map parse_user_definition_file(const std::string & theme_filename) {
 
     std::ifstream theme_file(theme_filename.c_str());
     if(!theme_file)
@@ -107,10 +121,11 @@ try {
 
   } catch(const boost::program_options::error & e) {
 
-    std::cerr << "Exception: " << e.what() << '\n';
-    exit(1);
+    throw std::string("Exception: ") + e.what();
 
   }
+
+  return vm;
 
 }
 
@@ -119,49 +134,49 @@ user_defined_theme::user_defined_theme(const std::string & highlight_level,
                                        const std::string & theme_filename)
     : theme_t(highlight_level, is_html) {
 
-    parse_user_definition_file(theme_filename);
+    boost::program_options::variables_map vm = parse_user_definition_file(theme_filename);
 
     if(is_html) {
 
-        background_color  = "transparent";
-        text_color        = "black";
-        line_number_color = "grey";
+        background_color  = vm["background-color"].as<color_t>().to_html();
+        text_color        = vm["text-color"].as<color_t>().to_html();
+        line_number_color = vm["line-number-color"].as<color_t>().to_html();
 
-        common_color = "<span style=\"background-color: transparent;\">";
-        delete_color = "<span style=\"color: rgb(160, 160, 160); text-decoration: line-through;\"><span style=\"color: black; background-color: rgb(255,187,187); font-weight: bold;\">";
-        insert_color = "<span style=\"background-color: rgb(0 , 250, 108); font-weight: bold;\">";
+        common_color = "<span style=\"background-color: " + vm["common-color"].as<color_t>().to_html() + ";\">";
+        delete_color = "<span style=\"color: rgb(160, 160, 160); text-decoration: line-through;\"><span style=\"color: black; background-color: " + vm["delete-color"].as<color_t>().to_html() + "; font-weight: bold;\">";
+        insert_color = "<span style=\"background-color: " + vm["insert-color"].as<color_t>().to_html() + "; font-weight: bold;\">";
 
-        keyword_color = "<span style=\"color: rgb(249, 38, 114);\">";
-        storage_color = "<span style=\"color: rgb(249, 38, 114);\">";
-        type_color    = "<span style=\"color: rgb(50, 150, 225);\">";
+        keyword_color = "<span style=\"color: " + vm["keyword-color"].as<color_t>().to_html() + ";\">";
+        storage_color = "<span style=\"color: " + vm["storage-color"].as<color_t>().to_html() + ";\">";
+        type_color    = "<span style=\"color: " + vm["type-color"].as<color_t>().to_html() + ";\">";
 
-        comment_color = "<span style=\"color: rgb(110, 110, 110);\">";
-        number_color  = "<span style=\"color: rgb(255, 0, 255);\">";
-        string_color  = "<span style=\"color: rgb(255, 128, 0);\">";
+        comment_color = "<span style=\"color: " + vm["comment-color"].as<color_t>().to_html() + ";\">";
+        number_color  = "<span style=\"color: " + vm["number-color"].as<color_t>().to_html() + ";\">";
+        string_color  = "<span style=\"color: " + vm["string-color"].as<color_t>().to_html() + ";\">";
 
-        function_name_color = "<span style=\"color: rgb(0, 160, 64);\">";
-        class_name_color    = "<span style=\"color: rgb(0, 160, 64);\">";
-        call_name_color     = "<span style=\"color: rgb(50, 140, 215);\">";
+        function_name_color = "<span style=\"color: " + vm["function-name-color"].as<color_t>().to_html() + ";\">";
+        class_name_color    = "<span style=\"color: " + vm["class-name-color"].as<color_t>().to_html() + ";\">";
+        call_name_color     = "<span style=\"color: " + vm["call-name-color"].as<color_t>().to_html() + ";\">";
 
     } else {
 
-        line_number_color = "\x1b[36m";
+        line_number_color = "\x1b[" + vm["line-number-color-color"].as<color_t>().to_ansi() + 'm';
 
-        common_color = "\x1b[0m";
-        delete_color = "\x1b[9;48;5;217;1m";
-        insert_color = "\x1b[48;5;120;1m";
+        common_color = "\x1b[" + vm["common-color"].as<color_t>().to_ansi() + 'm';
+        delete_color = "\x1b[" + vm["delete-color"].as<color_t>().to_ansi() + 'm';
+        insert_color = "\x1b[" + vm["insert-color"].as<color_t>().to_ansi() + 'm';
 
-        keyword_color = "\x1b[38;5;198m";
-        storage_color = "\x1b[38;5;198m";
-        type_color    = "\x1b[38;5;68m";
+        keyword_color = "\x1b[" + vm["keyword-color"].as<color_t>().to_ansi() + 'm';
+        storage_color = "\x1b[" + vm["storage-color"].as<color_t>().to_ansi() + 'm';
+        type_color    = "\x1b[" + vm["type-color"].as<color_t>().to_ansi() + 'm';
 
-        comment_color = "\x1b[38;5;102m";
-        number_color  = "\x1b[38;5;201m";
-        string_color  = "\x1b[38;5;208m";
+        comment_color = "\x1b[" + vm["comment-color"].as<color_t>().to_ansi() + 'm';
+        number_color  = "\x1b[" + vm["number-color"].as<color_t>().to_ansi() + 'm';
+        string_color  = "\x1b[" + vm["string-color"].as<color_t>().to_ansi() + 'm';
 
-        function_name_color = "\x1b[38;5;28m";
-        class_name_color    = "\x1b[38;5;28m";
-        call_name_color     = "\x1b[38;5;68m";
+        function_name_color = "\x1b[" + vm["function-name-color"].as<color_t>().to_ansi() + 'm';
+        class_name_color    = "\x1b[" + vm["class-name-color"].as<color_t>().to_ansi() + 'm';
+        call_name_color     = "\x1b[" + vm["call-name-color"].as<color_t>().to_ansi() + 'm';
 
     }
 
