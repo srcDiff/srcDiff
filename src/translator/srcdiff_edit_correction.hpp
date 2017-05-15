9aa895b8021d8600dacda1401c959bdb6134bc14
Michael John Decker <mdecker6@kent.edu>
2017-05-15 15:51:38 -0400

    Fixes case with no additional problems.

--- src/translator/srcdiff_edit_correction.hpp
+++ src/translator/srcdiff_edit_correction.hpp
@@ -12,7 +12,7 @@
 #include <shortest_edit_script.hpp>
 #include <node_sets.hpp>
 #include <srcdiff_text_measure.hpp>
-#include <boost/optional.hpp>
+#include <memory>
 
 class srcdiff_edit_correction {
 
@@ -28,7 +28,7 @@ private:
 	                    edit_t *& last_edits);
 	edit_t * correct_common_inner(edit_t * change_edit);
 	edit_t * correct_common(edit_t * start_edit);
-	boost::optional<srcdiff_text_measure> edit2measure(int original_offset, int modified_offset);
+	std::shared_ptr<srcdiff_text_measure> edit2measure(int original_offset, int modified_offset);
 
 public:
 	srcdiff_edit_correction(const node_sets & sets_original,
