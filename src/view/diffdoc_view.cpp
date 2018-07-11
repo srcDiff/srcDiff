461efaab81d6f4a9ec966655c847aeabfb16aab7
Michael John Decker <mdecke@bgsu.edu>
2018-07-11 12:07:32 -0400

    Strip out unified_view code

--- src/view/diffdoc_view.cpp
+++ src/view/diffdoc_view.cpp
@@ -1,4 +1,4 @@
-#include <unified_view.hpp>
+#include <diffdoc_view.hpp>
 
 #include <srcdiff_constants.hpp>
 
@@ -7,122 +7,36 @@
 #include <cstring>
 #include <cassert>
 
-unified_view::unified_view(const std::string & output_filename,
+diffdoc_view::diffdoc_view(const std::string & output_filename,
                            const std::string & syntax_highlight,
                            const std::string & theme,
                            bool ignore_all_whitespace,
                            bool ignore_whitespace,
                            bool ignore_comments,
-                           bool is_html,
-                           boost::any context_type)
+                           bool is_html)
               : view_t(output_filename,
-                          syntax_highlight, 
-                          theme,
-                          ignore_all_whitespace,
-                          ignore_whitespace,
-                          ignore_comments,
-                          is_html),
-                last_character_operation(view_t::UNSET), modes(LINE), line_number_delete(0), line_number_insert(0), number_context_lines(3),
-                is_after_change(false), wait_change(true), in_function(),
-                context_type(context_type), length(0), is_after_additional(false),
-                after_edit_count(0), last_context_line((unsigned)-1),
-                change_starting_line(false), change_ending_space(),
-                change_ending_operation(view_t::UNSET),
-                close_num_spans(0) {
-
-  if(context_type.type() == typeid(size_t)) {
-
-    number_context_lines = boost::any_cast<size_t>(context_type);
-
-  } else {
-
-    const std::string & context_type_str = boost::any_cast<std::string>(context_type);
-    const std::string::size_type dash_pos = context_type_str.find('-');
-    context_mode mode = context_string_to_id(context_type_str.substr(0, dash_pos));
-
-    if(mode == ALL) number_context_lines = -1;
-    else modes |= mode;
-
-    // assume dash is -only /** @todo actually complete this */
-    if(dash_pos != std::string::npos) modes = FUNCTION;
-
-  }
+                       syntax_highlight, 
+                       theme,
+                       ignore_all_whitespace,
+                       ignore_whitespace,
+                       ignore_comments,
+                       true) {
 
 }
 
-unified_view::~unified_view() {}
+diffdoc_view::~diffdoc_view() {}
 
-void unified_view::reset_internal() {
+void diffdoc_view::reset_internal() {
+}
 
-  line_number_delete = 0;
-  line_number_insert = 0;
-  is_after_change = false;
-  wait_change = true;
-  in_function.clear();
-  length = 0;
-  additional_context.clear();
-  is_after_additional = false;
-  after_edit_count = 0;
-  last_context_line = -1;
-  last_character_operation = view_t::UNSET;
-  change_starting_line = false;
-  change_ending_space = "";
-  change_ending_operation = view_t::UNSET;
-  close_num_spans = 0;
+
+void diffdoc_view::output_characters(const std::string & ch, int operation) {
+
+  //output_characters_to_buffer(*output, ch, operation, last_character_operation, close_num_spans);
 
 }
 
-unified_view::context_mode unified_view::context_string_to_id(const std::string & context_type_str) const {
-
-  if(context_type_str == "all")      return ALL;
-  if(context_type_str == "function") return FUNCTION;
-  else                               return LINE;
-
-
-}
-
-bool unified_view::in_mode(context_mode mode) {
-
-  return mode & modes;
-
-}
-
-void unified_view::output_additional_context() {
-
-  size_t line_delete = line_number_delete + 1 - additional_context.size();
-  size_t line_insert = line_number_insert + 1 - additional_context.size();
-
-  if(number_context_lines != -1 && wait_change && last_context_line != (line_number_delete - 1)) {
-
-    if(!is_html)
-      (*output) << theme->common_color << theme->line_number_color << "@@ -" << line_delete << " +" << line_insert << " @@" << theme->common_color << '\n';
-    else
-      (*output) << "<span style=\"color:" << theme->line_number_color << ";\">@@ -" << line_delete << " +" << line_insert << " @@" << "</span>\n";
-
-  }
-
-  if(additional_context.empty()) return;
-
-  for(std::list<std::string>::const_iterator citr = additional_context.begin(); citr != additional_context.end(); ++citr) {
-
-    (*output) << *citr;
-
-    ++line_delete, ++line_insert;
-
-  }
-
-  additional_context.clear();
-  length = 0;
-
-}
-
-void unified_view::output_characters(const std::string & ch, int operation) {
-
-  output_characters_to_buffer(*output, ch, operation, last_character_operation, close_num_spans);
-
-}
-
-void unified_view::start_unit(const std::string & local_name,
+void diffdoc_view::start_unit(const std::string & local_name,
                               const char * prefix,
                               const char * URI,
                               int num_namespaces,
@@ -130,12 +44,11 @@ void unified_view::start_unit(const std::string & local_name,
                               int num_attributes,
                               const struct srcsax_attribute * attributes) {
 
-  if(is_html)
-    (*output) << "<pre>";
+  (*output) << "<pre>";
 
 }
 
-void unified_view::start_element(const std::string & local_name,
+void diffdoc_view::start_element(const std::string & local_name,
                                  const char * prefix,
                                  const char * URI,
                                  int num_namespaces,
@@ -146,253 +59,43 @@ void unified_view::start_element(const std::string & local_name,
 
   if(URI == SRCDIFF_DEFAULT_NAMESPACE_HREF) {
 
-    if(ignore_comments && in_comment) return;
-
     if(local_name == "common")
      diff_stack.push_back(view_t::COMMON);
     else if(local_name == "delete")
      diff_stack.push_back(DELETE);
     else if(local_name == "insert")
      diff_stack.push_back(INSERT);
-    else if(local_name == "ws" && ignore_all_whitespace)
-      diff_stack.push_back(view_t::COMMON);
     
   } else {
 
-    if(local_name == "comment") {
-
-      if(ignore_comments)
-        diff_stack.push_back(view_t::COMMON);
-
-    }
-
-    if(in_mode(FUNCTION) && is_function_type(local_name)) {
-
-      in_function.push_back(true);
-
-      if(in_function.size() == 1) {
-
-        additional_context.clear();
-        length = 0;
-
-      }
-
-    }
-
   }
 
 }
 
-void unified_view::end_unit(const std::string & local_name,
+void diffdoc_view::end_unit(const std::string & local_name,
                             const char * prefix,
                             const char * URI) {
 
-  if(!change_ending_space.empty()) {
 
-    output_characters(change_ending_space, view_t::COMMON);
-    change_ending_space = "";
-    change_ending_operation = view_t::COMMON;
-
-  }
-  output_additional_context();
-
-  if(is_html)
-    (*output) << "</pre>";
+  (*output) << "</pre>";
 
 }
 
-void unified_view::end_element(const std::string & local_name,
+void diffdoc_view::end_element(const std::string & local_name,
                                const char * prefix,
                                const char * URI) {
 
     if(URI == SRCDIFF_DEFAULT_NAMESPACE_HREF) {
 
-      if(ignore_comments && in_comment) return;
-
-      if(local_name == "common" || local_name == "delete" || local_name == "insert"
-        || (local_name == "ws" && ignore_all_whitespace))
+      if(local_name == "common" || local_name == "delete" || local_name == "insert")
         diff_stack.pop_back();
 
   } else {
-
-    if(local_name == "comment") {
-
-      if(ignore_comments)
-        diff_stack.pop_back();
-      
-    }
-
-    if(in_mode(FUNCTION) && is_function_type(local_name)) {
-
-      in_function.pop_back();
-      if(in_function.size() == 0) {
-
-        additional_context.clear();
-        length = 0;
-
-      }
-
-    }
 
   }
 
 }
 
-void unified_view::characters(const char * ch, int len) {
-
-  if(diff_stack.back() != view_t::COMMON) {
-
-   output_additional_context();
-
-   is_after_additional = false;
-   is_after_change = false;
-   wait_change = false;
-
-   output->write(context.str().c_str(), context.str().size());
-   context.str("");
-
-  }
-
-  if(!change_ending_space.empty() && change_ending_operation != diff_stack.back()) {
-
-    output_characters(change_ending_space, view_t::COMMON);
-    change_ending_space = "";
-    change_ending_operation = view_t::UNSET;
-
-  }
-
-  if((last_character_operation == view_t::UNSET || last_character_operation == view_t::COMMON)
-     && diff_stack.back() != view_t::COMMON && ignore_whitespace)
-    change_starting_line = true;
-
-  for(int i = 0; i < len; ++i) {
-
-    bool skip = false;
-    bool is_space = isspace(ch[i]);
-    if(is_space) {
-
-      if(ignore_whitespace && diff_stack.back() != view_t::COMMON) {
-
-        if(change_starting_line) {
-
-          output_character(ch[i], view_t::COMMON);
-
-        } else {
-
-          change_ending_space += ch[i];
-          change_ending_operation = diff_stack.back();
-
-        }
-
-        skip = true;
-
-      }
-
-    } else {
-
-      if(!change_ending_space.empty()) {
-
-        output_characters(change_ending_space, diff_stack.back());
-        change_ending_space = "";
-        change_ending_operation = view_t::UNSET;
-
-      }
-
-      change_starting_line = false;
-
-    }
-
-    std::string str(1, ch[i]);
-    if(!is_space || ch[i] != '\n') {
-
-      ++i;
-      while(i < len && isspace(ch[i]) == is_space && ch[i] != '\n') {
-
-        str += ch[i];
-        ++i;
-
-      }
-
-      --i;
-
-    }
-
-    if(wait_change) {
-
-      assert(!skip);
-      output_characters_to_buffer(context, str, view_t::COMMON, last_character_operation, close_num_spans);
-
-    } else if(!skip) {
-
-      if(diff_stack.back() != view_t::COMMON && ch[i] == '\n') {
-
-        output_characters(CARRIAGE_RETURN_SYMBOL, diff_stack.back());
-        output_character(ch[i], view_t::COMMON);
-
-      } else {
-
-        output_characters(str, diff_stack.back());
-
-      }
-
-    }
-
-    if(ch[i] == '\n') {
-
-      if(!change_ending_space.empty()) {
-
-        output_characters(change_ending_space, view_t::COMMON);
-        change_ending_space = "";
-        change_ending_operation = view_t::UNSET;
-
-      }
-
-      if(diff_stack.back() != view_t::COMMON)
-        change_starting_line = true;
-
-      if(is_after_change) {
-
-        is_after_change = false;
-        is_after_additional = true;
-
-      } else if(is_after_additional) {
-
-        ++after_edit_count;
-
-        if((in_mode(LINE) && (!in_mode(FUNCTION) || !in_function.size()) && after_edit_count == number_context_lines) || (in_mode(FUNCTION) && !in_function.size())) {
-
-          is_after_additional = false;
-          after_edit_count = 0;
-          wait_change = true;
-          last_context_line = line_number_delete;
-
-          end_buffer(context, close_num_spans);
-          last_character_operation = view_t::UNSET;      
-
-        }
-
-      } else if(wait_change && ((in_mode(LINE) && (!in_mode(FUNCTION) || !in_function.size()) && number_context_lines != 0) || (in_mode(FUNCTION) && in_function.size()))) {
-
-        if(in_mode(LINE) && (!in_mode(FUNCTION) || !in_function.size()) && length >= number_context_lines)
-          additional_context.pop_front(), --length;
-
-        end_buffer(context, close_num_spans);
-        last_character_operation = view_t::UNSET;
-
-        additional_context.push_back(context.str());
-        ++length;
-
-      }
-
-      if(diff_stack.back() != INSERT) ++line_number_delete;
-      if(diff_stack.back() != DELETE) ++line_number_insert;
-
-      context.str("");
-
-    }
-
-  }
-
-  if(diff_stack.back() != view_t::COMMON) is_after_change  = true;
+void diffdoc_view::characters(const char * ch, int len) {
 
 }
--- src/view/diffdoc_view.hpp
+++ src/view/diffdoc_view.hpp
@@ -1,5 +1,5 @@
-#ifndef INCLUDED_UNIFIED_VIEW_HPP
-#define INCLUDED_UNIFIED_VIEW_HPP
+#ifndef INCLUDED_DIFFDOC_VIEW_HPP
+#define INCLUDED_DIFFDOC_VIEW_HPP
 
 #include <view.hpp>
 
@@ -12,64 +12,24 @@
 #include <fstream>
 #include <iostream>
 
-class unified_view : public view_t {
+class diffdoc_view : public view_t {
 
 private:
 
-  typedef unsigned long context_mode;
-
-  static const context_mode LINE     = 1 << 0;
-  static const context_mode FUNCTION = 1 << 1;
-  static const context_mode ALL      = 1 << 2;
-
-  context_mode modes;
-
-  int last_character_operation;
-
-  size_t line_number_delete;
-  size_t line_number_insert;
-
-  std::ostringstream context;
-
-  size_t number_context_lines;
-
-  bool is_after_change;
-  bool wait_change;
-  std::vector<bool> in_function;
-
-  boost::any context_type;
-  std::list<std::string>::size_type length;
-  std::list<std::string> additional_context;
-
-  bool is_after_additional;
-  size_t after_edit_count;
-  size_t last_context_line;
-
-  bool change_starting_line;
-  std::string change_ending_space;
-  int change_ending_operation;
-
-  unsigned int close_num_spans;
-
 public:
 
-  unified_view(const std::string & output_filename,
+  diffdoc_view(const std::string & output_filename,
                const std::string & syntax_highlight,
                const std::string & theme,
                bool ignore_all_whitespace,
                bool ignore_whitespace,
                bool ignore_comments,
-               bool is_html,
-               boost::any context_type);
-  virtual ~unified_view();
+               bool is_html);
+  virtual ~diffdoc_view();
 
 private:
 
   virtual void reset_internal();
-
-  bool in_mode(context_mode mode);
-
-  void output_additional_context();
 
   virtual void start_unit(const std::string & local_name,
                           const char * prefix,
@@ -92,7 +52,6 @@ private:
                            const char * URI);
   virtual void characters(const char * ch, int len);
 
-  context_mode context_string_to_id(const std::string & context_type_str) const;
 
 public:
   
