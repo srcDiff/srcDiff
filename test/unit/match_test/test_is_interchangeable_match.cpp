4cc91647a8ee1faf634d1559904e2f7faaf1832f
luke <lotting@bgsu.edu>
2020-08-24 17:52:35 -0400

    fixed small bugs

--- test/unit/match_test/test_is_interchangeable_match.cpp
+++ test/unit/match_test/test_is_interchangeable_match.cpp
@@ -14,11 +14,11 @@ ret_node_set_code("if(i <10){}","else if(i>5){}","C++"),
 ret_node_set_code("if(i <10){}","int i = 20;","C++"),
 ret_node_set_code("","","C++"),
 ret_node_set_code("","","C++"),
-}
+};
 
 
 
 BOOST_DATA_TEST_CASE(passes, bu::data::make(dinput1),d1){
-BOOST_TEST(srcdiff_match::is_interchangeable_match(d1.nsone,d2.nstwo));
-};
+BOOST_TEST(srcdiff_match::is_interchangeable_match(d1.nsone,d1.nstwo));
+}
 
