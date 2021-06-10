95b40c2220b04a318d6594c7b4002f5a407e2485
Evan <erbucha@bgsu.edu>
2021-06-10 18:47:16 -0400

    Update test of get_name

--- test/unit/match_test/test_get_name.cpp
+++ test/unit/match_test/test_get_name.cpp
@@ -14,9 +14,6 @@ namespace bu = boost::unit_test::data;
 
 
 
-
-
-
 //=========================================================================
 //===================== passing test set Start here =======================
 //=========================================================================
@@ -29,6 +26,44 @@ const std::shared_ptr<srcml_nodes> nodes[] = {
 	create_nodes("int parameter;", "C++"),
 	create_nodes("int param;", "C++"),
 	create_nodes("int decl;", "C++"),
+
+	create_nodes("public foo:", "C++"),
+	create_nodes("public foo:", "C++"),
+	create_nodes("public foo:", "C++"),
+	create_nodes("protected foo:", "C++"),
+	create_nodes("protected foo:", "C++"),
+	create_nodes("protected foo:", "C++"),
+	create_nodes("private foo:", "C++"),
+	create_nodes("private foo:", "C++"),
+	create_nodes("private foo:", "C++"),
+
+	create_nodes("for (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("for (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("for (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("for (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("foreach (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("foreach (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("foreach (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("foreach (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("while (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("while (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("while (int i = 0; i < count; ++i) {}(", "C++"),
+	create_nodes("while (int i = 0; i < count; ++i) {}(", "C++"),
+
+	create_nodes("if(i < 10) {} else {}(", "C++"),
+	create_nodes("if(i < 10) {} else {}(", "C++"),
+	create_nodes("if(i < 10) {} else {}(", "C++"),
+	create_nodes("if(i < 10) {} else {}(", "C++"),
+
+	create_nodes("expr_stmt("");(", "C++"),
+	create_nodes("expr_stmt("");(", "C++"),
+	create_nodes("expr_stmt("");(", "C++"),
+	create_nodes("decl_stmt foo;(", "C++"),
+	create_nodes("decl_stmt foo;(", "C++"),
+	create_nodes("decl_stmt foo;(", "C++"),
+	create_nodes("return 0;(", "C++"),
+	create_nodes("return 0;(", "C++"),
+	create_nodes("return 0;(", "C++")
 };
 
 
@@ -40,16 +75,61 @@ const int name_start_pos[] = {
 	1,
 	4,
 	4,
+
+	0,
+	1,
+	1,
+	4,
+	4,
+
+	0,
+	1,
+	1,
+	4,
+	4,
+
+	0,
+	1,
+	1,
+	4,
+	4,
+
+	0,
+	1,
+	1,
+	4,
+	4,
+
+	0,
+	1,
+	1,
+	4,
+	4,
+
+	0,
+	1,
+	1,
+	4,
+	4,
+
+	0,
+	1,
+	1
 };
 
 
+const std::string names[] = {
+  "i",
+};
 
-BOOST_DATA_TEST_CASE(passes, bu::make(nodes) ^ bu::make(name_start_pos), node, name_start_pos, rhs) {
+
+BOOST_DATA_TEST_CASE(passes, bu::make(nodes) ^ bu::make(name_start_pos) ^ bu::make(names), node, name_start_pos, rhs) {
 
 	std::cerr << node << ":" << name_start_pos << ":" << rhs << '\n';
 
-    BOOST_TEST(get_name(*node, name_start_pos) == rhs);
+	  BOOST_TEST(get_name(*node, name_start_pos) == rhs);
 }
+
 
 
 //=========================================================================
