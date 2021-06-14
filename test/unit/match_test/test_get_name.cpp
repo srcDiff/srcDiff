d66a919db0a7c04165af425d04e56cc59e757980
Evan <erbucha@bgsu.edu>
2021-06-14 10:40:59 -0400

    test_get_name remove repeated test cases

--- test/unit/match_test/test_get_name.cpp
+++ test/unit/match_test/test_get_name.cpp
@@ -13,142 +13,52 @@
 namespace bu = boost::unit_test::data;
 
 
-
-//=========================================================================
-//===================== passing test set Start here =======================
-//=========================================================================
-
-
 const std::shared_ptr<srcml_nodes> nodes[] = {
-	
-	create_nodes("int i", "C++"),
-	create_nodes("int decl_stmt;", "C++"),
-	create_nodes("int parameter;", "C++"),
-	create_nodes("int param;", "C++"),
-	create_nodes("int decl;", "C++"),
+					      
+        create_nodes("int i;", "C++"),
+	create_nodes("int i;", "C++"),
 
 	create_nodes("public foo:", "C++"),
-	create_nodes("public foo:", "C++"),
-	create_nodes("public foo:", "C++"),
-	create_nodes("protected foo:", "C++"),
-	create_nodes("protected foo:", "C++"),
-	create_nodes("protected foo:", "C++"),
-	create_nodes("private foo:", "C++"),
-	create_nodes("private foo:", "C++"),
-	create_nodes("private foo:", "C++"),
 
-	create_nodes("for (int i = 0; i < count; ++i) {}(", "C++"),
-	create_nodes("for (int i = 0; i < count; ++i) {}(", "C++"),
-	create_nodes("for (int i = 0; i < count; ++i) {}(", "C++"),
-	create_nodes("for (int i = 0; i < count; ++i) {}(", "C++"),
 	create_nodes("foreach (int i = 0; i < count; ++i) {}(", "C++"),
 	create_nodes("foreach (int i = 0; i < count; ++i) {}(", "C++"),
-	create_nodes("foreach (int i = 0; i < count; ++i) {}(", "C++"),
-	create_nodes("foreach (int i = 0; i < count; ++i) {}(", "C++"),
-	create_nodes("while (int i = 0; i < count; ++i) {}(", "C++"),
-	create_nodes("while (int i = 0; i < count; ++i) {}(", "C++"),
-	create_nodes("while (int i = 0; i < count; ++i) {}(", "C++"),
-	create_nodes("while (int i = 0; i < count; ++i) {}(", "C++"),
-
-	create_nodes("if(i < 10) {} else {}(", "C++"),
-	create_nodes("if(i < 10) {} else {}(", "C++"),
-	create_nodes("if(i < 10) {} else {}(", "C++"),
-	create_nodes("if(i < 10) {} else {}(", "C++"),
-
-	create_nodes("expr_stmt("");(", "C++"),
-	create_nodes("expr_stmt("");(", "C++"),
-	create_nodes("expr_stmt("");(", "C++"),
-	create_nodes("decl_stmt foo;(", "C++"),
-	create_nodes("decl_stmt foo;(", "C++"),
-	create_nodes("decl_stmt foo;(", "C++"),
-	create_nodes("return 0;(", "C++"),
-	create_nodes("return 0;(", "C++"),
-	create_nodes("return 0;(", "C++")
 };
 
-
-
-const int name_start_pos[] = {
+const int names_start_pos[] = {
 	
-	0,
-	1,
-	1,
-	4,
-	4,
+         3,  // int i;
+	 8,
+	 
+	 3,  // public foo:
+	 
+	 13, // foreach (int i = 0; i < count; ++i) {}(
+	 40,
 
-	0,
-	1,
-	1,
-	4,
-	4,
-
-	0,
-	1,
-	1,
-	4,
-	4,
-
-	0,
-	1,
-	1,
-	4,
-	4,
-
-	0,
-	1,
-	1,
-	4,
-	4,
-
-	0,
-	1,
-	1,
-	4,
-	4,
-
-	0,
-	1,
-	1,
-	4,
-	4,
-
-	0,
-	1,
-	1
+	 
 };
-
 
 const std::string names[] = {
-  "i",
+
+	"int",
+	"i",
+	
+        "foo",
+	
+	"i",
+	"count",
 };
 
 
-BOOST_DATA_TEST_CASE(passes, bu::make(nodes) ^ bu::make(name_start_pos) ^ bu::make(names), node, name_start_pos, rhs) {
 
-	std::cerr << node << ":" << name_start_pos << ":" << rhs << '\n';
+BOOST_DATA_TEST_CASE(passes, bu::make(nodes) ^ bu::make(names_start_pos) ^ bu::make(names), node, names_start_pos, rhs) {
+ 
+        //std::cerr << node << ":" << names_start_pos << ":" << rhs << '\n';
 
-	  BOOST_TEST(get_name(*node, name_start_pos) == rhs);
+        BOOST_TEST(get_name(*node, names_start_pos) == rhs);
+
+	std::cerr << "\n" << get_name(*node, names_start_pos) << "\n\n";
+
 }
 
-
-
-//=========================================================================
-// ==================== passing test set End here =========================
-//=========================================================================
-
-
-
-
-
-
-//==========================================================================
-// ====================== failed test set Start here =======================
-//==========================================================================
-
-
-
-//==========================================================================
-// ====================== failed test set End here =========================
-//==========================================================================
 
 
