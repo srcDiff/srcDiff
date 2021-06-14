6418260b42ad9acff51c8f616eb19dfff62507d5
Evan <erbucha@bgsu.edu>
2021-06-14 12:35:58 -0400

    Add a couple tests

--- test/unit/match_test/test_extract_name.cpp
+++ test/unit/match_test/test_extract_name.cpp
@@ -25,14 +25,29 @@ namespace bu = boost::unit_test::data;
 	
 const std::shared_ptr<srcml_nodes> nodes[] = {
 	
-
 	
 	create_nodes("int i", "C++"),
+	create_nodes("int i", "C++"),
+	create_nodes("int i", "C++"),
+	create_nodes("int i", "C++"),
+	
 	create_nodes("int decl_stmt;", "C++"),
 	create_nodes("int parameter;", "C++"),
 	create_nodes("int param;", "C++"),
 	create_nodes("int decl;", "C++"),
 
+	create_nodes("const std::shared_ptr<srcml_nodes> nodes[] = {", "C++"),
+	
+	create_nodes("const", "C++"),
+	create_nodes("const", "C++"),
+	create_nodes("const", "C++"),
+	create_nodes("const", "C++"),
+
+	create_nodes("int numbers[5] = { 1, 2, 3, 4, 5 };", "C++"),
+
+	create_nodes("int numbers[5];", "C++"),
+	create_nodes("int numbers[5];", "C++"),
+	
 };
 
 
@@ -42,12 +57,26 @@ const int start_pos[] = {
 
 	0,
 	1,
+	2,
+	3,
+	
+	1,
 	1,
 	4,
 	4,
 	
+	2,
 	
+	0,
+	1,
+	2,
+	3,
 
+	0,
+	
+	7,
+        0,
+	
 };
 
 
@@ -57,21 +86,35 @@ const std::string names[] = {
 
 
 	"i",
+	"int i",
+	"int i",
+	"int i",
+	
 	"decl_stmt",
 	"parameter",
 	"param",
 	"decl",
 
+	"",
 	
+	"",
+	"",
+	"",
+	"",
 
-
+	"int",
+	
+	"ee",
+	"ww",
+	
 };
 
 
 
 BOOST_DATA_TEST_CASE(passes, bu::make(nodes) ^ bu::make(start_pos) ^ bu::make(names), node, start_pos, rhs) {
 
-	std::cerr << node->at(start_pos)->name << ":" << start_pos << ":" << rhs << '\n';
+  
+	std::cerr << node->at(start_pos)->name << ":" << start_pos << ":" << rhs << "\n\n";
 
     BOOST_TEST(extract_name(*node, start_pos) == rhs);
 }
