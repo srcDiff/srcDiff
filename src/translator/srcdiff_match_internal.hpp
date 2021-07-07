f9a5f95798a2f6347f1828ef6d45161a5af115e0
Bailejd <bailejd@bgsu.edu>
2021-07-07 13:51:01 -0400

    Add is_single_call_expr function to header

--- src/translator/srcdiff_match_internal.hpp
+++ src/translator/srcdiff_match_internal.hpp
@@ -11,6 +11,7 @@ std::string get_decl_name(const srcml_nodes & nodes, int start_pos);
 std::string extract_name(const srcml_nodes & nodes, int start_pos);
 void skip_tag(const srcml_nodes & nodes, int & start_pos);
 void top_level_name_seek(const srcml_nodes & nodes, int & start_pos);
+bool is_single_call_expr(const srcml_nodes & nodes, int start_pos);
 
 #endif
 
