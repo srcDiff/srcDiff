067a85e4d7d657effbab7238d0e3f7c845a9b8c2
Bailejd <bailejd@bgsu.edu>
2021-07-26 14:02:04 -0400

    Add conditional_has_block function

--- src/translator/srcdiff_match_internal.hpp
+++ src/translator/srcdiff_match_internal.hpp
@@ -15,5 +15,6 @@ void top_level_name_seek(const srcml_nodes & nodes, int & start_pos);
 bool is_single_call_expr(const srcml_nodes & nodes, int start_pos);
 std::string get_function_type_name(const srcml_nodes & nodes, int start_pos);
 std::string get_for_condition(const srcml_nodes & nodes, int start_pos);
+bool conditional_has_block(const node_set & set);
 
 #endif
