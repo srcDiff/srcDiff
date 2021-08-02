042106d013de8c7647bd2065a2abd2b26f049870
Bailejd <bailejd@bgsu.edu>
2021-08-02 14:28:48 -0400

    Move if statment tests to separate test case

--- test/unit/match_test/test_conditional_has_block.cpp
+++ test/unit/match_test/test_conditional_has_block.cpp
@@ -11,29 +11,26 @@
 
 namespace bu = boost::unit_test::data;
 
-const node_set_data nodes[] = {
+const node_set_data non_if_nodes[] = {
 
     // Conditional with block
     create_node_set("while(true) {}", "C++"),
     create_node_set("switch(value) {}", "C++"),
     create_node_set("do {} while(i <= 10);", "C++"),
-    create_node_set("if(true) {}", "C++"),
     create_node_set("for(int i = 0; i <= 10; i++) {}", "C++"),
     create_node_set("foreach(const QString &str, values) {}", "C++"),
 
     // Conditional without block
     create_node_set("while(x <= 10) cout << x;", "C++"),
-    create_node_set("if(x <= 10) return true;", "C++"),
     create_node_set("for(int i = 0; i <= 10; i++) cout << i;", "C++"),
     create_node_set("foreach(const QString &str, values) cout << str;", "C++"),
     create_node_set("(value % 2 == 0) ? cout << \"even\" : cout << \"odd\";", "C++"),
 
 };
 
-const bool result[] = {
+const bool non_if_result[] = {
 
     // Conditional with block
-    true,
     true,
     true,
     true,
@@ -45,11 +42,37 @@ const bool result[] = {
     false,
     false,
     false,
+
+};
+
+const node_set_data if_nodes[] = {
+
+    // If with block
+    create_node_set("if(true) {}", "C++"),
+
+    // If without block
+    create_node_set("if(true) return true;", "C++"),
+
+};
+
+const bool if_result[] = {
+
+    // If with block
+    true,
+
+    // If without block
     false,
 
 };
 
-BOOST_DATA_TEST_CASE(passes, bu::make(nodes) ^ bu::make(result), node, rhs) {
+BOOST_DATA_TEST_CASE(non_ifs, bu::make(non_if_nodes) ^ bu::make(non_if_result), node, rhs) {
 
     BOOST_TEST(conditional_has_block(node.set) == rhs);
+}
+
+BOOST_DATA_TEST_CASE(ifs, bu::make(if_nodes) ^ bu::make(if_result), node, rhs) {
+
+    node_set first = get_first_child(node.set);
+
+    BOOST_TEST(conditional_has_block(first) == rhs);
 }
