776a73b9376ea435ea242d88037bb46d859181f4
Evan <erbucha@bgsu.edu>
2021-08-07 14:59:57 -0400

    Cleaned up/Changed how testing is set up

--- test/unit/match_test/test_name_list_similarity.cpp
+++ test/unit/match_test/test_name_list_similarity.cpp
@@ -8,6 +8,7 @@
 #include <boost/test/data/test_case.hpp>
 #include <string>
 #include <vector>
+#include <utility>
 #include <test_utils.hpp>
 
 namespace bu = boost::unit_test::data;
@@ -32,110 +33,93 @@ struct string_vector_wrapper {
 };
 
 
-const string_vector_wrapper original_name_lists[] = {
+const string_vector_wrapper name_pairs[] = {
 
-        std::vector<std::string>{},
+	std::make_pair(std::vector<std::string>{},
+		       std::vector<std::string>{}),
+	
+	std::make_pair(std::vector<std::string>{},
+		       std::vector<std::string>{"one", "two"}),
+	
+        std::make_pair(std::vector<std::string>{"one", "two"},
+		       std::vector<std::string>{}),
 
-	std::vector<std::string>{},
+	std::make_pair(std::vector<std::string>{"a", "Area"},
+		       std::vector<std::string>{"b", "Area"}),
 
-	std::vector<std::string>{"one", "two"},
+	std::make_pair(std::vector<std::string>{"a", "b", "c"},
+		       std::vector<std::string>{"a", "b", "d", "d"}),
+	
+	std::make_pair(std::vector<std::string>{"a", "b", "c"},
+		       std::vector<std::string>{"a ", "b", "d", "d"}),
+	
+	std::make_pair(std::vector<std::string>{"b", "b", "a"},
+		       std::vector<std::string>{"a", "b", "z"}),
+	
+	std::make_pair(std::vector<std::string>{"1", "2", "3", "4"},
+		       std::vector<std::string>{"1", "2", "3", "4"}),
+	
+	
+	std::make_pair(std::vector<std::string>{"1", "2", "3", "4", "5",
+	                                        "6", "7", "8", "9", "10",
+	                                        "11", "12", "13", "14", "15",
+	                                        "16", "17", "18", "19", "20"},
+	  
+	               std::vector<std::string>{"1", "2", "3", "4", "5",
+						"6", "7", "8", "9", "10",
+						"11", "12", "13", "14", "15",
+						"16", "17", "18", "19", "20"}),
+	
+	
+	std::make_pair(std::vector<std::string>{"1", "2", "3", "4", "5",
+						"6", "7", "8", "9", "10",
+						"11", "12", "13", "14", "15",
+						"16", "17", "18", "19", "20"},
+	  
+	               std::vector<std::string>{"2", "3", "4", "5",
+						"6", "7", "8", "9", "10",
+						"11", "12", "13", "14", "15",
+						"16", "17", "18", "19", "20"}),
+	
+	
+	std::make_pair(std::vector<std::string>{"int z = 4;", "string name;",
+                                               "for(int i; i < 10; i++){ cout << i; }"},
+	  
+	               std::vector<std::string>{"int z = 5;", "string name;",
+					        "for(int j; j < 15; j++){ cout << j; }"}),
+	
 
-        std::vector<std::string>{"a", "Area"},
+	std::make_pair(std::vector<std::string>{"a", "b", "c", "d", "e"},
+		       std::vector<std::string>{"a", "bc", "c", "d"}),
 
-	std::vector<std::string>{"a", "b", "c",},
+	std::make_pair(std::vector<std::string>{"a", "b", "c", "d", "e", "f"},
+		       std::vector<std::string>{"a", "b", "c"}),
 
-	std::vector<std::string>{"a", "b", "c",},
+	std::make_pair(std::vector<std::string>{"1", "3", "5"},
+		       std::vector<std::string>{"1", "2", "3", "4", "5"}),
 
-	std::vector<std::string>{"b", "b", "a",},
+	std::make_pair(std::vector<std::string>{"-1", "1", "3", "5"},
+		       std::vector<std::string>{"0", "1", "2", "3", "4", "5"}),
 
-	std::vector<std::string>{"1", "2", "3", "4"},
+	std::make_pair(std::vector<std::string>{"int i = 0;", "int p = 1;",
+						"int w = 3;", "int c = 9;"},
+	  
+	               std::vector<std::string>{"int i = 0;", "bool isThere = false;",
+						"bool changed = false;", "int p = 1;",
+						"int w = 3;", "char grade = 'a';",
+						"int c = 9;"}),
 
-	std::vector<std::string>{"1", "2", "3", "4", "5",
-	                         "6", "7", "8", "9", "10",
-	                         "11", "12", "13", "14", "15",
-	                         "16", "17", "18", "19", "20"},
+	std::make_pair(std::vector<std::string>{"a", "b", "c", "d", "e", "f"},
+		       std::vector<std::string>{"a", "f", "b", "c", "d", "e"}),
 
-	std::vector<std::string>{"1", "2", "3", "4", "5",
-	                         "6", "7", "8", "9", "10",
-	                         "11", "12", "13", "14", "15",
-	                         "16", "17", "18", "19", "20"},
+	std::make_pair(std::vector<std::string>{"a", "f"},
+		       std::vector<std::string>{"a", "b", "c", "d", "e", "f"}),
 
-	std::vector<std::string>{"int z = 4;", "string name;",
-                                 "for(int i; i < 10; i++){ cout << i; }"},
+	std::make_pair(std::vector<std::string>{"1", "2", "3", "4", "5"},
+		       std::vector<std::string>{"5", "5", "4", "3", "2", "2", "2", "1"}),
 
-	std::vector<std::string>{"a", "b", "c", "d", "e"},
-
-	std::vector<std::string>{"a", "b", "c", "d", "e", "f"},
-
-	std::vector<std::string>{"1", "3", "5"},
-
-	std::vector<std::string>{"-1", "1", "3", "5"},
-
-	std::vector<std::string>{"int i = 0;", "int p = 1;",
-				 "int w = 3;", "int c = 9;"},
-
-	std::vector<std::string>{"a", "b", "c", "d", "e", "f"},
-
-	std::vector<std::string>{"a", "f"},
-
-	std::vector<std::string>{"1", "2", "3", "4", "5"},
-
-	std::vector<std::string>{"1", "2", "3", "4", "5"},
-							
-};
-
-
-const string_vector_wrapper modified_name_lists[] = {
-
-	std::vector<std::string>{},
-
-	std::vector<std::string>{"one", "two"},
-
-	std::vector<std::string>{},
-
-        std::vector<std::string>{"b", "Area"},
-
-	std::vector<std::string>{"a", "b", "d", "d"},
-
-	std::vector<std::string>{"a ", "b", "d", "d"},
-
-	std::vector<std::string>{"a", "b", "z"},
-
-	std::vector<std::string>{"1", "2", "3", "4"},
-
-	std::vector<std::string>{"1", "2", "3", "4", "5",
-	                         "6", "7", "8", "9", "10",
-	                         "11", "12", "13", "14", "15",
-	                         "16", "17", "18", "19", "20"},
-
-	std::vector<std::string>{"2", "3", "4", "5",
-	                         "6", "7", "8", "9", "10",
-	                         "11", "12", "13", "14", "15",
-	                         "16", "17", "18", "19", "20"},
-
-	std::vector<std::string>{"int z = 5;", "string name;",
-                                 "for(int j; j < 15; j++){ cout << j; }"},
-
-	std::vector<std::string>{"a", "bc", "c", "d"},
-
-	std::vector<std::string>{"a", "b", "c"},
-
-	std::vector<std::string>{"1", "2", "3", "4", "5"},
-
-	std::vector<std::string>{"0", "1", "2", "3", "4", "5"},
-
-	std::vector<std::string>{"int i = 0;", "bool isThere = false;",
-                                 "bool changed = false;", "int p = 1;",
-				 "int w = 3;", "char grade = 'a';",
-				 "int c = 9;"},
-
-	std::vector<std::string>{"a", "f", "b", "c", "d", "e"},
-
-	std::vector<std::string>{"a", "b", "c", "d", "e", "f"},
-
-        std::vector<std::string>{"5", "5", "4", "3", "2", "2", "2", "1"},
-
-	std::vector<std::string>{"5", "4", "3", "2", "1"},
+	std::make_pair(std::vector<std::string>{"1", "2", "3", "4", "5"},
+		       std::vector<std::string>{"5", "4", "3", "2", "1"}),
 };
 
 
@@ -183,9 +167,9 @@ const int similarities[] = {
 };
 
 
-BOOST_DATA_TEST_CASE(passes, bu::make(original_name_lists) ^ bu::make(modified_name_lists) ^ bu::make(similarities), original, modified, rhs){
+BOOST_DATA_TEST_CASE(passes, bu::make(name_pairs) ^ bu::make(similarities), pair, original, modified, rhs){
 
-        BOOST_TEST(name_list_similarity(original.contents, modified.contents) == rhs);
+        BOOST_TEST(name_list_similarity(pair.first, pair.second) == rhs);
 	
 }
 
