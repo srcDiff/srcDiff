70a03449c324d0c9db908d74cf1272af7297b584
Mitch J <mjacovet@kent.edu>
2024-03-29 10:20:29 -0400

    Patch incorrect choice of free function in libsrcml

--- vcpkg/srcml/portfile.cmake
+++ vcpkg/srcml/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
   REF b5ac74e76d9bc5df55805ae01f1ec6765feb523f
   SHA512 2800ccb4781e6a8cf409d229a49f03ab8a31f327946c619052f043fdd7af2e8293da687df2cdfc31cd908eed915ad98f1cd7ad0487e50227ccbeacd41ff01068
   HEAD_REF main
-  PATCHES "skip-client-and-package.patch"
+  PATCHES "skip-client-and-package.patch" "fix-free.patch"
 )
 
 vcpkg_cmake_configure(
