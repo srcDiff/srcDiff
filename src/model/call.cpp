dd858477a140ed0b4f3ed986c25e311e523763e5
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2024-05-09 13:14:46 -0400

    Change call to conform to named_construct interface

--- src/model/call.cpp
+++ src/model/call.cpp
@@ -22,16 +22,16 @@
 
 #include <srcdiff_text_measure.hpp>
 
-std::shared_ptr<const construct> call::function_name() const {
-    std::shared_ptr<const name_t> full_name = name();
+std::shared_ptr<const name_t> call::name() const {
+    std::shared_ptr<const name_t> full_name = named_construct::name();
     if(full_name->root_term()->is_simple()) return full_name;
 
-    return full_name->children().back();
+    return std::static_pointer_cast<const name_t>(full_name->children().back());
 }
 
 bool call::is_matchable_impl(const construct & modified) const {
-    std::shared_ptr<const construct> original_name = function_name();
-    std::shared_ptr<const construct> modified_name = dynamic_cast<const call &>(modified).function_name();
+    std::shared_ptr<const name_t> original_name = name();
+    std::shared_ptr<const name_t> modified_name = dynamic_cast<const call &>(modified).name();
 
     srcdiff_text_measure text_measure(*original_name, *modified_name, false);
     text_measure.compute();
--- src/model/call.hpp
+++ src/model/call.hpp
@@ -30,7 +30,7 @@ public:
 
     call(const srcml_nodes & node_list, std::size_t & start, std::shared_ptr<srcdiff_output> out)
         : construct(node_list, start, out), named_construct(node_list, start, out) {}
-    std::shared_ptr<const construct> function_name() const;
+    virtual std::shared_ptr<const  name_t> name() const;
     virtual bool is_matchable_impl(const construct & modified) const;
 
 private:
