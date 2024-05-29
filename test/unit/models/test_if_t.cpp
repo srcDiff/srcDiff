687cec1e8349094c7d4a97ffb151f175bfbe6187
Andrew Symonds <notafan56@gmail.com>
2024-05-29 14:28:46 -0400

    Rearranged test data to match with the proper test case

--- test/unit/models/test_if_t.cpp
+++ test/unit/models/test_if_t.cpp
@@ -15,17 +15,6 @@ std::vector<std::tuple<std::string, std::string>> test_cases_has_real_block = {
     {"if (z) {a=0;}", "if"}
 };
 
-std::vector<std::tuple<std::string, std::string, std::string, std::string>> test_cases_is_block_matchable = {
-       // Original            // Modified
-      {"if (x) {y=1;}", "if", "if (y) {x=1;}", "if"},
-  // Just swap X and Y
-       // Original            // Modified
-      {"if (y) {x=1;}", "if", "if (x) {y=1;}", "if"},
-
-  
-  
-};
-
 BOOST_DATA_TEST_CASE(if_t_has_real_block, data::make(test_cases_has_real_block), code, construct_name) {
 
     construct_test_data test_data = create_test_construct(code, construct_name);
@@ -39,6 +28,15 @@ BOOST_DATA_TEST_CASE(if_t_has_real_block, data::make(test_cases_has_real_block),
 
 }
 
+std::vector<std::tuple<std::string, std::string, std::string, std::string>> test_cases_is_block_matchable = {
+       // Original            // Modified
+      {"if (x) {y=1;}", "if", "if (y) {x=1;}", "if"},
+  // Just swap X and Y
+       // Original            // Modified
+      {"if (y) {x=1;}", "if", "if (x) {y=1;}", "if"},  
+  
+};
+
 BOOST_DATA_TEST_CASE(if_t_is_block_matchable, data::make(test_cases_is_block_matchable), code1, construct_name1, code2, construct_name2) {
 
   construct_test_data original_data = create_test_construct(code1, construct_name1);
@@ -50,7 +48,7 @@ BOOST_DATA_TEST_CASE(if_t_is_block_matchable, data::make(test_cases_is_block_mat
   std::shared_ptr<if_t> if_construct_original = std::dynamic_pointer_cast<if_t>(original_data.test_construct);
   std::shared_ptr<if_t> if_construct_modified = std::dynamic_pointer_cast<if_t>(modified_data.test_construct);
 
-  BOOST_TEST(!if_construct_original->is_block_matchable(*if_construct_modified));
+  BOOST_TEST(!(if_construct_original)->is_block_matchable(*if_construct_modified));
 }
 
 
