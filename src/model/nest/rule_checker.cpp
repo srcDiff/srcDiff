4cc9476f0d03050ab8137bd7dd3cc58e44cba5eb
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2024-05-30 15:14:49 -0400

    Swap caller/param for can nest

--- src/model/nest/rule_checker.cpp
+++ src/model/nest/rule_checker.cpp
@@ -160,17 +160,17 @@ bool rule_checker::can_nest(const construct& modified) const {
 
 bool rule_checker::can_nest_internal(const construct& modified) const {
 
-  int block = is_block_type(modified);
+  int block = is_block_type(client);
 
   if(block == -1)
     return false;
 
   /** Only can nest a block into another block if it's parent is a block */
-  bool is_block = client.root_term_name() == "block" && modified.root_term_name() == "block";
-  bool parent_is_block = client.root_term()->get_parent() && client.root_term()->get_parent()->get_name() == "block";
+  bool is_block = modified.root_term_name() == "block" && client.root_term_name() == "block";
+  bool parent_is_block = modified.root_term()->get_parent() && modified.root_term()->get_parent()->get_name() == "block";
   if(is_block && !parent_is_block) return false;
 
-  if(is_nest_type(client, modified, block)) {
+  if(is_nest_type(modified, client, block)) {
 
     return true;
 
@@ -183,10 +183,10 @@ bool rule_checker::can_nest_same(const construct& modified) const {
 
   if(!can_nest_internal(modified)) return false;
 
-  std::shared_ptr<const construct> best_match = modified.find_best_descendent(client);
+  std::shared_ptr<const construct> best_match = client.find_best_descendent(modified);
   if(!best_match) return false;
 
-  srcdiff_text_measure match_measure(client, *best_match);
+  srcdiff_text_measure match_measure(*best_match, modified);
   match_measure.compute();
 
   srcdiff_text_measure measure(client, modified);
@@ -197,7 +197,7 @@ bool rule_checker::can_nest_same(const construct& modified) const {
 
   return (match_measure.similarity() >= measure.similarity() && match_measure.difference() <= measure.difference()) 
   || (match_min_size > 50 && min_size > 50 && (match_min_size / match_measure.similarity()) < (0.9 * (min_size / measure.similarity()))
-    && best_match->check_nest(client));
+    && best_match->check_nest(modified));
 
 }
 
