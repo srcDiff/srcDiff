56db388f9db3c16ca8761e14802f3c4d2f070f75
randrew <notafan56@gmail.com>
2024-06-15 17:57:40 -0400

    Added copyright header for test/unit/*, where needed

--- test/unit/shortest_edit_script/test_shortest_edit_script.c
+++ test/unit/shortest_edit_script/test_shortest_edit_script.c
@@ -1,9 +1,12 @@
-/*
- * SPDX-License-Identifier: GPL-3.0-only
-
- * Copyright (C) 2011-2024  SDML (www.srcDiff.org)
- * This file is part of the srcDiff translator.
+// SPDX-License-Identifier: GPL-3.0-only
+/**
+ * @file test_shortest_edit_script.c
+ *
+ * @copyright Copyright (C) 2014-2024 SDML (www.srcDiff.org)
+ *
+ * This file is part of the srcDiff Infrastructure.
  */
+
 #include <shortest_edit_script.h>
 #include <shortest_edit_script_private.h>
 
--- test/unit/utils/test_utils.hpp
+++ test/unit/utils/test_utils.hpp
@@ -1,9 +1,12 @@
-/*
- * SPDX-License-Identifier: GPL-3.0-only
-
- * Copyright (C) 2011-2024  SDML (www.srcDiff.org)
- * This file is part of the srcDiff translator.
+// SPDX-License-Identifier: GPL-3.0-only
+/**
+ * @file test_utils.hpp
+ *
+ * @copyright Copyright (C) 2020-2024 SDML (www.srcDiff.org)
+ *
+ * This file is part of the srcDiff Infrastructure.
  */
+
 #ifndef INCLUDED_TEST_UTILS_HPP
 #define INCLUDED_TEST_UTILS_HPP
 
