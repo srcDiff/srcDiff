c4128c7c1d2a3b582c6b9e937fec70d76b1c0aa8
Andrew Symonds <notafan56@gmail.com>
2024-06-27 23:20:07 -0400

    Added more testing for find_if(), find_else(), condition()

--- test/unit/models/test_if_stmt.cpp
+++ test/unit/models/test_if_stmt.cpp
@@ -21,18 +21,16 @@ namespace data = boost::unit_test::data;
 const std::string construct_type = "if_stmt";
 
 std::vector<std::tuple<std::string, std::string>> test_cases_find_if = {
-    {"if (y) {}"                           , "if (y) {}"},
-    /*
-    {"else if (x) {}"                      },
-    {"if (z>=1) {x=1;}"                    },
-    {"if (z!=1) {x=1;}"                    },
-    {"if (1) {a;}"                         },
-    {"if (x) {} else if (y) {}"            },
-    {"if (1) a; else if (2) b;"            },
-    {"if (1) {b;} else if (2) a;"          },
-    {"if (1) b; else if (2) {a;}"          },
-    {"if (1) {b;} else if (2) {a;} else {}"},
-    */
+    {"if (y) {}"                           , "if (y) {}"       },
+    {"else if (x) {}"                      , "if (x) {}"       },
+    {"if (z>=1) {x=1;}"                    , "if (z>=1) {x=1;}"},
+    {"if (z!=1) {x=1;}"                    , "if (z!=1) {x=1;}"},
+    {"if (1) {a;}"                         , "if (1) {a;}"     },
+    {"if (x) {} else if (y) {}"            , "if (x) {}"       },
+    {"if (1) a; else if (2) b;"            , "if (1) a;"       },
+    {"if (1) {b;} else if (2) a;"          , "if (1) {b;}"     },
+    {"if (1) b; else if (2) {a;}"          , "if (1) b;"       },
+    {"if (1) {b;} else if (2) {a;} else {}", "if (1) {b;}"     }
 };
 
 BOOST_DATA_TEST_CASE(if_stmt_find_if, data::make(test_cases_find_if), code, expected) {
@@ -43,28 +41,51 @@ BOOST_DATA_TEST_CASE(if_stmt_find_if, data::make(test_cases_find_if), code, expe
     std::shared_ptr<const if_stmt> if_stmt_construct = std::dynamic_pointer_cast<const if_stmt>(test_data.test_construct);
     std::shared_ptr<const if_t> if_child = if_stmt_construct->find_if();
 
-    BOOST_TEST(if_child == expected);
+    BOOST_TEST(if_child->to_string() == expected);
 }
 
-std::vector<std::tuple<std::string>> test_cases_find_else = {
-    {"if (y) {} else {}"                    },
-    {"if (y) {} else x;"                    },
-    {"else if (x) {} else {}"               },
-    {"if (z>=1) {x=1;} else {test::else()}" },
-    {"if (z!=1) {x=1;} else {int x = 0}"    },
-    {"if (1) {a;} else {}"                  },
-    {"if (x) {} else if (y) {} else {t;}"   },
-    {"if (1) a; else if (2) b; else a;"     },
-    {"if (1) {b;} else if (2) a; else {t;}" },
-    {"if (1) b; else if (2) {a;} else nuts;"},
-    {"if (1) {b;} else if (2) {a;} else {}" },
+std::vector<std::tuple<std::string, std::string>> test_cases_find_else = {
+    {"if (y) {} else {}"                    , "else {}"            },
+    {"if (y) {} else x;"                    , "else x;"            },
+    {"else if (x) {} else {}"               , "else {}"            },
+    {"if (z>=1) {x=1;} else {test::else()}" , "else {test::else()}"},
+    {"if (z!=1) {x=1;} else {int x = 0}"    , "else {int x = 0}"   },
+    {"if (1) {a;} else {}"                  , "else {}"            },
+    {"if (x) {} else if (y) {} else {t;}"   , "else {t;}"          },
+    {"if (1) a; else if (2) b; else a;"     , "else a;"            },
+    {"if (1) {b;} else if (2) a; else {t;}" , "else {t;}"          },
+    {"if (1) b; else if (2) {a;} else nuts;", "else nuts;"         },
+    {"if (1) {b;} else if (2) {a;} else {}" , "else {}"            },
 };
 
-BOOST_DATA_TEST_CASE(if_stmt_find_else, data::make(test_cases_find_else), code) {
+BOOST_DATA_TEST_CASE(if_stmt_find_else, data::make(test_cases_find_else), code, expected) {
 
     construct_test_data test_data = create_test_construct(code, construct_type);
     BOOST_TEST(test_data.test_construct);
 
     std::shared_ptr<const if_stmt> if_stmt_construct = std::dynamic_pointer_cast<const if_stmt>(test_data.test_construct);
-    BOOST_TEST(if_stmt_construct->find_else());
+    std::shared_ptr<const else_t> else_child = if_stmt_construct->find_else();
+
+    BOOST_TEST(else_child->to_string() == expected);
+}
+
+std::vector<std::tuple<std::string, std::string>> test_cases_is_syntax_similar_impl = {
+    {"if (x) {}"            , "x"           },
+    {"if (x>1) {}"          , "x>1"         },
+    {"else if (x<1) {}"     , "x<1"         },
+    {"if (else()) {}"       , "else()"      },
+    {"if (!test) {}"        , "!test"       },
+    {"if (a.out) {}"        , "a.out"       }   
+};
+
+BOOST_DATA_TEST_CASE(if_stmt_is_syntax_similar_impl, data::make(test_cases_is_syntax_similar_impl), code, expected) {
+
+    construct_test_data test_data = create_test_construct(code, construct_type);
+    BOOST_TEST(test_data.test_construct);
+
+    std::shared_ptr<const if_stmt> if_stmt_construct = std::dynamic_pointer_cast<const if_stmt>(test_data.test_construct);
+
+    std::shared_ptr<const construct> if_stmt_condition = if_stmt_construct->condition();
+
+    BOOST_TEST(if_stmt_condition->to_string() == expected);
 }
