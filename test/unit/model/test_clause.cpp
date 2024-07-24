0467ec141c421ea34d0f3ccf6b1f170deb86eccd
Andrew Symonds <notafan56@gmail.com>
2024-07-24 14:18:42 -0400

    Tried to implement for tests, didn't work

--- test/unit/model/test_clause.cpp
+++ test/unit/model/test_clause.cpp
@@ -19,23 +19,24 @@
 namespace data = boost::unit_test::data;
 
 std::vector<std::tuple<std::string, std::string, std::string>> test_cases_block = {
-    {"if (x<1){x+=1;}"              , "{x+=1;}"        , "if"   },
-    {"if (i < 5){test;}"            , "{test;}"        , "if"   },
-    {"if (i<0){rhs-=lhs;}"          , "{rhs-=lhs;}"    , "if"   },
-    {"if (i=0){i=1;}"               , "{i=1;}"         , "if"   },
-    {"if (i>0){f;g;}"               , "{f;g;}"         , "if"   },
-    {"if (i!=0){i=0;}"              , "{i=0;}"         , "if"   },
-    {"if (test){}"                  , "{}"             , "if"   },
-    {"if (!test){not_pass();}"      , "{not_pass();}"  , "if"   },
-    {"if (test && pass){throw i;}"  , "{throw i;}"     , "if"   },
-    {"if (*test)"                   , ""               , "if"   },
-    {"if (*test && !pass){null();}" , "{null();}"      , "if"   },
-    {"if (!test || !pass){t+=x;}"   , "{t+=x;}"        , "if"   },
-    {"else if (i<0){i--;}"          , "{i--;}"         , "if"   },
-    {"else if (i=0){i++;}"          , "{i++;}"         , "if"   },
-    {"else if (i>0){subtract();}"   , "{subtract();}"  , "if"   },
-    {"else if (i!=0){make_zero();}" , "{make_zero();}" , "if"   },
-    {"else if (test){test->pass();}", "{test->pass();}", "if"   }
+    {"if (x<1){x+=1;}"               , "{x+=1;}"        , "if"   },
+    {"if (i < 5){test;}"             , "{test;}"        , "if"   },
+    {"if (i<0){rhs-=lhs;}"           , "{rhs-=lhs;}"    , "if"   },
+    {"if (i=0){i=1;}"                , "{i=1;}"         , "if"   },
+    {"if (i>0){f;g;}"                , "{f;g;}"         , "if"   },
+    {"if (i!=0){i=0;}"               , "{i=0;}"         , "if"   },
+    {"if (test){}"                   , "{}"             , "if"   },
+    {"if (!test){not_pass();}"       , "{not_pass();}"  , "if"   },
+    {"if (test && pass){throw i;}"   , "{throw i;}"     , "if"   },
+    {"if (*test)"                    , ""               , "if"   },
+    {"if (*test && !pass){null();}"  , "{null();}"      , "if"   },
+    {"if (!test || !pass){t+=x;}"    , "{t+=x;}"        , "if"   },
+    {"else if (i<0){i--;}"           , "{i--;}"         , "if"   },
+    {"else if (i=0){i++;}"           , "{i++;}"         , "if"   },
+    {"else if (i>0){subtract();}"    , "{subtract();}"  , "if"   },
+    {"else if (i!=0){make_zero();}"  , "{make_zero();}" , "if"   },
+    {"else if (test){test->pass();}" , "{test->pass();}", "if"   },
+    // Can't do for?
 };
 
 BOOST_DATA_TEST_CASE(block_clause, data::make(test_cases_block), code, expected, construct_type) {
