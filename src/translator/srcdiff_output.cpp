0d7348fdff947938891dbfb17d99d4ed4db85e73
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2024-08-03 15:43:26 -0400

    Pass uri to start element write, now that srcML supports it

--- src/translator/srcdiff_output.cpp
+++ src/translator/srcdiff_output.cpp
@@ -572,7 +572,7 @@ void srcdiff_output::output_node_inner(const srcML::node & node) {
     srcml_write_start_element(wstate->unit,
       node.get_namespace()->get_prefix() ? node.get_namespace()->get_prefix()->c_str() : 0,
       node.get_name().c_str(),
-      nullptr//node.get_namespace()->get_uri().c_str()
+      node.get_namespace()->get_uri().c_str()
       );
 
     // copy all the attributes
