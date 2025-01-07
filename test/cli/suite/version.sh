5e3ceb1cb684ff78d3e913e943cbce51b1ac5e45
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2025-01-07 12:35:41 +0900

    Add version cli test

---
+++ test/cli/suite/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# @file version.sh
+#
+# @copyright Copyright (C) 2013-2024 SDML (www.srcDiff.org)
+#
+# This file is part of the srcDiff Infrastructure.
+#
+
+# test framework
+source $(dirname "$0")/../framework.sh
+
+define output <<- 'STDOUT'
+	srcdiff Version 1
+	STDOUT
+
+srcdiff --version | head -n 1
+check "$output"
+
+srcdiff -V | head -n 1
+check "$output"
