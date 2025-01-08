2cd67e2294fa77165e238dd3c89c7ed7deed6539
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2025-01-08 14:10:15 +0900

    Add help test

---
+++ test/cli/suite/help.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# @file version.sh
+#
+# @copyright Copyright (C) 2013-2024 SDML (www.srcDiff.org)
+#
+# This file is part of the srcDiff Infrastructure.
+#
+
+# test framework
+source $(dirname "$0")/../framework.sh
+
+srcdiff --help
+srcdiff -h
