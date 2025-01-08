979112ce6a3c4f4d69a0c097da67844710a5b778
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2025-01-08 14:11:07 +0900

    Update copyright

--- test/cli/suite/version.sh
+++ test/cli/suite/version.sh
@@ -3,7 +3,7 @@
 #
 # @file version.sh
 #
-# @copyright Copyright (C) 2013-2024 SDML (www.srcDiff.org)
+# @copyright Copyright (C) 2024-2024 SDML (www.srcDiff.org)
 #
 # This file is part of the srcDiff Infrastructure.
 #
