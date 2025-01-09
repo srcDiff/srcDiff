7133157e9f85bd1cd1d46d91c06ad86c8948bb35
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2025-01-09 13:31:06 +0900

    Add a multiple pairs test

---
+++ test/cli/suite/multiple-pairs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# @file file-pair.sh
+#
+# @copyright Copyright (C) 2024-2024 SDML (www.srcDiff.org)
+#
+# This file is part of the srcDiff Infrastructure.
+#
+
+# test framework
+source $(dirname "$0")/../framework.sh
+
+define original <<- 'SOURCE'
+	a;
+	SOURCE
+
+define modified <<- 'SOURCE'
+	b;
+	SOURCE
+
+define output <<- 'STDOUT'
+	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	<unit xmlns="http://www.srcML.org/srcML/src" xmlns:diff="http://www.srcML.org/srcDiff" revision="1.0.0">
+
+	<unit revision="1.0.0" language="C++" filename="sub/a.cpp|sub/b.cpp"><diff:delete type="replace"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></diff:delete><diff:insert type="replace"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></diff:insert>
+	</unit>
+
+	<unit revision="1.0.0" language="C++" filename="sub/b.cpp|sub/a.cpp"><diff:delete type="replace"><expr_stmt><expr><name>b</name></expr>;</expr_stmt></diff:delete><diff:insert type="replace"><expr_stmt><expr><name>a</name></expr>;</expr_stmt></diff:insert>
+	</unit>
+
+	</unit>
+	STDOUT
+
+xmlcheck "$output"
+
+createfile sub/a.cpp "$original"
+createfile sub/b.cpp "$modified"
+
+srcdiff sub/a.cpp sub/b.cpp sub/b.cpp sub/a.cpp
+check "$output"
+
+srcdiff sub/a.cpp sub/b.cpp sub/b.cpp sub/a.cpp -o sub/ab.xml
+check sub/ab.xml "$output"
