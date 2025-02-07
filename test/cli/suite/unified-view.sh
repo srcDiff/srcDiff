fa73732ce9f68972996bdc9266329df0fce657af
Andrew Symonds <asymond@bgsu.edu>
2025-02-07 09:39:13 -0500

    Added archive.sh, testing on unified-view.sh

---
+++ test/cli/suite/unified-view.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# @file unified-view.sh
+#
+# @copyright Copyright (C) 2024-2025 SDML (www.srcDiff.org)
+#
+# This file is part of the srcDiff Infrastructure.
+#
+
+# test framework
+source $(dirname "$0")/../framework.sh
+
+define original <<- 'SOURCE'
+	a;
+	SOURCE
+
+define modified <<- 'SOURCE'
+	b;
+	SOURCE
+
+define output <<- 'STDOUT'
+	\x1b[0m\x1b[0m\x1b[36m@@ -1 +1 @@\x1b[0m
+	STDOUT
+
+createfile sub/a.cpp "$original"
+createfile sub/b.cpp "$modified"
+
+srcdiff sub/a.cpp sub/b.cpp -u | head -n 1
+
+check "$output"
