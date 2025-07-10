c274133743f73d35b3bb402d71d3328b1a2ffa00
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2025-07-10 10:57:23 +0900

    Remove assumption that expr has a child in expr_stmt

--- src/model/expr_stmt.cpp
+++ src/model/expr_stmt.cpp
@@ -9,16 +9,22 @@
 
 #include <expr_stmt.hpp>
 
-bool expr_stmt::is_matchable_impl(const construct & modified) const {
-    const expr_stmt & modified_expr_stmt = dynamic_cast<const expr_stmt &>(modified);
+bool expr_stmt::is_matchable_impl(const construct& modified) const {
+    const expr_stmt& modified_expr_stmt = dynamic_cast<const expr_stmt&>(modified);
 
-    const expr_t & original_expr = dynamic_cast<const expr_t &>(*children().front());
-    const expr_t & modified_expr = dynamic_cast<const expr_t &>(*modified_expr_stmt.children().front());
+    const construct::construct_list& original_children = children();
+    const construct::construct_list& modified_children = modified_expr_stmt.children();
+
+    if(original_children.empty() && modified_children.empty()) return true;
+    if(original_children.empty() || modified_children.empty()) return false;
+
+    const expr_t& original_expr = dynamic_cast<const expr_t&>(*children().front());
+    const expr_t& modified_expr = dynamic_cast<const expr_t&>(*modified_expr_stmt.children().front());
 
     if(original_expr.is_single_call() && modified_expr.is_single_call())
         return original_expr.children().front()->is_matchable_impl(*modified_expr.children().front());
 
 
-    const srcdiff::measurer & expr_stmt_measure = *measure(modified);
+    const srcdiff::measurer& expr_stmt_measure = *measure(modified);
     return expr_stmt_measure.similarity() > 0 && expr_stmt_measure.difference() <= expr_stmt_measure.max_length();
 }
