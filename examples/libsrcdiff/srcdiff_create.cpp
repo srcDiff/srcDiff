2c0271c0e813393e3968fca42f6177436bd3cb85
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2026-05-09 16:16:17 -0400

    Add the start of some srcdiff library examples

---
+++ examples/libsrcdiff/srcdiff_create.cpp
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/**
+ * @file srcdiff_create.cpp
+ *
+ * @copyright Copyright (C) 2026-2026 SDML (www.srcDiff.org)
+ *
+ * This file is part of the srcDiff Infrastructure.
+ *
+ * Show basic libsrcdiff usage to create an archive.
+ */
+
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+
+    return 0;
+}
