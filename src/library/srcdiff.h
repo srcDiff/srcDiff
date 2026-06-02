15f197ceb42a0fa11062ce96ad22d701020b7364
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2026-06-02 14:22:52 -0400

    Update srcdiff library

--- src/library/srcdiff.h
+++ src/library/srcdiff.h
@@ -27,80 +27,19 @@ typedef SSIZE_T ssize_t;
 #define LIBSRCDIFF_EXPORT
 #endif
 
-  // temp forward declaration of srcDiff types
-  struct srcdiff_archive;
-  struct srcdiff_unit;
 
   // srcDiff convenience function - original and modified to/from srcDiff archive
-  int srcDiff(const char * original_filename, const char * modified_filename, const char * srcdiff_filename);
+  int srcDiff(const char * original_filename, 
+              const char * modified_filename, 
+              const char * srcdiff_filename);
 
-  LIBSRCDIFF_EXPORT struct srcdiff_archive* srcdiff_archive_create();
-
-  // srcdiff archive api
-  LIBSRCDIFF_EXPORT struct srcdiff_archive* srcdiff_archive_clone(const struct srcdiff_archive* archive);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_error_number(const struct srcdiff_archive* archive);
-  LIBSRCDIFF_EXPORT const char* srcdiff_archive_error_string(const struct srcdiff_archive* archive);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_write_unit(struct srcdiff_archive* archive, struct srcdiff_unit* unit);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_write_string(struct srcdiff_archive* archive, const char* s, int len);
-  LIBSRCDIFF_EXPORT void srcdiff_archive_close(struct srcdiff_archive* archive);
-  LIBSRCDIFF_EXPORT void srcdiff_archive_free(struct srcdiff_archive* archive);
-
-  LIBSRCDIFF_EXPORT int srcdiff_archive_write_open_filename(struct srcdiff_archive* archive, const char* srcml_filename);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_write_open_memory(struct srcdiff_archive* archive, char** buffer, size_t * size);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_write_open_FILE(struct srcdiff_archive* archive, FILE* srcml_file);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_write_open_fd(struct srcdiff_archive* archive, int srcml_fd);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_write_open_io(struct srcdiff_archive* archive, void * context, int (*write_callback)(void * context, const char* buffer, int len), int (*close_callback)(void * context));
-
-  LIBSRCDIFF_EXPORT int srcdiff_archive_read_open_filename(struct srcdiff_archive* archive, const char* srcml_filename);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_read_open_memory(struct srcdiff_archive* archive, const char* buffer, size_t buffer_size);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_read_open_FILE(struct srcdiff_archive* archive, FILE* srcml_file);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_read_open_fd(struct srcdiff_archive* archive, int srcml_fd);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_read_open_io(struct srcdiff_archive* archive, void * context, int (*read_callback)(void * context, char* buffer, int len), int (*close_callback)(void * context));
-
-  // add get_archive
-  // Do not mirror/implement.
-
-  // need a utility function to split attributes into: check if diff, original/modified
-
-  LIBSRCDIFF_EXPORT struct srcdiff_unit* srcdiff_archive_read_unit_header(struct srcdiff_archive* archive);
-  LIBSRCDIFF_EXPORT struct srcdiff_unit* srcdiff_archive_read_unit(struct srcdiff_archive* archive);
-  LIBSRCDIFF_EXPORT int srcdiff_archive_skip_unit(struct srcdiff_archive* archive);
-
-// transformation library. Not needed, but maybe a register ext func in future
-
-  // srcdiff unit api
-  LIBSRCDIFF_EXPORT struct srcdiff_unit* srcdiff_unit_create(struct srcdiff_archive* archive);
-  LIBSRCDIFF_EXPORT struct srcdiff_unit* srcdiff_unit_clone(const struct srcdiff_unit* unit);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_error_number(const struct srcdiff_unit* unit);
-  LIBSRCDIFF_EXPORT const char* srcdiff_unit_error_string(const struct srcdiff_unit* unit);
-  LIBSRCDIFF_EXPORT void srcdiff_unit_free(struct srcdiff_unit* unit);
-
-  // a srcdiff_unit get original/modified srcml_unit
-  // convience get original/modified and general get_version
-  LIBSRCDIFF_EXPORT const char* srcdiff_unit_get_srcml(struct srcdiff_unit* unit);
-  LIBSRCDIFF_EXPORT const char* srcdiff_unit_get_srcml_outer(struct srcdiff_unit* unit);
-  LIBSRCDIFF_EXPORT const char* srcdiff_unit_get_srcml_inner(struct srcdiff_unit* unit);
-
-  // parse be rename a difference/delta
-  // original/modified file (other srcML IO), original/modified srcml_unit
-  // do you always give srcml_unit and run on that
-  LIBSRCDIFF_EXPORT int srcdiff_unit_parse_filename(struct srcdiff_unit* unit, const char* src_filename);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_parse_memory(struct srcdiff_unit* unit, const char* src_buffer, size_t buffer_size);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_parse_FILE(struct srcdiff_unit* unit, FILE* src_file);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_parse_fd(struct srcdiff_unit* unit, int src_fd);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_parse_io(struct srcdiff_unit* unit, void * context, ssize_t (*read_callback)(void * context, void * buffer, size_t len), int (*close_callback)(void * context));
-
-  // unparse is not needed
-  LIBSRCDIFF_EXPORT const char* srcdiff_unit_get_src(struct srcdiff_unit* unit);
-  LIBSRCDIFF_EXPORT ssize_t srcdiff_unit_get_src_size(struct srcdiff_unit* unit);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_unparse_filename(struct srcdiff_unit* unit, const char* src_filename);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_unparse_memory(struct srcdiff_unit* unit, char** src_buffer, size_t * src_size);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_unparse_FILE(struct srcdiff_unit* unit, FILE* file);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_unparse_fd(struct srcdiff_unit* unit, int fd);
-  LIBSRCDIFF_EXPORT int srcdiff_unit_unparse_io(struct srcdiff_unit* unit, void * context, int (*write_callback)(void * context, const char* buffer, int len), int (*close_callback)(void * context));
+  struct srcdiff_config;
+  LIBSRCDIFF_EXPORT struct srcdiff_config* srcdiff_config_create();
+  LIBSRCDIFF_EXPORT struct srcml_unit    * srcdiff_create_delta (struct srcdiff_unit  * original_unit, 
+                                                                 struct srcdiff_unit  * modified_unit,
+                                                                 struct srcdiff_config* config);
 
   // use revision instead of version in naming
-
   /** Constant for original srcDiff revision number */
   #define SRCDIFF_REVISION_ORIGINAL 0
   /** Constant for modified srcDiff revision number */
