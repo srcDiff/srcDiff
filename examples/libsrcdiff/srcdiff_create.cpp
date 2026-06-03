83aa01becf5cf7eddd8330c30192dbff2c7a60c2
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2026-06-03 17:07:21 -0400

    Fix create example

--- examples/libsrcdiff/srcdiff_create.cpp
+++ examples/libsrcdiff/srcdiff_create.cpp
@@ -19,22 +19,24 @@ int main(int argc, char * argv[]) {
     srcml_archive_write_open_filename(archive, "srcdiff.xml");
     srcdiff_config* config = srcdiff_config_create(archive);
 
-    for(int i = 1; i < argc; i += 2) {
+    for(int i = 2; i < argc; i += 2) {
 
         // srcml original file
-        const int orginal_file_pos  = i - 1;
-        
+        const int original_file_pos  = i - 1;
+
         srcml_unit* original_unit = srcml_unit_create(archive);
-        srcml_unit_set_filename(original_unit, argv[orginal_file_pos]);
-        srcml_unit_parse_filename(original_unit, argv[orginal_file_pos]);
+        srcml_unit_set_filename(original_unit, argv[original_file_pos]);
+        srcml_unit_set_language(original_unit, srcml_check_extension(argv[original_file_pos]));
+        srcml_unit_parse_filename(original_unit, argv[original_file_pos]);
 
         const int modified_file_pos  = i;
         srcml_unit* modified_unit = srcml_unit_create(archive);
         srcml_unit_set_filename(modified_unit, argv[modified_file_pos]);
+        srcml_unit_set_language(modified_unit, srcml_check_extension(argv[modified_file_pos]));
         srcml_unit_parse_filename(modified_unit, argv[modified_file_pos]);
 
-        // unit is self-contained (namespace on unit)
         srcml_unit* unit = srcdiff_create_delta(original_unit, modified_unit, config);
+        srcml_archive_write_unit(archive, unit);
 
         srcml_unit_free(original_unit);
         srcml_unit_free(modified_unit);
