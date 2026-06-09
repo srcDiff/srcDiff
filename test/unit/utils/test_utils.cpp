e398e126d8f428d34717260ba1fa8c915e7ca069
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2026-06-09 15:18:53 -0400

    Fix test utils with previous srcml_convert changes

--- test/unit/utils/test_utils.cpp
+++ test/unit/utils/test_utils.cpp
@@ -49,9 +49,9 @@ std::shared_ptr<srcml_nodes> create_nodes(const std::string & code, const std::s
     srcml_archive_register_namespace(archive, "diff", "http://www.srcML.org/srcDiff");
 
     //create srcml_nodes
-    srcml_converter contNodes(archive, true);
+    srcml_converter contNodes(true);
     std::string source = code;
-    contNodes.convert(language, (void*)&source, &str_read, &str_close);
+    contNodes.convert(archive, language, (void*)&source, &str_read, &str_close);
     srcml_nodes testNode = contNodes.create_nodes();
 
     return std::make_shared<srcml_nodes>(testNode);
