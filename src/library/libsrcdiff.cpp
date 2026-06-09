16f1a99ad3a34378d6f033687d0923b16a3ebf02
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2026-06-09 15:18:34 -0400

    Copy with modifications extract src/revision from srcML

--- src/library/libsrcdiff.cpp
+++ src/library/libsrcdiff.cpp
@@ -11,14 +11,21 @@
 #include <srcdiff_types.hpp>
 
 #include <deltor.hpp>
-#include <output_stream.hpp>
 #include <input_stream.hpp>
+#include <output_stream.hpp>
+#include <operation.hpp>
+
 #include <client_options.hpp>
 
 #include <string>
 #include <optional>
 #include <fstream>
 #include <memory>
+
+#include <string_view>
+using namespace ::std::literals::string_view_literals;
+
+#include <libxml/parserInternals.h>
 
 class file_input {
 public:
@@ -162,4 +169,153 @@ struct srcml_unit* srcdiff_create_delta(struct srcdiff_config* configuration,
   config->manager->append_stream(modified_input);
 
   return config->deltor->create(srcml_unit_get_archive(original_unit), *config->manager, srcml_unit_get_language(original_unit), std::optional<std::string>(), std::optional<std::string>());
+}
+
+std::string extract_revision(const char* srcml, int size, int revision, bool text_only) {
+
+    std::string_view DIFF_PREFIX = "diff:"sv;
+
+    std::stack<srcdiff::operation> mode;
+    mode.push(srcdiff::COMMON);
+
+    std::string news;
+    const char* p = srcml;
+    const char* lastp = p;
+    while ((p = (const char*) memchr(p, '<', static_cast<size_t>(size - (p - srcml))))) {
+
+        bool inmode = mode.top() == srcdiff::COMMON || (revision == 0 && mode.top() == srcdiff::DELETE) || (revision == 1 && mode.top() == srcdiff::INSERT);
+
+        // output previous non-tag text
+        if (inmode) {
+            news.append(lastp, static_cast<size_t>(p - lastp));
+        }
+
+        auto sp = p;
+
+        // skip to end of tag
+        p = (const char*) memchr(p, '>', static_cast<size_t>(size - (p - srcml)));
+        ++p;
+
+        if (strncmp(sp + 1, DIFF_PREFIX.data(), DIFF_PREFIX.size()) == 0) {
+
+            const char* tstart = sp + 1 + DIFF_PREFIX.size();
+
+            if (strncmp(tstart, "delete", 6) == 0) {
+                mode.push(srcdiff::DELETE);
+            } else if (strncmp(tstart, "insert", 6) == 0) {
+                mode.push(srcdiff::INSERT);
+            } else if (strncmp(tstart, "ws", 2) != 0) {
+                mode.push(srcdiff::COMMON);
+            }
+
+        }
+        else if (*(sp + 1) == '/' && strncmp(sp + 2, DIFF_PREFIX.data(), DIFF_PREFIX.size()) == 0) {
+            if(strncmp(sp + 2 + DIFF_PREFIX.size(), "ws", 2) != 0) {
+                mode.pop();
+            }
+        }
+        else {
+            if (inmode && !text_only) {
+                news.append(sp, static_cast<size_t>(p - sp));
+            }
+        }
+
+        lastp = p;
+    }
+
+    bool inmode = mode.top() == srcdiff::COMMON || (revision == 0 && mode.top() == srcdiff::DELETE) || (revision == 1 && mode.top() == srcdiff::INSERT);
+    auto remaining_size = (size_t) size - (size_t) (lastp - srcml);
+    if (inmode && remaining_size > 0) {
+        news.append(lastp, remaining_size);
+    }
+
+    return news;
+}
+
+struct extract_context {
+    std::string src;
+    srcdiff::operation operation;
+    std::stack<srcdiff::operation> mode;  
+};
+
+// Extract source code from srcml
+std::string extract_src(std::string_view srcml, srcdiff::operation operation) {
+
+    extract_context context;
+    context.operation = operation;
+    context.mode.push(srcdiff::COMMON);
+
+    // parse the srcml collecting the (now needed) src
+    xmlSAXHandler charactersax;
+    memset(&charactersax, 0, sizeof(charactersax));
+    charactersax.initialized    = XML_SAX2_MAGIC;
+
+    charactersax.ignorableWhitespace = charactersax.characters = [](void* ctx, const xmlChar* ch, int len) {
+
+        auto ctxt = (xmlParserCtxtPtr) ctx;
+        if (ctxt == nullptr)
+            return;
+        auto context = (extract_context*) ctxt->_private;
+        if (context == nullptr)
+            return;
+
+        context->src.append((const char*) ch, static_cast<size_t>(len));
+    };
+
+    charactersax.startElementNs = [](void* ctx, const xmlChar* localname, const xmlChar* /* prefix */, const xmlChar* URI,
+                     int /* nb_namespaces */, const xmlChar** /* namespaces */,
+                     int /* nb_attributes */, int /* nb_defaulted */, const xmlChar** attributes) {
+
+        auto ctxt = (xmlParserCtxtPtr) ctx;
+        if (ctxt == nullptr)
+            return;
+        auto context = (extract_context*) ctxt->_private;
+        if (context == nullptr)
+            return;
+
+        if ("escape"sv == (const char*) localname && "http://www.srcML.org/srcML/src"sv == (const char*) URI) {
+            std::string svalue((const char *)attributes[0 * 5 + 3], static_cast<std::size_t>(attributes[0 * 5 + 4] - attributes[0 * 5 + 3]));
+
+            // use strtol() instead of atoi() since strtol() understands hex encoding of '0x0?'
+            char value = (char)strtol(svalue.data(), NULL, 0);
+
+            context->src.append(1, value);
+
+        } else if (context->operation && srcML::name_space::DIFF_NAMESPACE->get_uri() == (const char*) URI) {
+
+            if ((const char*) localname == "srcdiff::INSERT"sv)
+                context->mode.push(srcdiff::INSERT);
+            else if ((const char*) localname == "srcdiff::DELETE"sv)
+                context->mode.push(srcdiff::DELETE);
+            else
+                context->mode.push(srcdiff::COMMON);
+        }
+    };
+
+    xmlParserCtxtPtr xml_context = xmlCreateMemoryParserCtxt(srcml.data(), (int) srcml.size());
+    auto save_private = xml_context->_private;
+    xml_context->_private = &context;
+    auto save_sax = xml_context->sax;
+    xml_context->sax = &charactersax;
+
+    xmlParseDocument(xml_context);
+
+    xml_context->_private = save_private;
+    xml_context->sax = save_sax;
+
+    xmlFreeParserCtxt(xml_context);
+
+    return context.src;
+}
+
+std::string_view attribute_revision(std::string_view attribute, int revision) {
+
+    auto pos = attribute.find('|');
+    if (pos == std::string::npos)
+        return attribute;
+
+    if (revision == SRCDIFF_REVISION_ORIGINAL)
+        return attribute.substr(0, pos);
+
+    return attribute.substr(pos + 1);
 }
