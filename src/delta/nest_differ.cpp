8373aaad4918f5c10d4958ca151ecbd397e057e1
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2026-09-04 14:59:27 -0400

    Only get measure when check is_better_nested guard passes

--- src/delta/nest_differ.cpp
+++ src/delta/nest_differ.cpp
@@ -85,9 +85,10 @@ bool is_better_nest(std::shared_ptr<const construct> node_set_outer,
 
 bool nest_differ::is_better_nested(construct::construct_list_view original, construct::construct_list_view modified) {
 
-  const measurer & measure = *original[0]->measure(*modified[0]);
   nest_result nestable = check_nestable(original, modified);
   if(nestable.operation == COMMON) return false;
+
+  const measurer & measure = *original[0]->measure(*modified[0]);
 
   for(std::size_t pos = 0; pos < original.size(); ++pos) {
     if(is_better_nest(original[pos], modified[0], measure)) {
